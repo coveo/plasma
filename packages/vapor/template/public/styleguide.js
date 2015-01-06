2597fe12d281ab8d2e78d76ea6370efa584d334a
Jean-Philippe Lachance <jplachance@coveo.com>
2015-01-06 18:35:13 -0500

    Change kss template for a better one, customize it, make some fixes.

--- packages/vapor/template/public/styleguide.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	hljs.initHighlightingOnLoad();
-
-	var ref = $('.styleguide-menu').find('.styleguide-menu-list').data('kss-ref');
-	$('.styleguide-menu').find('a').eq(ref).addClass('selected');
-});
