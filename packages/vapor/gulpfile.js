45e282a225effb806508b3f9d48fe9e023fdd602
Jean-Philippe Lachance <jplachance@coveo.com>
2015-03-25 11:46:21 -0400

    * Make sure less files are imported in only one place * Move fonts and images folder to resources * Add SVG! * Add a proper sprites management

--- packages/vapor/gulpfile.js
+++ packages/vapor/gulpfile.js
@@ -1,7 +1,9 @@
 var concat = require('gulp-concat');
 var del = require('del');
 var git = require('gulp-git');
-var gulp = require('gulp');
+var gulp = require('gulp-help')(require('gulp'));
+var less = require('gulp-less');
+var pngSprite = require('png-sprite');
 var rename = require('gulp-rename');
 var run = require('gulp-run');
 
@@ -11,7 +13,26 @@ gulp.task('clean', function () {
     });
 });
 
-gulp.task('kss-compile', function () {
+gulp.task('sprites', 'Generate sprites.less from images', function () {
+    var template = '<%= "i." + node.className + ", ." + node.className %> {\
+      background-position:<%= -node.x / ratio %>px <%= -node.y / ratio %>px;\
+      width:<%= node.width / ratio %>px;\
+      height:<%= node.height / ratio %>px;\
+      text-indent:<%= node.width / ratio %>px;\
+    }';
+
+    return gulp.src('./resources/sprites/**/*.png')
+        .pipe(pngSprite.gulp({
+            cssPath: 'sprites.less',
+            pngPath: '../target/package/images/CoveoStyleGuide.Sprites.png',
+            relPath: '../images/CoveoStyleGuide.Sprites.png',
+            eachTemplate: template,
+            namespace: 'coveo-sprites'
+        }))
+        .pipe(gulp.dest('less'));
+});
+
+gulp.task('kss-compile', 'Build the styleguide using kss-node', ['sprites'], function () {
     run('kss-node less --less less/guide.less --template template').exec([],
         function () {
             gulp.src('styleguide/public/style.css')
@@ -35,12 +56,17 @@ gulp.task('pull', function () {
 });
 
 gulp.task('styleguide', ['kss-compile', 'lib'], function () {
-    gulp.src('./images/**').pipe(gulp.dest('styleguide/images'));
-    gulp.src('./fonts/**').pipe(gulp.dest('styleguide/fonts'));
+    gulp.src('./resources/images/**').pipe(gulp.dest('styleguide/images'));
+    gulp.src('./resources/fonts/**').pipe(gulp.dest('styleguide/fonts'));
+    gulp.src('./target/package/images/CoveoStyleGuide.Sprites.png').pipe(gulp.dest('styleguide/images'));
 });
 
-gulp.task('default', ['styleguide']);
+gulp.task('default', 'Compile with all dependencies', ['styleguide'], function () {}, {
+    aliases: ['d', 'D']
+});
 
 gulp.task('watch', function () {
     gulp.watch(['less/**/*'], ['kss-compile']);
+}, {
+    aliases: ['w', 'W']
 });
