b50d00e0bac599b779f428b5a0aecb1a479d1fa5
William Fortin <wfortin@coveo.com>
2015-10-13 09:00:41 -0400

    Add publish task script

--- packages/vapor/publish.sh
+++ packages/vapor/publish.sh
@@ -2,11 +2,3 @@
 rm -rf styleguide || exit 0;
 npm install
 npm run styleguide
-( cd styleguide
- git init
- git config user.name "wfortin"
- git config user.email "willyfortin@gmail.com"
- git add .
- git commit -m "Autodeploy to Github Pages"
- git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
-)
