ba15968b1a58fba456d0a028cd1b160606f2c398
William Fortin <wfortin@coveo.com>
2015-10-15 13:55:58 -0400

    Auto deploy script for gh-pages

--- packages/vapor/deploy.sh
+++ packages/vapor/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+rm -rf styleguide || exit 0;
 npm install
-npm run clean
 npm run compile
 
-#TODO add an auto deploy
+git init
+git config user.name "wfortin"
+git config user.email "willyfortin@gmail.com"
+git add .
+git commit -m "Autodeploy to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
