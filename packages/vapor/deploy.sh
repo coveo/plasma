8915549f3b0073f768e431c82751a2cc01b8a4de
William Fortin <willyfortin@gmail.com>
2015-10-15 16:23:46 -0400

    Update deploy.sh

--- packages/vapor/deploy.sh
+++ packages/vapor/deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-npm install
+-rm -rf _gh_pages || exit 0;
+
 npm run compile
 
+bundle exec jekyll build
+
+( cd _gh_pages
 git init
 git config user.name "wfortin"
 git config user.email "willyfortin@gmail.com"
 git add .
 git commit -m "Autodeploy to Github Pages"
 git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
+)
