bff2b35949bf579d2c07550cf7c2381542b02c28
William Fortin <willyfortin@gmail.com>
2015-11-11 17:10:52 -0500

    Merge pull request #27 from coveo/feature-buttons
    Buttons

--- packages/vapor/gulpTasks/sass.js
+++ packages/vapor/gulpTasks/sass.js
@@ -37,7 +37,7 @@ gulp.task('sass', 'Compile sass files to dist folder', ['sprites'], function () 
 });
 
 gulp.task('sass:format', function() {
-    return gulp.src(['./scss/**/*.scss', '!./scss/sprites.scss', '!./scss/common/palette.scss'])
+    return gulp.src(['./scss/**/*.scss', '!./scss/sprites.scss', '!./scss/utility/colors.scss'])
         .pipe(csscomb())
         .pipe(gulp.dest('./scss'));
 });
