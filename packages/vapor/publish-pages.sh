9eb86e5e210b65526b21461feac6343b2374e602
Pierre-Alexandre St-Jean <pa@stjean.me>
2015-11-20 15:12:10 -0500

    linting

--- packages/vapor/publish-pages.sh
+++ packages/vapor/publish-pages.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-rm -rf _gh_pages || exit 0;
-
-bundle install
-bundle exec jekyll build
 
 ( cd _gh_pages
-git init
-git config user.name "wfortin"
-git config user.email "willyfortin@gmail.com"
-git add .
-git commit -m "Autodeploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
+  git init
+  git config user.name "wfortin"
+  git config user.email "willyfortin@gmail.com"
+  git add .
+  git commit -m "Autodeploy to Github Pages"
+  git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
 )
