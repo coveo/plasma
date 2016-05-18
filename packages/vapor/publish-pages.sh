b63d4e5eab12cd12d9a64e13c7cf608964e7c33d
Pierre-Alexandre St-Jean <pa@stjean.me>
2016-05-18 16:44:41 -0400

    Merge pull request #155 from coveo/fixpublish
    Update publish-pages.sh for new repos

--- packages/vapor/publish-pages.sh
+++ packages/vapor/publish-pages.sh
@@ -6,5 +6,5 @@
   git config user.email "willyfortin@gmail.com"
   git add .
   git commit -m "Autodeploy to Github Pages"
-  git push --force --quiet "https://${GH_TOKEN}@github.com/Coveo/styleguide.git" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@github.com/coveo/vapor.git" master:gh-pages > /dev/null 2>&1
 )
