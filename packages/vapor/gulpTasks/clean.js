cb6d2ae6cc8d226167b8dc62e44f22ee808d2dff
pochretien <moipeter10@gmail.com>
2016-06-13 18:25:39 -0400

    Merge branch 'master' of https://github.com/coveo/styleguide

--- packages/vapor/gulpTasks/clean.js
+++ packages/vapor/gulpTasks/clean.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp-help')(require('gulp'));
 var del = require('del');
 
-gulp.task('clean', 'Delete the dist folder and clean the docs output', function () {
-    del(['./dist', './docs/dist', './_gh_pages', './.sass-cache'], function (err, deletedFiles) {
+gulp.task('clean', 'Delete the dist folder and clean the docs output', function() {
+    return del(['./dist', './docs/dist', './_gh_pages', './.sass-cache']).then(function(deletedFiles) {
         console.log('Files deleted:', deletedFiles.join(', '));
     });
 });
