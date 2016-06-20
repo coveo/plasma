72b487e5d81538d1911d21c5fcc1a0cf02bf7745
Pierre-Olivier Chretien <moipeter10@gmail.com>
2016-06-20 17:10:49 -0400

    Merge pull request #176 from coveo/feature-texteare
    Feature texteare and multiline input

--- packages/vapor/gulpTasks/docs.js
+++ packages/vapor/gulpTasks/docs.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp-help')(require('gulp'));
 
-gulp.task('docs', 'Build less, libs and sprites and copy it to docs', ['default'], function () {
+gulp.task('docs', 'Build less, libs and sprites and copy it to docs', ['default', 'docs:external-libs'], function () {
     return gulp.src('./dist/**/*')
         .pipe(gulp.dest('./docs/dist'));
+});
+
+gulp.task('docs:external-libs', 'Build dev libs and copy the js to docs', function() {
+    return gulp.src('./node_modules/autosize/dist/autosize.js')
+        .pipe(gulp.dest('./docs/dist/js'));
 });
