f51479549ecc8e5d80c70fb80d0fc79d540bb20d
Stephanie Roy <sroy3@users.noreply.github.com>
2016-09-21 08:56:24 -0400

    Merge pull request #232 from coveo/coveo/svg-module
    *Defined coveo-styleguide with VaporSVG

--- packages/vapor/resources/js/VaporSVG.js
+++ packages/vapor/resources/js/VaporSVG.js
@@ -1,54 +1,74 @@
-/* SVG Enum */
+(function() {
+    /* SVG Enum */
 
-function svgWrapper(svgString, svgClass, spanClass, title, attr) {
-    svgClass = svgClass || '';
-    spanClass = spanClass || '';
-    title = title || '';
-    attr = attr || {};
+    function svgWrapper(svgString, svgClass, spanClass, title, attr) {
+        svgClass = svgClass || '';
+        spanClass = spanClass || '';
+        title = title || '';
+        attr = attr || {};
 
-    if (s.contains(svgString, '#coveo-icon-')) {
-        var svgNameFormatted = formatSvgName(svgString.replace('#coveo-icon-', ''));
-        if (!_.isUndefined(svgEnum[svgNameFormatted])) {
-            return svgEnum[svgNameFormatted].render(svgClass, spanClass, title, attr);
+        if (s.contains(svgString, '#coveo-icon-')) {
+            var svgNameFormatted = formatSvgName(svgString.replace('#coveo-icon-', ''));
+            if (!_.isUndefined(svgEnum[svgNameFormatted])) {
+                return svgEnum[svgNameFormatted].render(svgClass, spanClass, title, attr);
+            }
         }
+
+        var titleToDisplay = title ? 'title="' + title + '"' : '';
+        var spanClassAttribute = spanClass ? 'class="' + spanClass + '"' : '';
+        return '<span ' + spanClassAttribute + ' ' + titleToDisplay + '>' + svg(svgString, svgClass, attr) + '</span>';
     }
 
-    var titleToDisplay = title ? 'title="' + title + '"' : '';
-    var spanClassAttribute = spanClass ? 'class="' + spanClass + '"' : '';
-    return '<span ' + spanClassAttribute + ' ' + titleToDisplay + '>' + svg(svgString, svgClass, attr) + '</span>';
-}
+    function svgFromName(svgName, svgClass, spanClass, title) {
+        svgClass = svgClass || '';
+        spanClass = spanClass || '';
+        title = title || '';
 
-function svgFromName(svgName, svgClass, spanClass, title) {
-    svgClass = svgClass || '';
-    spanClass = spanClass || '';
-    title = title || '';
-
-    if (!_.isUndefined(svgEnum[svgName])) {
-        return svgEnum[svgName].render(svgClass, spanClass, title);
+        if (!_.isUndefined(svgEnum[svgName])) {
+            return svgEnum[svgName].render(svgClass, spanClass, title);
+        }
+        return '';
     }
-    return '';
-}
 
-function svg(svgString, svgClass, attr) {
-    svgClass = svgClass || '';
-    attr = attr || {};
+    function svg(svgString, svgClass, attr) {
+        svgClass = svgClass || '';
+        attr = attr || {};
 
-    if (svgString) {
-        var svgHTML = $($.parseHTML(svgString));
-        svgHTML[0].setAttribute('class', svgClass);
-        _.mapObject(attr, function(value, key) {
-            svgHTML[0].setAttribute(key, value);
+        if (svgString) {
+            var svgHTML = $($.parseHTML(svgString));
+            svgHTML[0].setAttribute('class', svgClass);
+            _.mapObject(attr, function (value, key) {
+                svgHTML[0].setAttribute(key, value);
+            });
+            return svgHTML.prop('outerHTML');
+        }
+        return '<svg class="' + svgClass + '"></svg>';
+    }
+
+    function formatSvgName(svgName) {
+        return s.camelize(s.humanize(svgName), true);
+    }
+
+    var VaporSVG = {};
+    VaporSVG.SVG = svgWrapper;
+    VaporSVG.svgFromName = svgFromName;
+    VaporSVG.svg = svgEnum;
+
+    var root = this;
+
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = VaporSVG;
+        }
+        exports.VaporSVG = VaporSVG;
+    } else {
+        root.VaporSVG = VaporSVG;
+    }
+
+    if (typeof define === 'function' && define.amd) {
+        define('coveo-styleguide', [], function() {
+            return VaporSVG;
         });
-        return svgHTML.prop('outerHTML');
     }
-    return '<svg class="' + svgClass + '"></svg>';
-}
+}.call(this));
 
-function formatSvgName(svgName) {
-    return s.camelize(s.humanize(svgName), true);
-}
-
-var VaporSVG = {};
-VaporSVG.SVG = svgWrapper;
-VaporSVG.svgFromName = svgFromName;
-VaporSVG.svg = svgEnum;
