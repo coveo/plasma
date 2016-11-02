1dff851665b1c823d404f0d10d054eb56efd397e
Stephanie Roy <sroy3@coveo.com>
2016-11-02 09:56:29 -0400

    - Removed now useless PleasingWebstorm file

--- src/PleasingWebstorm.ts
+++
@@ -1,4 +0,0 @@
-// References to types that cannot be found by WebStorm depending on file location
-// Webpack can easily compile without them, but WebStorm is pickier
-
-///<reference path="../node_modules/@types/jasmine/index.d.ts"/>
