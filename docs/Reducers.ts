eb467894077c0d331b3fc640653a3a5fdb584b5d
Stephanie Roy <sroy3@coveo.com>
2016-11-02 14:40:44 -0400

    ! Formated code

--- docs/Reducers.ts
+++ docs/Reducers.ts
@@ -1,7 +1,7 @@
 import { Reducer, combineReducers } from 'redux';
 import { membersReducers, IMembersCompositeState } from './members-example/reducers/MembersReducers';
-import {IReactVaporState} from '../src/utils/ReduxUtils';
-import {lastUpdatedComposite} from '../src/components/lastUpdated/LastUpdatedReducers';
+import { IReactVaporState } from '../src/utils/ReduxUtils';
+import { lastUpdatedComposite } from '../src/components/lastUpdated/LastUpdatedReducers';
 
 export interface IReactVaporExampleState extends IReactVaporState {
   membersCompositeState: IMembersCompositeState;
