36570dc625a7a0d20be762c36e2022b582df005b
Stephanie Roy <sroy3@coveo.com>
2016-11-16 16:31:57 -0500

    * Format code

--- src/Index.ts
+++ src/Index.ts
@@ -1,19 +1,19 @@
-import {ChosenSelect} from './components/chosen/ChosenSelect';
-import {Popover} from './components/popover/Popover';
-import {Svg} from './components/svg/Svg';
-import {Tooltip} from './components/tooltip/Tooltip';
-import {LastUpdated} from './components/lastUpdated/LastUpdated';
-import {LastUpdatedConnected} from './components/lastUpdated/LastUpdatedConnected';
-import {lastUpdatedCompositeReducer} from './components/lastUpdated/LastUpdatedReducers';
-import {changeLastUpdated} from './components/lastUpdated/LastUpdatedActions';
-import {FilterBox} from './components/filterBox/FilterBox';
-import {FilterBoxConnected} from './components/filterBox/FilterBoxConnected';
-import {filterBoxesReducer} from './components/filterBox/FilterBoxReducers';
-import {filterThrough} from './components/filterBox/FilterBoxActions';
-import {Facet} from './components/facets/Facet';
-import {FacetConnected} from './components/facets/FacetConnected';
-import {facetsReducer} from './components/facets/FacetReducers';
-import {changeFacet, emptyFacet} from './components/facets/FacetActions';
+import { ChosenSelect } from './components/chosen/ChosenSelect';
+import { Popover } from './components/popover/Popover';
+import { Svg } from './components/svg/Svg';
+import { Tooltip } from './components/tooltip/Tooltip';
+import { LastUpdated } from './components/lastUpdated/LastUpdated';
+import { LastUpdatedConnected } from './components/lastUpdated/LastUpdatedConnected';
+import { lastUpdatedCompositeReducer } from './components/lastUpdated/LastUpdatedReducers';
+import { changeLastUpdated } from './components/lastUpdated/LastUpdatedActions';
+import { FilterBox } from './components/filterBox/FilterBox';
+import { FilterBoxConnected } from './components/filterBox/FilterBoxConnected';
+import { filterBoxesReducer } from './components/filterBox/FilterBoxReducers';
+import { filterThrough } from './components/filterBox/FilterBoxActions';
+import { Facet } from './components/facets/Facet';
+import { FacetConnected } from './components/facets/FacetConnected';
+import { facetsReducer } from './components/facets/FacetReducers';
+import { changeFacet, emptyFacet } from './components/facets/FacetActions';
 import {
   ReduxUtils,
   ReduxConnect,
