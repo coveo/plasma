6db2527700fdd95ae73aa1a5fd09a9a76ce92a0a
Stephanie Roy <sroy3@coveo.com>
2017-01-11 16:12:18 -0500

    + Added a check to see if the tooltip element is still there before destroying it

--- src/components/tooltip/Tooltip.spec.tsx
+++ src/components/tooltip/Tooltip.spec.tsx
@@ -1,6 +1,6 @@
 import { shallow, mount, ReactWrapper } from 'enzyme';
 import { ITooltipProps, Tooltip } from './Tooltip';
-
+import * as $ from 'jquery';
 // Until Webpack provided plugins works with TS 2.0
 /* tslint:disable:no-unused-variable */
 import * as React from 'react';
@@ -34,6 +34,38 @@ describe('<Tooltip>', () => {
         { attachTo: document.getElementById('App') }
       );
     }).not.toThrow();
+
+    expect(() => {
+      tooltipWrapper.unmount();
+      tooltipWrapper.detach();
+    }).not.toThrow();
+  });
+
+  it('should unmount without error when tooltip element is not present in the DOM', () => {
+    tooltipWrapper = mount(
+      <Tooltip {...tooltipProps}>
+        Hover me!
+      </Tooltip>,
+      { attachTo: document.getElementById('App') }
+    );
+
+    let tooltip = (tooltipWrapper.instance()as Tooltip)['tooltip'];
+    tooltip = $('.new');
+
+    expect(tooltip[0]).toBeFalsy();
+    expect(() => {
+      tooltipWrapper.unmount();
+      tooltipWrapper.detach();
+    }).not.toThrow();
+
+
+    tooltipWrapper = mount(
+      <Tooltip {...tooltipProps}>
+        Hover me!
+      </Tooltip>,
+      { attachTo: document.getElementById('App') }
+    );
+    (tooltipWrapper.instance()as Tooltip)['tooltip'] = null;
 
     expect(() => {
       tooltipWrapper.unmount();
