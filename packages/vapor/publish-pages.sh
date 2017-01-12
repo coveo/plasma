b59fcff42cc76225355a247419468f226c3871d0
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2017-01-12 14:49:23 -0500

    Fix travis ci with multiple nice things

--- packages/vapor/publish-pages.sh
+++ packages/vapor/publish-pages.sh
@@ -4,9 +4,9 @@ set -x
 
 ( cd _gh_pages
   git init
-  git config user.name "wfortin"
-  git config user.email "willyfortin@gmail.com"
+  git config user.name "pastjean"
+  git config user.email "pastjean@coveo.com"
   git add .
   git commit -m "Autodeploy to Github Pages"
-  git push --force --quiet "https://${GH_TOKEN}@github.com/coveo/vapor.git" master:gh-pages > /dev/null 2>&1
+  git push --force --quiet "git@github.com/coveo/vapor.git" master:gh-pages > /dev/null 2>&1
 )
