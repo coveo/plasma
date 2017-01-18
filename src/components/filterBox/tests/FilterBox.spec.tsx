e5dccb07474505ee257bb1601e7f87353083148d
sbelzile <sbelzile@coveo.com>
2017-01-17 20:14:35 -0500

    *fixes unit test

--- src/components/filterBox/tests/FilterBox.spec.tsx
+++ src/components/filterBox/tests/FilterBox.spec.tsx
@@ -119,10 +119,11 @@ describe('FilterBox', () => {
 
     it('should set container class when the container class is specified', () => {
       let containerClass = 'mod-small';
+      let containerClasses = [containerClass];
       let container = filterBox.find('div').first();
       expect(container.hasClass(containerClass)).toBe(false);
 
-      filterBox.setProps({ id: id, containerClass });
+      filterBox.setProps({ id: id, containerClasses });
       filterBox.mount();
       expect(container.hasClass(containerClass)).toBe(true);
     });
