5ae389431881d1baa719284a28fd9086becd5543
Pierre-Olivier Chretien <moipeter10@gmail.com>
2017-01-27 13:58:22 -0500

    Merge pull request #302 from coveo/fix-prompt-header-text-color
    add the pure-white color for the prompt header text

--- packages/vapor/publish-pages.sh
+++ packages/vapor/publish-pages.sh
@@ -4,8 +4,8 @@ set -x
 
 ( cd _gh_pages
   git init
-  git config user.name "pastjean"
-  git config user.email "pastjean@coveo.com"
+  git config user.name "pochretien"
+  git config user.email "pochretien@coveo.com"
   git add .
   git commit -m "Autodeploy to Github Pages"
   git push --force --quiet "git@github.com:coveo/vapor.git" master:gh-pages
