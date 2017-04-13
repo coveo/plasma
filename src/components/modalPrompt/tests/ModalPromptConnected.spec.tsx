0f95d95aa3046edf751ef0e2b794b6c92f980918
mroussel <rousselm4@gmail.com>
2017-04-13 14:28:02 -0400

    Refactor to clean up tests

--- src/components/modalPrompt/tests/ModalPromptConnected.spec.tsx
+++ src/components/modalPrompt/tests/ModalPromptConnected.spec.tsx
@@ -21,6 +21,14 @@ describe('ModalPrompt', () => {
     const cancelLabel: string = 'Cancel';
     const message: string = 'message';
 
+    const findInStore = (store: Store<IReactVaporState>, id: string) => {
+      return store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id);
+    };
+
+    const findOneInStore = (store: Store<IReactVaporState>, id: string) => {
+      return findInStore(store, id)[0];
+    };
+
     beforeEach(() => {
       store = TestUtils.buildStore();
 
@@ -46,116 +54,122 @@ describe('ModalPrompt', () => {
     });
 
     it('should get its id as a prop', () => {
-      let idProp = modalPrompt.props().id;
+      const idProp = modalPrompt.props().id;
 
       expect(idProp).toBeDefined();
       expect(idProp).toBe(id);
     });
 
     it('should get its title as a prop', () => {
-      let titleProp = modalPrompt.props().title;
+      const titleProp = modalPrompt.props().title;
 
       expect(titleProp).toBeDefined();
       expect(titleProp).toBe(title);
     });
 
     it('should get its message as a prop', () => {
-      let titleProp = modalPrompt.props().message;
+      const titleProp = modalPrompt.props().message;
 
       expect(titleProp).toBeDefined();
       expect(titleProp).toBe(message);
     });
 
     it('should get the cancel label as a prop', () => {
-      let titleProp = modalPrompt.props().cancelLabel;
+      const titleProp = modalPrompt.props().cancelLabel;
 
       expect(titleProp).toBeDefined();
       expect(titleProp).toBe(cancelLabel);
     });
 
     it('should get the confirm label as a prop', () => {
-      let titleProp = modalPrompt.props().confirmLabel;
+      const titleProp = modalPrompt.props().confirmLabel;
 
       expect(titleProp).toBeDefined();
       expect(titleProp).toBe(confirmLabel);
     });
 
     it('should get isOpened false as a prop', () => {
-      let isOpened = modalPrompt.props().isOpened;
+      const isOpened = modalPrompt.props().isOpened;
 
       expect(isOpened).toBeDefined();
       expect(isOpened).toBe(false);
     });
 
     it('should get what to do on render as a prop', () => {
-      let onRenderProp = modalPrompt.props().onRender;
+      const onRenderProp = modalPrompt.props().onRender;
 
       expect(onRenderProp).toBeDefined();
     });
 
     it('should get what to do on destroy as a prop', () => {
-      let onDestroyProp = modalPrompt.props().onDestroy;
+      const onDestroyProp = modalPrompt.props().onDestroy;
 
       expect(onDestroyProp).toBeDefined();
     });
 
     it('should get what to do on cancel as a prop', () => {
-      let onCloseProp = modalPrompt.props().onCancel;
+      const onCancelProp = modalPrompt.props().onCancel;
 
-      expect(onCloseProp).toBeDefined();
+      expect(onCancelProp).toBeDefined();
+    });
+
+    it('should get what to do on confirm as a prop', () => {
+      const onConfirmProp = modalPrompt.props().onConfirm;
+
+      expect(onConfirmProp).toBeDefined();
     });
 
     it('should add the modalPrompt in the store on render', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id).length).toBe(1);
+      expect(findInStore(store, id).length).toBe(1);
     });
 
     it('should open the modalPrompt in the store when dispatching a "openModalPrompt" action', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id).length).toBe(1);
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findInStore(store, id).length).toBe(1);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
 
       store.dispatch(openModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(true);
+      expect(findOneInStore(store, id).isOpened).toBe(true);
     });
 
     it('should close the modalPrompt in the store when dispatching a "cancelModalPrompt" action', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
       store.dispatch(openModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(true);
+      expect(findOneInStore(store, id).isOpened).toBe(true);
 
       store.dispatch(cancelModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
     });
 
     it('should close the modalPrompt in the store when clicking on modalPrompt x', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
       store.dispatch(openModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(true);
+      expect(findOneInStore(store, id).isOpened).toBe(true);
 
       modalPrompt.find('.small-close').simulate('click');
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
     });
 
     it('should close the modalPrompt in the store when clicking on confirm', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
       store.dispatch(openModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(true);
+      expect(findOneInStore(store, id).isOpened).toBe(true);
 
       modalPrompt.find('.mod-primary').simulate('click');
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].isOpened).toBe(false);
+      expect(findOneInStore(store, id).isOpened).toBe(false);
     });
 
     it('should set confirmed to true in the store when clicking on confirm', () => {
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].confirmed).toBe(false);
+      expect(findOneInStore(store, id).confirmed).toBe(false);
       store.dispatch(openModalPrompt(id));
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].confirmed).toBe(false);
+      expect(findOneInStore(store, id).confirmed).toBe(false);
 
       modalPrompt.find('.mod-primary').simulate('click');
-      expect(store.getState().modalPrompts.filter(modalPrompt => modalPrompt.id === id)[0].confirmed).toBe(true);
+      expect(findOneInStore(store, id).confirmed).toBe(true);
     });
 
     it('should remove the modalPrompt in the store on destroy', () => {
       wrapper.unmount();
-      expect(store.getState().modalPrompts.filter(modalPrompts => modalPrompts.id === id).length).toBe(0);
+      expect(findInStore(store, id).length).toBe(0);
     });
   });
 });
