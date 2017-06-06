8689ddfee60f8471cd8c18469b4903a00abc3c44
Sebastien Belzile <sbelzile@coveo.com>
2017-06-06 09:41:52 -0400

    *more tests

---
+++ src/components/input/tests/Input.spec.tsx
@@ -0,0 +1,58 @@
+import { shallow, mount, ReactWrapper } from 'enzyme';
+// tslint:disable-next-line:no-unused-variable
+import * as React from 'react';
+import { Input, IInputProps } from '../Input';
+
+describe('Input', () => {
+  //let title: string = 'Title';
+
+  describe('<Input />', () => {
+    it('should render without errors', () => {
+      expect(() => {
+        shallow(
+          <Input />
+        );
+      }).not.toThrow();
+    });
+  });
+
+  describe('<Modal />', () => {
+    let input: ReactWrapper<IInputProps, any>;
+    let inputInstance: Input;
+
+    beforeEach(() => {
+      input = mount(
+        <Input />,
+        { attachTo: document.getElementById('App') }
+      );
+      inputInstance = input.instance() as Input;
+    });
+
+    afterEach(() => {
+      input.unmount();
+      input.detach();
+    });
+
+    it('should call prop onChange on inner input blur', () => {
+      let changeSpy = jasmine.createSpy('onChange');
+      let innerInput = input.find('input');
+
+      input.setProps({ onChange: changeSpy });
+      input.mount();
+      innerInput.simulate('blur');
+
+      expect(changeSpy.calls.count()).toBe(1);
+    });
+
+    it('should call prop onKeyUp on key up', () => {
+      let keyUpSpy = jasmine.createSpy('onKeyUp');
+      let innerInput = input.find('input');
+
+      input.setProps({ onKeyUp: keyUpSpy });
+      input.mount();
+      innerInput.simulate('keyUp');
+
+      expect(keyUpSpy.calls.count()).toBe(1);
+    });
+  });
+});
