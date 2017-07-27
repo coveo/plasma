c565e18644f07fa2b6c80e5426a9c782b0bad779
Daniel <daniel.carmichael1@gmail.com>
2017-07-27 11:44:47 -0400

    add SelectedOption tests

---
+++ src/components/dropdownSearch/MultiSelectDropdownSearch/tests/SelectedOption.spec.tsx
@@ -0,0 +1,69 @@
+import { mount, ReactWrapper } from 'enzyme';
+// tslint:disable-next-line:no-unused-variable
+import * as React from 'react';
+import { UUID } from '../../../../utils/UUID';
+import {DropdownSearch, IDropdownOption, IDropdownSearchProps} from '../DropdownSearch';
+import {ISelectedOptionProps, SelectedOption} from '../SelectedOption';
+
+describe('SelectedOption', () => {
+  const key: string = UUID.generate();
+  const props: ISelectedOptionProps = {
+    key,
+    displayValue: 'test',
+    onRemoveClick: () => {},
+  };
+
+  describe('<SelectedOption />', () => {
+    let selectedOption: ReactWrapper<ISelectedOptionProps, any>;
+    let selectedOptionInstance: SelectedOption;
+
+    const renderSelectedOption = (props?: IDropdownSearchProps) => {
+      selectedOption = mount(
+        <SelectedOption {...props} />,
+        { attachTo: document.getElementById('App') },
+      );
+      selectedOptionInstance = selectedOption.instance() as SelectedOption;
+    };
+
+    beforeEach(() => {
+      renderSelectedOption(props);
+    });
+
+    afterEach(() => {
+      selectedOption.unmount();
+      selectedOption.detach();
+    });
+
+    describe('render', () => {
+      it('should render the display value in the selectedOption', () => {
+        const displayValue: string = 'displayTest';
+        selectedOption.setProps({ displayValue });
+
+        expect(selectedOption.find('.selected-option-value').text()).toBe(displayValue);
+      });
+    });
+
+    describe('remove option', () => {
+      it('should call remove option when click on the remove-option div', () => {
+        const onRemoveOptionClick = jasmine.createSpy('onRemoveOptionClick');
+        selectedOption.setProps({ onRemoveClick: onRemoveOptionClick });
+
+        selectedOption.find('.remove-option').simulate('click');
+
+        expect(onRemoveOptionClick).toHaveBeenCalled();
+      });
+
+      it('should not call remove option when clicking anywhere else', () => {
+        const onRemoveOptionClick = jasmine.createSpy('onRemoveOptionClick');
+        selectedOption.setProps({ onRemoveClick: onRemoveOptionClick });
+
+        selectedOption.simulate('click');
+        selectedOption.find('.selected-option').simulate('click');
+        selectedOption.find('.selected-option-value').simulate('click');
+        selectedOption.find('.selected-option-separator').simulate('click');
+
+        expect(onRemoveOptionClick).not.toHaveBeenCalled();
+      });
+    });
+  });
+});
