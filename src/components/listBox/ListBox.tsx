78d7a81b3d2f5bb1675f42558fbe07ec06f24f70
pochretien <moipeter10@gmail.com>
2017-09-13 15:44:32 -0400

    fix the listbox with the new itembox

--- src/components/listBox/ListBox.tsx
+++ src/components/listBox/ListBox.tsx
@@ -1,46 +1,22 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
 import * as _ from 'underscore';
-import { ISvgProps, Svg } from '../svg/Svg';
-
-export interface IItemOption {
-  id?: string;
-  svg?: ISvgProps;
-  value: string;
-  displayValue?: string;
-  prefix?: string;
-  selected?: boolean;
-  hidden?: boolean;
-  title?: string;
-}
+import {BoxItem, IBoxItemProps} from '../boxItem/BoxItem';
 
 export interface IListBoxOwnProps {
-  id: string;
-  noResultText?: string;
-  classes?: string[];
-  itemClasses?: string[];
-  defaultItems?: IItemOption[];
-  hideSelectedItem?: boolean;
-  showTitleOnItem?: boolean;
-  onOptionClickCallBack?: (option: IItemOption) => void;
+  items?: IBoxItemProps[];
+  onOptionClick?: (option: IBoxItemProps) => void;
+  noResultItem: IBoxItemProps;
 }
 
-export interface IListBoxStateProps {
-  items?: IItemOption[];
-}
-
-export interface IListBoxDispatchProps {
-  onOptionClick?: (option: IItemOption) => void;
-}
-
-export interface IListBoxProps extends IListBoxOwnProps, IListBoxStateProps, IListBoxDispatchProps { }
+export interface IListBoxProps extends IListBoxOwnProps, IListBoxStateProps { }
 
 export class ListBox extends React.Component<IListBoxProps, void> {
 
   static defaultProps: Partial<IListBoxProps> = {
-    noResultText: 'No items',
-    showTitleOnItem: true,
-    hideSelectedItem: false,
+    noResultItem: {
+      value: 'No Items',
+    }
   };
 
   getClasses(): string {
@@ -49,82 +25,28 @@ export class ListBox extends React.Component<IListBoxProps, void> {
 
   protected getItems(): JSX.Element[] {
     const items = _.chain(this.props.items)
-      .map((item: IItemOption) => {
-        const liClasses = classNames(
-          'list-box-item',
-          {
-            'active': item.selected,
-          });
-
-        const value = item.displayValue || item.value;
-        const title = this.props.showTitleOnItem ? item.title || value : '';
-
-        return (
-          <li key={item.id || item.value}
-            className={liClasses}
-            title={title}>
-            <span onMouseDown={(e: React.MouseEvent<HTMLSpanElement>) => this.handleOnOptionClick(e)}
-              data-value={item.value}>
-              {this.getDropdownPrepend(item)}
-              {this.getSvg(item)}
-              {value}
-            </span>
-          </li>
-        );
-      })
+      .map((item: IBoxItemProps) => <BoxItem{...item}/>)
       .value();
 
-    return items.length ? items : this.getNoItems();
-  }
-
-  protected getDropdownPrepend(item: IItemOption): JSX.Element {
-    if (item && item.prefix) {
-      return <span key={`${item.id || item.value}_${item.prefix}`}
-        className='dropdown-prepend'>
-        {item.prefix}
-      </span>;
-    }
-    return null;
-  }
-
-  protected getSvg(item: IItemOption): JSX.Element {
-    if (item && item.svg) {
-      return (
-        <span key={`${item.id || item.value}_${item.svg.name}`}
-          className='value-icon'>
-          <Svg {...item.svg} />
-        </span>
-      );
-    }
-    return null;
-  }
-
-  protected getNoItems(): JSX.Element[] {
-    return [
-      <li key='noResultDropdownSearch'>
-        <span className='no-search-results'>{this.props.noResultText}</span>
-      </li>,
-    ];
-  }
-
-  handleOnOptionClick = (e: React.MouseEvent<HTMLSpanElement>) => {
-    if (e.target) {
-      const items = _.findWhere(this.props.items, { value: e.currentTarget.dataset.value });
-
-      if (this.props.onOptionClick) {
-        this.props.onOptionClick(items);
+      if (items.length) {
+        return items;
       }
 
-      if (this.props.onOptionClickCallBack) {
-        this.props.onOptionClickCallBack(items);
-      }
+      return (<BoxItem {...this.props.noResultItem}/>)
+  }
+
+  handleOnOptionClick = (option: IBoxItemProps) => {
+    if (this.props.onOptionClick) {
+      this.props.onOptionClick(option);
     }
   }
 
   render() {
     return (
       <ul className={this.getClasses()}>
-        {this.getItems()}
+        <BoxItem
+
+        />
       </ul>
     );
   }
