743d8582d3a67267c28cb64012a2bcc3af689f8b
Pierre-Olivier Chretien <pochretien@coveo.com>
2017-09-22 12:07:37 -0400

    apply reviews
    update styleguide
    add a new param to add props on component for the Content
    add an example

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import * as classname from 'classnames';
 import * as _ from 'underscore';
 
 export interface IContentProps {
   content: typeof React.Component | string | (() => JSX.Element);
+  componentProps?: { [key: string]: any };
   classes?: string[];
 }
 
@@ -13,11 +15,12 @@ export class Content extends React.Component<IContentProps, void> {
   };
 
   private getContent(): JSX.Element | string {
-    return _.isString(this.props.content) ? this.props.content : <this.props.content />;
+    return _.isString(this.props.content) ? this.props.content : <this.props.content {...this.props.componentProps} />;
   }
 
   render() {
-    return (<span className={this.props.classes.join(' ')}>
+    const classes = classname(this.props.classes);
+    return (<span className={classes}>
       {this.getContent()}
     </span>);
   }
