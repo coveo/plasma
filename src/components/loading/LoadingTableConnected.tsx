0515c879e1bbcded97af9c61f1b77f5395daaf03
MichelML <michmoreau.l@gmail.com>
2017-11-30 10:13:24 -0500

    add loading table

--- src/components/loading/LoadingTableConnected.tsx
+++ src/components/loading/LoadingTableConnected.tsx
@@ -4,25 +4,25 @@ import { Loading } from './Loading';
 import * as _ from 'underscore';
 
 export class LoadingTableConnected extends LoadingConnected {
-    render() {
-        if (this.props.hide) {
-            return null;
-        }
-
-        return (
-            <table className='mod-collapsible-rows'>
-                {_.range(this.props.numberOfRows).map((num: number) => {
-                    return (
-                        <tbody key={`loading-row${num}`} className='loading-row'>
-                            <tr>
-                                <td colSpan={this.props.columnsPerRow}>
-                                    <Loading />
-                                </td>
-                            </tr>
-                        </tbody>
-                    );
-                })}
-            </table>
-        );
+  render() {
+    if (this.props.hide) {
+      return null;
     }
+
+    return (
+      <table className='mod-collapsible-rows'>
+        {_.range(this.props.numberOfRows).map((num: number) => {
+          return (
+            <tbody key={`loading-row${num}`} className='loading-row'>
+              <tr>
+                <td colSpan={this.props.columnsPerRow}>
+                  <Loading />
+                </td>
+              </tr>
+            </tbody>
+          );
+        })}
+      </table>
+    );
+  }
 }
