3fd7699f168dda5cab9604b4bc9ae0fd628e606d
MichelML <michmoreau.l@gmail.com>
2017-12-14 10:31:27 -0500

    add uts for prepend

---
+++ src/components/dropdownSearch/tests/DropdownPrepend.spec.tsx
@@ -0,0 +1,33 @@
+import { DropdownPrepend } from '../DropdownPrepend';
+import { mount, ReactWrapper } from 'enzyme';
+
+describe('<DropdownPrepend />', () => {
+    describe('render', () => {
+        it('should render without error with a prepend as a string', () => {
+            expect(() => {
+                mount(
+                    <DropdownPrepend prepend='i am a string' />,
+                    { attachTo: document.getElementById('App') },
+                );
+            }).not.toThrow();
+        });
+
+        it('should render without error with a prepend as a JSX.Element', () => {
+            expect(() => {
+                mount(
+                    <DropdownPrepend prepend={<div>Hello World</div>} />,
+                    { attachTo: document.getElementById('App') },
+                );
+            }).not.toThrow();
+        });
+
+        it('should render children inside it if any', () => {
+            expect(
+                mount(
+                    <DropdownPrepend prepend='i am a string'><div>Hello World</div></DropdownPrepend>,
+                    { attachTo: document.getElementById('App') },
+                ).find('div').find('div').text()
+            ).toBe('Hello World');
+        });
+    });
+});
