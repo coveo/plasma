00dcd9777e8ea6dc0e58ee7bfa069e6950f294d9
MichelML <michmoreau.l@gmail.com>
2017-12-15 18:22:00 -0500

    add uts for tablechildheader

--- src/components/tables/tests/TableChildLastUpdated.spec.tsx
+++ src/components/tables/tests/TableChildLastUpdated.spec.tsx
@@ -9,32 +9,32 @@ import { Store, Provider } from 'react-redux';
 import { ITableProps } from '../Table';
 
 describe('<TableChildLastUpdated />', () => {
-    let store: Store<IReactVaporState>;
+  let store: Store<IReactVaporState>;
 
-    beforeEach(() => {
-        store = TestUtils.buildStore();
+  beforeEach(() => {
+    store = TestUtils.buildStore();
+  });
+
+  afterEach(() => {
+    store.dispatch(clearState());
+  });
+
+  describe('render', () => {
+    const mountComponentWithProps = (props: ITableProps) => {
+      return mount(
+        <Provider store={store}>
+          <TableChildLastUpdated {...props} />
+        </Provider>,
+        { attachTo: document.getElementById('App') },
+      );
+    };
+
+    describe('render without error', () => {
+      it('should render without error if basic props are passed', () => {
+        expect(() => {
+          mountComponentWithProps(tablePropsMock);
+        }).not.toThrow();
+      });
     });
-
-    afterEach(() => {
-        store.dispatch(clearState());
-    });
-
-    describe('render', () => {
-        const mountComponentWithProps = (props: ITableProps) => {
-            return mount(
-                <Provider store={store}>
-                    <TableChildLastUpdated {...props} />
-                </Provider>,
-                { attachTo: document.getElementById('App') },
-            );
-        };
-
-        describe('render without error', () => {
-            it('should render without error if basic props are passed', () => {
-                expect(() => {
-                    mountComponentWithProps(tablePropsMock);
-                }).not.toThrow();
-            });
-        });
-    });
+  });
 });
