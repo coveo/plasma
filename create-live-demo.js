7a56c10077721bbe493f1fd25980a5031b378706
MichelML <michmoreau.l@gmail.com>
2018-01-01 16:29:18 -0500

    complete script

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,12 +1,18 @@
 const sh = require('shelljs')
 const branchName = sh.exec('git rev-parse --abbrev-ref HEAD').stdout.trim()
-console.log(`Creating live demo for branch: ${branchName}`)
 
 // when travis build is finished,
 // move dist to a folder with the name of the branch
 // add this folder to the vcs, commit, and push to gh-pages
 // and than remove this folder from the vcs, commit, and delete it
 
+console.log(`Cleaning gh-pages branch before creating live demo for branch: ${branchName}`)
+sh.exec(`git checkout gh-pages && git pull origin && rm -rf ${branchName}`)
+sh.exec(`git add . && git commit -m 'remove previous ${branchName} folder if it exists'`)
+sh.exec(`git push origin gh-pages && git checkout ${branchName}`)
+
+
+console.log(`Creating live demo for branch: ${branchName}`)
 sh.cp('-R', 'docs', branchName)
 sh.exec(`git add ${branchName}`)
 sh.exec(`git commit -m 'create live demo for ${branchName}' --no-verify`)
