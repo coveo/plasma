3af429a247226f81a5329b1e63c73398a1988b13
MichelML <michmoreau.l@gmail.com>
2018-01-01 16:45:02 -0500

    push changes to branch

--- create-live-demo.js
+++ create-live-demo.js
@@ -18,7 +18,7 @@ sh.exec(`git add ${branchName}`)
 sh.exec(`git commit -m 'create live demo for ${branchName}' --no-verify`)
 sh.exec(`git push -f origin ${branchName}:gh-pages`)
 sh.rm('-rf', branchName)
-sh.exec(`git add . && git commit -m 'remove demo repo from vcs' --no-verify`)
+sh.exec(`git add . && git commit -m 'remove demo repo from vcs' --no-verify && git push origin ${branchName}`)
 
 console.log('Live demo creation completed.')
 console.log(`If it succeeded, the demo url should be https://coveo.github.io/react-vapor/${branchName}/index.html`)
