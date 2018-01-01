13232ad98375fea1a5f7919bc789c775ff388563
MichelML <michmoreau.l@gmail.com>
2018-01-01 16:38:28 -0500

    remove demo repo from vcs

--- live-demos/ReactVaporStore.ts
+++
@@ -1,7 +0,0 @@
-import { IReactVaporExampleState, Reducers } from './Reducers';
-import * as Redux from 'redux';
-import thunk from 'redux-thunk';
-
-
-export const ReactVaporStore: Redux.Store<IReactVaporExampleState> =
-  Redux.createStore<IReactVaporExampleState>(Reducers, (<any>window).devToolsExtension && (<any>window).devToolsExtension.call(), Redux.applyMiddleware(thunk));
