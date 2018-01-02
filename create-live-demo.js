9c64848a9fb8e4758f2e4a1df273508be712f803
MichelML <michmoreau.l@gmail.com>
2018-01-02 12:05:48 -0500

    use authenticated origin

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,13 +1,16 @@
 const sh = require('shelljs')
-const branchName = sh.exec('git rev-parse --abbrev-ref HEAD').stdout.trim()
+const argv = require('args').argv
+const branchName = argv.currentbranch
+const userpassword = argv.userpassword
+const originWithAuthentication = `https://${userpassword}@github.com/coveo/react-vapor.git`
 
 console.log(`Creating live demo for branch: ${branchName}`)
 sh.cp('-R', 'docs', branchName)
 sh.exec(`git add ${branchName}`)
 sh.exec(`git commit -m 'create live demo for ${branchName}' --no-verify`)
-sh.exec(`git push -f origin ${branchName}:gh-pages`)
+sh.exec(`git push -f ${originWithAuthentication} ${branchName}:gh-pages`)
 sh.rm('-rf', branchName)
-sh.exec(`git add . && git commit -m 'remove demo repo from vcs' --no-verify && git push origin ${branchName}`)
+sh.exec(`git add . && git commit -m 'remove demo repo from vcs' --no-verify && git push ${originWithAuthentication} ${branchName}`)
 
 console.log('Live demo creation completed.')
 console.log(`If it succeeded, the demo url should be https://coveo.github.io/react-vapor/${branchName}/index.html`)
