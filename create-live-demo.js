7df5597f12562054ae5704af1e51aaa66dd20359
MichelML <michmoreau.l@gmail.com>
2018-01-02 11:25:54 -0500

    try a new demo strategy

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,12 +1,6 @@
 const sh = require('shelljs')
 const branchName = sh.exec('git rev-parse --abbrev-ref HEAD').stdout.trim()
 
-console.log(`Cleaning gh-pages branch before creating live demo for branch: ${branchName}`)
-sh.exec(`git checkout gh-pages && git pull origin && rm -rf ${branchName}`)
-sh.exec(`git add . && git commit -m 'remove previous ${branchName} folder if it exists' --no-verify`)
-sh.exec(`git push origin gh-pages && git checkout ${branchName}`)
-
-
 console.log(`Creating live demo for branch: ${branchName}`)
 sh.cp('-R', 'docs', branchName)
 sh.exec(`git add ${branchName}`)
