c4f536503b257c9f7db12acc3acc418e62f94683
MichelML <michmoreau.l@gmail.com>
2018-01-04 18:06:10 -0500

    use full path for js file

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,11 +1,16 @@
 const sh = require('shelljs')
-const argv = require('yargs').argv
+const replace = require('replace-in-file');
 const branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH // travis provides this env variable
 const userpassword = process.env.GITUSRPWD
 const originWithAuthentication = `https://${userpassword}@github.com/coveo/react-vapor.git`
 
 console.log(`Creating live demo for branch: ${branchName}`)
 sh.cp('-R', 'docs', branchName)
+replace.sync({
+    files: `${branchName}/index.html`,
+    from: 'assets/bundle.js',
+    to: `https://raw.githubusercontent.com/coveo/react-vapor/gh-pages/${branchName}/assets/bundle.js`,
+});
 sh.exec(`git add ${branchName}`)
 sh.exec(`git commit -m 'create live demo for ${branchName}' --no-verify`)
 
