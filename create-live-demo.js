bcfcded27a46e94ee33ca5e33d3de3a359f9ba6d
MichelML <michmoreau.l@gmail.com>
2018-01-04 17:05:51 -0500

    remove yargs and use env variables

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,7 +1,7 @@
 const sh = require('shelljs')
 const argv = require('yargs').argv
-const branchName = argv.currentbranch
-const userpassword = argv.userpassword
+const branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH // travis provides this env variable
+const userpassword = process.env.GITUSRPWD
 const originWithAuthentication = `https://${userpassword}@github.com/coveo/react-vapor.git`
 
 console.log(`Creating live demo for branch: ${branchName}`)
@@ -9,8 +9,6 @@ sh.cp('-R', 'docs', branchName)
 sh.exec(`git add ${branchName}`)
 sh.exec(`git commit -m 'create live demo for ${branchName}' --no-verify`)
 sh.exec(`git push -f ${originWithAuthentication} ${branchName}:gh-pages`)
-sh.rm('-rf', branchName)
-sh.exec(`git add . && git commit -m 'remove demo repo from vcs' --no-verify && git push ${originWithAuthentication} ${branchName}`)
 
 console.log('Live demo creation completed.')
 console.log(`If it succeeded, the demo url should be https://coveo.github.io/react-vapor/${branchName}/index.html`)
