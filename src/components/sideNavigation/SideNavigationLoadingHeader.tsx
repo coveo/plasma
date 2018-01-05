e7f254181ea701a17cd9b18f5c79867e6cddae65
fguerreiro <francisco.guerreiro.jr@gmail.com>
2018-01-05 16:06:20 -0500

    Fixing: self-closing divs

--- src/components/sideNavigation/SideNavigationLoadingHeader.tsx
+++ src/components/sideNavigation/SideNavigationLoadingHeader.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 export const SideNavigationLoadingHeader = () =>
   <header className='navigation-menu-section-header'>
-    <div className='navigation-loading-item-light-grey mod-navigation-loading-bullet navigation-menu-section-header-icon'></div>
-    <div className='navigation-loading-item-light-grey mod-width-40'></div>
+    <div className='navigation-loading-item-light-grey mod-navigation-loading-bullet navigation-menu-section-header-icon' />
+    <div className='navigation-loading-item-light-grey mod-width-40' />
   </header>;
