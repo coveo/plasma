295ec2b50b7eb29d255d2609a2f95d767a7a7f9e
MichelML <michmoreau.l@gmail.com>
2018-01-17 20:36:36 -0500

    live demo

---
+++ fix-live-demo-again-and-again/Index.tsx
@@ -0,0 +1,150 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { ReactVaporStore } from './ReactVaporStore';
+import { render as ReactDOMRender } from 'react-dom';
+import './style.scss';
+import 'coveo-styleguide/dist/css/CoveoStyleGuide.css';
+import { UserFeedbackExample } from '../src/components/userFeedback/examples/UserFeedbackExample';
+import { SyncFeedbackExample } from '../src/components/syncFeedback/examples/SyncFeedbackExample';
+import { MembersExample } from './members-example/MembersExample';
+import { LastUpdatedExamples } from '../src/components/lastUpdated/examples/LastUpdatedExamples';
+import { LastUpdatedConnectedExamples } from '../src/components/lastUpdated/examples/LastUpdatedConnectedExamples';
+import { LoadingExamples } from '../src/components/loading/LoadingExamples';
+import { FilterBoxExamples } from '../src/components/filterBox/examples/FilterBoxExamples';
+import { FilterBoxConnectedExamples } from '../src/components/filterBox/examples/FilterBoxConnectedExamples';
+import { FacetConnectedExamples } from '../src/components/facets/examples/FacetConnectedExamples';
+import { FacetExamples } from '../src/components/facets/examples/FacetExamples';
+import { ModalConnectedExamples } from '../src/components/modal/examples/ModalConnectedExamples';
+import { NavigationExamples } from '../src/components/navigation/examples/NavigationExamples';
+import { NavigationConnectedExamples } from '../src/components/navigation/examples/NavigationConnectedExamples';
+import { ActionBarExamples } from '../src/components/actions/examples/ActionBarExamples';
+import { ActionBarConnectedExamples } from '../src/components/actions/examples/ActionBarConnectedExamples';
+import { TableExamples } from '../src/components/tables/examples/TableExamples';
+import { TableRowExamples } from '../src/components/tables/examples/TableRowExamples';
+import { TableRowConnectedExamples } from '../src/components/tables/examples/TableRowConnectedExamples';
+import { TableHeaderExamples } from '../src/components/tables/examples/TableHeaderExamples';
+import { ItemFilterExamples } from '../src/components/actions/filters/examples/ItemFilterExamples';
+import { ItemFilterConnectedExamples } from '../src/components/actions/filters/examples/ItemFilterConnectedExamples';
+import { TableEmptyRowExamples } from '../src/components/tables/examples/TableEmptyRowExamples';
+import { OptionsCycleExamples } from '../src/components/optionsCycle/examples/OptionsCycleExamples';
+import { SubNavigationExamples } from '../src/components/subNavigation/examples/SubNavigationExamples';
+import { SubNavigationConnectedExamples } from '../src/components/subNavigation/examples/SubNavigationConnectedExamples';
+import { OptionsCycleConnectedExamples } from '../src/components/optionsCycle/examples/OptionsCycleConnectedExamples';
+import { CalendarConnectedExamples } from '../src/components/calendar/examples/CalendarConnectedExamples';
+import { DatePickerBoxExamples } from '../src/components/datePicker/examples/DatePickerBoxExamples';
+import { DatesSelectionExamples } from '../src/components/datePicker/examples/DatesSelectionExamples';
+import { DatesSelectionConnectedExamples } from '../src/components/datePicker/examples/DatesSelectionConnectedExamples';
+import { DatePickerBoxConnectedExamples } from '../src/components/datePicker/examples/DatePickerBoxConnectedExamples';
+import { DatePickerDropdownConnectedExamples } from '../src/components/datePicker/examples/DatePickerDropdownConnectedExamples';
+import { DatePickerDropdownConnectedSingleDateExamples } from '../src/components/datePicker/examples/DatePickerDropdownConnectedSingleDateExamples';
+import { ModalExamples } from '../src/components/modal/examples/ModalExamples';
+import { TabsExamples } from '../src/components/tab/examples/TabConnectedExample';
+import { ModalPromptExamples } from '../src/components/modalPrompt/exemples/ModalPromptExamples';
+import { MultilineInputExamples } from '../src/components/multilineInput/examples/MultilineInputExamples';
+import { TooltipExamples } from '../src/components/tooltip/examples/TooltipExamples';
+import { ButtonExamples } from '../src/components/button/examples/ButtonExamples';
+import { BlankSlateExample } from '../src/components/blankSlate/examples/BlankSlateExample';
+import { ChosenSelectExamples } from '../src/components/chosen/examples/ChosenSelectExamples';
+import { DropdownSearchExamples } from '../src/components/dropdownSearch/examples/DropdownSearchExamples';
+import { ToastExamples } from '../src/components/toast/examples/ToastExamples';
+import { ToastConnectedExamples } from '../src/components/toast/examples/ToastConnectedExamples';
+import { RadioExamples } from '../src/components/radio/examples/RadioExamples';
+import { ChildFormExamples } from '../src/components/childForm/examples/ChildFormExamples';
+import { CheckboxExamples } from '../src/components/checkbox/examples/CheckboxExamples';
+import { ContentExamples } from '../src/components/content/examples/ContentExamples';
+import { ListBoxExamples } from '../src/components/listBox/examples/ListBoxExamples';
+import { ItemBoxExamples } from '../src/components/itemBox/examples/ItemBoxExamples';
+import { InputExamples } from '../src/components/input/examples/InputExamples';
+import { LinkSvgExamples } from '../src/components/svg/examples/LinkSvgExamples';
+import { SvgExamples } from '../src/components/svg/examples/SvgExamples';
+import { TitleExamples } from '../src/components/title/examples/TitleExamples';
+import { BreadcrumbsExamples } from '../src/components/breadcrumbs/examples/BreadcrumbsExamples';
+import { BasicHeaderExamples } from '../src/components/headers/examples/BasicHeaderExamples';
+import { BreadcrumbHeaderExample } from '../src/components/headers/examples/BreadcrumbHeaderExample';
+import { FlatSelectExamples } from '../src/components/flatSelect/examples/FlatSelectExamples';
+import { SliderExamples } from '../src/components/slider/examples/SliderExamples';
+import { SideNavigationLoadingExample } from '../src/components/sideNavigation/examples/SideNavigationLoadingExample';
+import { CheckboxConnectedExamples } from '../src/components/checkbox/examples/CheckboxConnectedExamples';
+
+class App extends React.Component<any, any> {
+
+  render() {
+    (window as any).store = ReactVaporStore;
+    return (
+      <Provider store={ReactVaporStore}>
+        <div className='coveo-form'>
+          <div className='form-group'>
+            <label className='form-control-label'>
+              My list of members
+            </label>
+            <div className='form-control'>
+              <MembersExample />
+            </div>
+          </div>
+          <SliderExamples />
+          <SvgExamples />
+          <LinkSvgExamples />
+          <TitleExamples />
+          <ContentExamples />
+          <ItemBoxExamples />
+          <ListBoxExamples />
+          <ButtonExamples />
+          <BreadcrumbsExamples />
+          <BasicHeaderExamples />
+          <BreadcrumbHeaderExample />
+          <DropdownSearchExamples />
+          <FlatSelectExamples />
+          <TooltipExamples />
+          <ChosenSelectExamples />
+          <UserFeedbackExample />
+          <SyncFeedbackExample />
+          <LastUpdatedExamples />
+          <LastUpdatedConnectedExamples />
+          <LoadingExamples />
+          <FilterBoxExamples />
+          <FilterBoxConnectedExamples />
+          <FacetExamples />
+          <FacetConnectedExamples />
+          <ModalExamples />
+          <ModalConnectedExamples />
+          <ModalPromptExamples />
+          <NavigationExamples />
+          <NavigationConnectedExamples />
+          <SubNavigationExamples />
+          <SubNavigationConnectedExamples />
+          <SideNavigationLoadingExample />
+          <TabsExamples />
+          <ActionBarExamples />
+          <ActionBarConnectedExamples />
+          <ItemFilterExamples />
+          <ItemFilterConnectedExamples />
+          <TableRowExamples />
+          <TableRowConnectedExamples />
+          <TableEmptyRowExamples />
+          <TableHeaderExamples />
+          <TableExamples />
+          <OptionsCycleExamples />
+          <OptionsCycleConnectedExamples />
+          <CalendarConnectedExamples />
+          <DatesSelectionExamples />
+          <DatesSelectionConnectedExamples />
+          <DatePickerBoxExamples />
+          <DatePickerBoxConnectedExamples />
+          <DatePickerDropdownConnectedExamples />
+          <DatePickerDropdownConnectedSingleDateExamples />
+          <MultilineInputExamples />
+          <BlankSlateExample />
+          <ToastExamples />
+          <ToastConnectedExamples />
+          <InputExamples />
+          <RadioExamples />
+          <CheckboxExamples />
+          <CheckboxConnectedExamples />
+          <ChildFormExamples />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+ReactDOMRender(<App />, document.getElementById('App'));
---
+++ fix-live-demo-again-and-again/Reducers.ts
@@ -0,0 +1,63 @@
+import { IMembersCompositeState, membersReducers } from './members-example/reducers/MembersReducers';
+import { actionBarsReducer } from '../src/components/actions/ActionBarReducers';
+import { itemFiltersReducer } from '../src/components/actions/filters/ItemFilterReducers';
+import { datePickersReducer } from '../src/components/datePicker/DatePickerReducers';
+import { dropdownsReducer } from '../src/components/dropdown/DropdownReducers';
+import { dropdownsSearchReducer } from '../src/components/dropdownSearch/DropdownSearchReducers';
+import { facetsReducer } from '../src/components/facets/FacetReducers';
+import { filterBoxesReducer } from '../src/components/filterBox/FilterBoxReducers';
+import { promptsReducer } from '../src/components/inlinePrompt/InlinePromptReducers';
+import { lastUpdatedCompositeReducer } from '../src/components/lastUpdated/LastUpdatedReducers';
+import { loadingsReducer } from '../src/components/loading/LoadingReducers';
+import { modalsReducer } from '../src/components/modal/ModalReducers';
+import { paginationCompositeReducer } from '../src/components/navigation/pagination/NavigationPaginationReducers';
+import { perPageCompositeReducer } from '../src/components/navigation/perPage/NavigationPerPageReducers';
+import { optionPickersReducer } from '../src/components/optionPicker/OptionPickerReducers';
+import { optionsCyclesReducer } from '../src/components/optionsCycle/OptionsCycleReducers';
+import { subNavigationsReducer } from '../src/components/subNavigation/SubNavigationReducers';
+import { tabGroupsReducer } from '../src/components/tab/TabReducers';
+import { tableHeaderCellsReducer } from '../src/components/tables/TableHeaderCellReducers';
+import { tableRowsReducer } from '../src/components/tables/TableRowReducers';
+import { toastsContainerReducer } from '../src/components/toast/ToastReducers';
+import { IReactVaporState } from '../src/ReactVapor';
+import * as Redux from 'redux';
+import { tablesReducer } from '../src/components/tables/TableReducers';
+import { flatSelectsReducer } from '../src/components/flatSelect/FlatSelectReducers';
+import { checkboxesReducer } from '../src/components/checkbox/CheckboxReducers';
+
+export interface IReactVaporExampleState extends IReactVaporState {
+  membersCompositeState: IMembersCompositeState;
+  lastAction: Redux.Action;
+}
+
+const lastAction = (state: IReactVaporExampleState = null, action: Redux.Action): Redux.Action => {
+  return action;
+};
+
+export const Reducers: Redux.Reducer<IReactVaporExampleState> = Redux.combineReducers<IReactVaporExampleState>({
+  tables: tablesReducer,
+  membersCompositeState: membersReducers,
+  lastUpdatedComposite: lastUpdatedCompositeReducer,
+  filters: filterBoxesReducer,
+  facets: facetsReducer,
+  perPageComposite: perPageCompositeReducer,
+  paginationComposite: paginationCompositeReducer,
+  loadings: loadingsReducer,
+  prompts: promptsReducer,
+  actionBars: actionBarsReducer,
+  dropdowns: dropdownsReducer,
+  dropdownSearch: dropdownsSearchReducer,
+  flatSelect: flatSelectsReducer,
+  rows: tableRowsReducer,
+  itemFilters: itemFiltersReducer,
+  optionsCycles: optionsCyclesReducer,
+  optionPickers: optionPickersReducer,
+  datePickers: datePickersReducer,
+  modals: modalsReducer,
+  subNavigations: subNavigationsReducer,
+  tabs: tabGroupsReducer,
+  toastContainers: toastsContainerReducer,
+  lastAction,
+  tableHeaderCells: tableHeaderCellsReducer,
+  checkboxes: checkboxesReducer,
+});
