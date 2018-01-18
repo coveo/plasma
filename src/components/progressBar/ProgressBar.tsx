1cbbb32673a8e1a6764c57f338cc43ca407b44cf
MichelML <michmoreau.l@gmail.com>
2018-01-18 16:52:35 -0500

    commit progress to make another change

---
+++ src/components/progressBar/ProgressBar.tsx
@@ -0,0 +1,16 @@
+// import * as React from 'react';
+// import {range} from 'underscore';
+
+// export interface IProgressBarProps {
+//     numberOfSteps: number;
+//     currentStep?: number;
+
+// }
+
+// export class ProgressBar extends React.Component<IProgressBarProps, {}> {
+//     render() {
+//         const stepWidth = Math.floor(100 / this.props.numberOfSteps);
+//         const allStep
+//         return range(this.props.numberOfSteps);
+//     }
+// };
