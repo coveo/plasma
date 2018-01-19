8fdd3397d6210c233ec2bdfd86db98874a0a0235
MichelML <michmoreau.l@gmail.com>
2018-01-19 12:53:50 -0500

    rename examples

--- src/components/stepProgressBar/examples/StepProgressBarExamples.tsx
+++ src/components/stepProgressBar/examples/StepProgressBarExamples.tsx
@@ -3,39 +3,39 @@ import { StepProgressBar } from '../StepProgressBar';
 
 export const StepProgressBarExamples = () => (
   <div className='mt2'>
-    <h1 className='text-blue mb1 bold'>StepProgressBar List</h1>
+    <h1 className='text-blue mb1 bold'>Step Progress Bar List</h1>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 3 steps (initial step)</label>
+      <label className='form-control-label'>Step Progress Bar 3 steps (initial step)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={3} currentStep={0} />
       </div>
     </div>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 5 steps (at step 3)</label>
+      <label className='form-control-label'>Step Progress Bar 5 steps (at step 3)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={5} currentStep={2} />
       </div>
     </div>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 5 steps (completed)</label>
+      <label className='form-control-label'>Step Progress Bar 5 steps (completed)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={5} currentStep={5} />
       </div>
     </div>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 10 steps (step 2)</label>
+      <label className='form-control-label'>Step Progress Bar 10 steps (step 2)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={10} currentStep={1} />
       </div>
     </div>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 10 steps (step 3)</label>
+      <label className='form-control-label'>Step Progress Bar 10 steps (step 3)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={10} currentStep={2} />
       </div>
     </div>
     <div className='form-group'>
-      <label className='form-control-label'>Progress Bar 10 steps (step 4)</label>
+      <label className='form-control-label'>Step Progress Bar 10 steps (step 4)</label>
       <div className='form-control'>
         <StepProgressBar numberOfSteps={10} currentStep={3} />
       </div>
