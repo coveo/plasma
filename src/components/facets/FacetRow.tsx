d2d39cb6c80b6f4127086d37329ef8581a6229af
Jean-Félix Thibodeau <thibodeaujf@gmail.com>
2018-03-07 17:36:32 -0500

    Fix checkbox facets

--- src/components/facets/FacetRow.tsx
+++ src/components/facets/FacetRow.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import { Checkbox } from '../checkbox/Checkbox';
-import { Label } from '../input/Label';
-import { Tooltip } from '../tooltip/Tooltip';
-import { IFacet } from './Facet';
+import {Tooltip} from '../tooltip/Tooltip';
+import {IFacet} from './Facet';
 
 export interface IFacetRowProps extends React.ClassAttributes<FacetRow> {
   facetRow: IFacet;
@@ -22,13 +20,17 @@ export class FacetRow extends React.Component<IFacetRowProps, any> {
 
     return (
       <li className='facet-value facet-selectable'>
-        <Checkbox
-          name={this.props.facetRow.name}
-          classes={['facet-value-label']}
-          checked={this.props.isChecked}
-          onClick={() => this.props.onToggleFacet(this.props.facetRow)}>
-          <Label classes={['label']}>{label}</Label>
-        </Checkbox>
+        <label className='coveo-checkbox-label facet-value-label'>
+          <input
+            type='checkbox'
+            name={this.props.facetRow.name}
+            className='coveo-checkbox facet-checkbox-input'
+            checked={this.props.isChecked}
+            onChange={() => this.props.onToggleFacet(this.props.facetRow)}
+          />
+          <button type='button'></button>
+          <span className='label'>{label}</span>
+        </label>
       </li>
     );
   }
--- src/components/facets/tests/FacetConnected.spec.tsx
+++ src/components/facets/tests/FacetConnected.spec.tsx
@@ -203,7 +203,7 @@ describe('Facets', () => {
       expect(onToggleFacet).not.toHaveBeenCalled();
 
       expect(facetRowInput.length).toBe(1);
-      facetRowInput.simulate('click');
+      facetRowInput.simulate('change');
 
       expect(onToggleFacet).toHaveBeenCalled();
     });
