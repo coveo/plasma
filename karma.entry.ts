47e345713c5adce9df50b3e27d051fcc1f2dc2cb
MichelML <michmoreau.l@gmail.com>
2018-03-14 17:00:33 -0400

    try without cache

--- karma.entry.ts
+++ karma.entry.ts
@@ -1,6 +1,5 @@
 import * as $ from 'jquery';
 import * as s from 'underscore.string';
-declare var __dirname: string;
 
 // Polyfill MouseEvent if needed
 ((window) => {
@@ -35,10 +34,10 @@ afterEach(() => {
   $('body > div:not(.jasmine_html-reporter)').remove();
 });
 
-const testsContext = require.context(`${__dirname}/src`, true, /\.spec\.ts(x?)$/);
+const testsContext = require.context('./src', true, /\.spec\.ts(x?)$/);
 testsContext.keys().forEach(testsContext);
 
-const coverageContext = require.context(`${__dirname}/src`, true, /.*\.ts(x?)$/);
+const coverageContext = require.context('./src', true, /.*\.ts(x?)$/);
 coverageContext.keys()
   .filter((file) => !s.contains(file, '.spec.') && !s.contains(file, 'Examples'))
   .forEach(coverageContext);
