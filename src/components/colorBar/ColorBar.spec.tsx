93dc37f7abf03cfe20960d9df8b9b9f2b2269bcd
MichelML <michmoreau.l@gmail.com>
2018-03-24 17:49:13 -0400

    refactor uts

--- src/components/colorBar/ColorBar.spec.tsx
+++ src/components/colorBar/ColorBar.spec.tsx
@@ -2,7 +2,7 @@ import {shallow} from 'enzyme';
 import * as React from 'react';
 import * as _ from 'underscore';
 import {ColorBar, IColorBarProps} from './ColorBar';
-import {noColorsPropsScenarios, withColorsPropsScenarios} from './ColorBarPropsScenarios.spec.ts';
+import {noColorsPropsScenarios, withColorsPropsScenarios} from './ColorBarPropsScenarios.spec';
 
 describe('ColorBar', () => {
     it('should render without error in different prop scenarios', () => {
@@ -13,9 +13,9 @@ describe('ColorBar', () => {
 
     it('should render a div with only a transparent color with width 100% if no colors are passed', () => {
         noColorsPropsScenarios.forEach((props) => {
-            const colorBars = shallow(<ColorBar widthPerColor={{blue: 100}} />).find('.color-bar-color');
+            const colorBars = shallow(<ColorBar {...props} />).find('.color-bar-color');
             expect(colorBars.length).toBe(1);
-            expect(colorBars.first().prop('style').backgroundColor).toBe('blue');
+            expect(colorBars.first().prop('style').backgroundColor).toBe('transparent');
             expect(colorBars.first().prop('style').width).toBe('100%');
         });
     });
@@ -25,11 +25,12 @@ describe('ColorBar', () => {
             const nonZeroNonTransparentColors = _.omit(props.widthPerColor, (width: number, color: string) => !width);
 
             shallow(<ColorBar {...props} />).find('.color-bar-color').forEach((color) => {
-                if (color.prop('style').backgroundColor !== 'transparent') {
-                    expect(nonZeroNonTransparentColors[color.first().prop('style').backgroundColor])
-                        .toBe(parseInt(color.prop('style').width, 10));
+                const {backgroundColor, width} = color.first().prop('style'); 
+
+                if (backgroundColor !== 'transparent') {
+                    expect(nonZeroNonTransparentColors[backgroundColor]).toBe(parseInt(width, 10));
                 } else {
-                    expect(color.prop('style').backgroundColor).toBe('transparent');
+                    expect(backgroundColor).toBe('transparent');
                 }
             });
         });
@@ -45,23 +46,26 @@ describe('ColorBar', () => {
         };
 
         shallow(<ColorBar {...doubledWidthProps} />).find('.color-bar-color').forEach((color) => {
-            expect(doubledWidthProps.widthPerColor[color.prop('style').backgroundColor] / 2)
-                .toBe(parseInt(color.prop('style').width, 10));
+            const {backgroundColor, width} = color.prop('style');
+            expect(doubledWidthProps.widthPerColor[backgroundColor] / 2).toBe(parseInt(width, 10));
         });
     });
 
     it('should have a 5px height by default', () => {
-        expect(shallow(<ColorBar widthPerColor={{blue: 10}} />).find('.color-bar-color').first().prop('style').height)
-            .toBe('5px');
+        const {height} = shallow(<ColorBar widthPerColor={{blue: 10}} />).find('.color-bar-color').first().prop('style');
+
+        expect(height).toBe('5px');
     });
 
     it('should change the default height if height is passed as prop', () => {
-        expect(shallow(<ColorBar widthPerColor={{blue: 10}} height='10px' />).find('.color-bar-color').first().prop('style').height)
-            .toBe('10px');
+        const {height} = shallow(<ColorBar widthPerColor={{blue: 10}} height='10px' />).find('.color-bar-color').first().prop('style');
+
+        expect(height).toBe('10px');
     });
 
     it('should pass extra classes to container if passed as prop', () => {
-        expect(shallow(<ColorBar widthPerColor={{blue: 10}} className='extra-class' />).find('div').first().prop('className'))
-            .toContain('extra-class');
+        const colorBar = shallow(<ColorBar widthPerColor={{blue: 10}} className='extra-class' />).find('div').first();
+
+        expect(colorBar.prop('className')).toContain('extra-class');
     });
 });
