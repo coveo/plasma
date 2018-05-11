e964b4ec767cb43571e16e943e41bf1d400840c1
Gaël Dostie <gdostie@coveo.com>
2018-05-11 16:45:09 -0400

    + Add callIfDefined util + unit tests

--- src/utils/FalsyValuesUtils.spec.tsx
+++ src/utils/FalsyValuesUtils.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {convertUndefinedAndNullToEmptyString} from './FalsyValuesUtils';
+import {callIfDefined, convertUndefinedAndNullToEmptyString} from './FalsyValuesUtils';
 
 describe('FalsyValuesUtils', () => {
     it('should return empty string if the value is undefined', () => {
@@ -14,5 +14,43 @@ describe('FalsyValuesUtils', () => {
         [1000, 'non empty string', '', <div>testing a jsx element</div>].forEach((value: any) => {
             expect(convertUndefinedAndNullToEmptyString(value)).toBe(value);
         });
+    });
+});
+
+describe('callIfDefined', () => {
+    let callbackSpy: jasmine.Spy;
+
+    beforeEach(() => {
+        callbackSpy = jasmine.createSpy('callback');
+    });
+
+    it('should call the callback when it is a defined function', () => {
+        callIfDefined(callbackSpy);
+
+        expect(callbackSpy).toHaveBeenCalled();
+    });
+
+    it('should call the callback and pass down args properly', () => {
+        const args = [
+            'some'
+            ['args'],
+            -10,
+            true,
+            () => 'ooookay',
+        ];
+
+        callIfDefined(callbackSpy, ...args);
+
+        expect(callbackSpy).toHaveBeenCalledWith(...args);
+    });
+
+    it('should not throw errors when calling with undefined values', () => {
+        const someDeclaredButNotAssignedCallback: () => any = undefined;
+
+        expect(() => callIfDefined(undefined)).not.toThrow();
+        expect(() => callIfDefined(null)).not.toThrow();
+        expect(() => callIfDefined('')).not.toThrow();
+        expect(() => callIfDefined('askjdh')).not.toThrow();
+        expect(() => callIfDefined(someDeclaredButNotAssignedCallback)).not.toThrow();
     });
 });
