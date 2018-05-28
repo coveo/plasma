83364b336c807544ade69ef7bc8cec8ba0ba382d
MichelML <michmoreau.l@gmail.com>
2018-05-28 09:16:49 -0400

    rename to BorderedLine

--- src/components/borderedRow/examples/BorderedLineExamples.tsx
+++ src/components/borderedRow/examples/BorderedLineExamples.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import {BorderedRow} from '../BorderedRow';
+import {BorderedLine} from '../BorderedLine';
 
-export const BorderedRowExamples = () =>
+export const BorderedLineExamples = () =>
     <div className='mt2'>
-        <h1 className='text-blue mb1 bold'>BorderedRow List</h1>
+        <h1 className='text-blue mb1 bold'>BorderedLine List</h1>
         <div className='form-group'>
             <label className='form-control-label'>Bordered Row</label>
             <div className='form-control'>
-                <BorderedRow className='full-content-x'>
+                <BorderedLine className='full-content-x'>
                     I am a bordered row and you can fill me with whatever you want!
-                </BorderedRow>
+                </BorderedLine>
             </div>
         </div>
     </div>;
