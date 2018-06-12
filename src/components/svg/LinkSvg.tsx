9e99f96a78d5c05f472e4fcb68926e7c136eba92
MichelML <michmoreau.l@gmail.com>
2018-06-12 14:58:23 -0400

    apply po review

--- src/components/svg/LinkSvg.tsx
+++ src/components/svg/LinkSvg.tsx
@@ -1,16 +1,18 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
+import * as _ from 'underscore';
 import {ITooltipProps, Tooltip} from '../tooltip/Tooltip';
 import {ISvgProps, Svg} from './Svg';
-
-export interface ILinkSvgProps extends React.ClassAttributes<LinkSvg> {
+import {keys} from 'ts-transformer-keys';
+export interface ILinkSvgPropsWithoutHTMLProps extends React.ClassAttributes<LinkSvg> {
     url?: string;
     target?: string;
     linkClasses?: string[];
     svg?: ISvgProps;
     tooltip?: ITooltipProps;
-    onClick?: (e?: React.MouseEvent<HTMLAnchorElement>) => any;
 }
+
+export interface ILinkSvgProps extends React.HTMLProps<HTMLAnchorElement> {}
 
 export class LinkSvg extends React.Component<ILinkSvgProps, {}> {
     static defaultProps: Partial<ILinkSvgProps> = {
@@ -23,7 +25,12 @@ export class LinkSvg extends React.Component<ILinkSvgProps, {}> {
         const classes = classNames(this.props.linkClasses);
         const href = this.props.url ? {href: this.props.url} : null;
         return (
-            <a {...href} target={this.props.target} className={classes} onClick={(e) => this.props.onClick(e)}>
+            <a
+                {..._.omit(this.props, keys<ILinkSvgPropsWithoutHTMLProps>())}
+                {...href}
+                target={this.props.target}
+                className={classes}
+            >
                 <Tooltip {...this.props.tooltip}>
                     <Svg {...this.props.svg} />
                 </Tooltip>
