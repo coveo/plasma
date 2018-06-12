3828513cdd8f5b9c23b8f14a876c56845f5299d3
MichelML <michmoreau.l@gmail.com>
2018-06-12 16:45:25 -0400

    revert unneeded changes

--- src/components/svg/LinkSvg.tsx
+++ src/components/svg/LinkSvg.tsx
@@ -2,6 +2,7 @@ import * as classNames from 'classnames';
 import * as React from 'react';
 import {ITooltipProps, Tooltip} from '../tooltip/Tooltip';
 import {ISvgProps, Svg} from './Svg';
+
 export interface ILinkSvgProps extends React.ClassAttributes<LinkSvg> {
     url?: string;
     target?: string;
@@ -20,16 +21,10 @@ export class LinkSvg extends React.Component<ILinkSvgProps, {}> {
     render() {
         const classes = classNames(this.props.linkClasses);
         const href = this.props.url ? {href: this.props.url} : null;
-        return (
-            <a
-                {...href}
-                target={this.props.target}
-                className={classes}
-            >
-                <Tooltip {...this.props.tooltip}>
-                    <Svg {...this.props.svg} />
-                </Tooltip>
-            </a>
-        );
+        return (<a {...href} target={this.props.target} className={classes}>
+            <Tooltip {...this.props.tooltip}>
+                <Svg {...this.props.svg} />
+            </Tooltip>
+        </a>);
     }
 }
