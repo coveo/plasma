67af841e954d86e45630da5aaf3439414985992a
MichelML <michmoreau.l@gmail.com>
2018-07-30 15:29:51 -0400

    add final fixes

--- webpack.config.js
+++ webpack.config.js
@@ -40,16 +40,6 @@ module.exports = {
             },
             {
                 /**
-                 *  Transform let and const to var in the files below to make them ES5 compatible
-                 *  A simple/global fix with ts-loader or awesome-typescript-loader was not found for this issue
-                 **/
-                include: [
-                    path.resolve(__dirname, 'node_modules/unidiff/hunk.js'),
-                ],
-                use: [{loader: 'babel-loader', options: {plugins: ['transform-es2015-block-scoping']}}],
-            },
-            {
-                /**
                  *  Transform let and const to var in js files below to make them ES5 compatible
                  *  Target only problematic files to prevent compilation from hanging 
                  **/
