f982963449bbb8e1ffcc301834ac42a5fc36b2f8
MichelML <michmoreau.l@gmail.com>
2018-09-06 18:48:02 -0400

    use axios

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,5 +1,5 @@
 const sh = require('shelljs');
-const $ = require('jquery');
+const axios = require('axios');
 const branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH; // travis provides this env variable
 const prNumber = process.env.TRAVIS_PULL_REQUEST;
 const userpassword = process.env.GITUSRPWD;
@@ -20,9 +20,9 @@ const currentCommit = sh.exec('git show --oneline -s').stdout.trim().split(' ')[
 sh.exec(`git push -f ${originWithAuthentication} ${currentCommit}:gh-pages`);
 
 console.log('Posting demo in GitHub Pull Request...\n');
-$.post(APIEndpointWithAuthentication, {event: 'COMMENT', body: livedemoMessage})
-    .done(() => console.log('Demo URL successfully posted in GitHub Pull Request'))
-    .fail((e) => {
+axios.post(APIEndpointWithAuthentication, {event: 'COMMENT', body: livedemoMessage})
+    .then(() => console.log('Demo URL successfully posted in GitHub Pull Request'))
+    .catch((e) => {
         console.log('Something went wrong while posting demo url in GitHub Pull Request. See error below for details:\n');
         console.log(e);
     });
