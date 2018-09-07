4ea9e9e6297b05891c83562b11a4d66fbe27cd38
MichelML <michmoreau.l@gmail.com>
2018-09-07 11:29:04 -0400

    post live demo only once in PRs

--- create-live-demo.js
+++ create-live-demo.js
@@ -1,5 +1,6 @@
 const sh = require('shelljs');
 const axios = require('axios');
+const _ = require('underscore');
 const branchName = process.env.TRAVIS_PULL_REQUEST_BRANCH; // travis provides this env variable
 const prNumber = process.env.TRAVIS_PULL_REQUEST;
 const userpassword = process.env.GITUSRPWD;
@@ -20,13 +21,20 @@ const currentCommit = sh.exec('git show --oneline -s').stdout.trim().split(' ')[
 sh.exec(`git push -f ${originWithAuthentication} ${currentCommit}:gh-pages`);
 
 console.log('Posting demo in GitHub Pull Request...\n');
-axios.post(APIEndpointWithAuthentication, {event: 'COMMENT', body: livedemoMessage})
-    .then(() => {
-        console.log('Demo URL successfully posted in GitHub Pull Request');
-        process.exit();
-    })
-    .catch((e) => {
-        console.log('Something went wrong while posting demo url in GitHub Pull Request. See error below for details:\n');
-        console.log(e);
-        process.exit();
-    });
+const handleError = (e) => {
+    console.log('Something went wrong while posting Demo URL in GitHub Pull Request. See error below for details:\n');
+    console.log(e);
+    process.exit(); 
+}
+axios.get(APIEndpointWithAuthentication)
+    .then((response) => _.chain(response).pluck('body').contains(livedemoMessage).value()
+        ? process.exit()
+        : axios.post(APIEndpointWithAuthentication, {event: 'COMMENT', body: livedemoMessage})
+            .then(() => {
+                console.log('Demo URL successfully posted in GitHub Pull Request');
+                process.exit();
+            })
+            .catch(handleError)
+    )
+    .catch(handleError);
+
