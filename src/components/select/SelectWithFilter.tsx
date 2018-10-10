d1a863b932ee4b1aab6e89f55c06905cf668e479
Pierre-Olivier Chretien <pochretien@coveo.com>
2018-10-10 12:05:25 -0400

    update SelectWithFilter with a button

--- src/components/select/SelectWithFilter.tsx
+++ src/components/select/SelectWithFilter.tsx
@@ -1,14 +1,18 @@
+import * as classNames from 'classnames';
 import * as React from 'react';
 import {keys} from 'ts-transformer-keys';
 import * as _ from 'underscore';
 import {IReactVaporState} from '../../ReactVapor';
 import {ReduxConnect} from '../../utils/ReduxUtils';
+import {Button} from '../button/Button';
 import {FilterBoxConnected} from '../filterBox/FilterBoxConnected';
 import {IItemBoxProps} from '../itemBox/ItemBox';
+import {Svg} from '../svg/Svg';
 import {ISelectProps} from './SelectConnected';
 
 export interface ISelectWithFilterOwnProps {
     matchFilter?: (filterValue: string, item: IItemBoxProps) => boolean;
+    customValues?: boolean;
 }
 const SelectWithFilterPropsToOmit = keys<ISelectWithFilterOwnProps>();
 
@@ -43,16 +47,38 @@ export const selectWithFilter = (Component: (React.ComponentClass<ISelectWithFil
 
     @ReduxConnect(mapStateToProps)
     class WrappedComponent extends React.Component<ISelectWithFilterProps> {
+
+        private getButton(): React.ReactNode {
+            return this.props.customValues
+                ? (
+                    <Button classes={['p1', 'ml1']} onClick={() => this.handleOnClick()}>
+                        <Svg svgName={'add'} className='icon mod-lg mod-align-with-text' />
+                    </Button>
+                )
+                : null;
+        }
+
         render() {
+            const filterBoxContainerClassNames: string = classNames({
+                'mb2': !!this.props.children,
+            });
+
+            const filterBoxClassNames: string = classNames({
+                'flex flex-center': this.props.customValues,
+            });
+
             return (
                 <Component {..._.omit(this.props, SelectWithFilterPropsToOmit)}>
                     <FilterBoxConnected
                         id={this.props.id}
-                        containerClasses={[!!this.props.children ? 'mb2' : '']}
+                        containerClasses={[filterBoxContainerClassNames]}
                         onKeyDown={(this.props as any).onKeyDown}
                         onKeyUp={(this.props as any).onKeyUp}
+                        className={filterBoxClassNames}
                         isAutoFocus
-                    />
+                    >
+                        {this.getButton()}
+                    </FilterBoxConnected>
                     {this.props.children}
                 </Component>
             );
