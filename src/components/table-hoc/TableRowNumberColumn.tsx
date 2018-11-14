621251f96b640bf8d089a2639ce24fbd85a5a9a5
MichelML <michmoreau.l@gmail.com>
2018-11-14 10:06:16 -0500

    use lynch for text number

--- src/components/table-hoc/TableRowNumberColumn.tsx
+++ src/components/table-hoc/TableRowNumberColumn.tsx
@@ -4,7 +4,7 @@ import * as styles from './styles/TableRowNumber.scss';
 
 export const TableRowNumberColumn = (props: {number: string | number}) => (
     <td key='table-row-number' className={styles.tableNumberedRow}>
-        <div className={classNames('bg-grey-1 mod-border-right text-medium-blue flex flex-column flex-center center', styles.tableNumberedRowContainer)}>
+        <div className={classNames('bg-grey-1 mod-border-right text-lynch flex flex-column flex-center center', styles.tableNumberedRowContainer)}>
             {props.number}
         </div>
     </td>
