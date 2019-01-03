ab5214db537b3e34a81b6026e965640fcdbc1c8e
Germain Bergeron <gbergeron@coveo.com>
2019-01-03 12:04:13 -0500

    Fix export corrupting the d.ts

--- src/utils/HocUtils.ts
+++ src/utils/HocUtils.ts
@@ -1,12 +1,7 @@
 import * as _ from 'underscore';
 
-type ConfigSupplier<C = {}> = C | ((...args: any[]) => C);
+export type ConfigSupplier<C = {}> = C | ((...args: any[]) => C);
 
-const HocUtils = {
+export const HocUtils = {
     supplyConfig: <C>(supplier: ConfigSupplier<C>): C => _.isFunction(supplier) ? supplier() : supplier,
-};
-
-export {
-    ConfigSupplier,
-    HocUtils,
 };
