0c624e735beb1260e0000630485cd795d62a168e
MichelML <michmoreau.l@gmail.com>
2019-01-31 13:05:18 -0500

    add onItemClick prop for actionable items

--- src/components/actionable-item/ActionableItem.tsx
+++ src/components/actionable-item/ActionableItem.tsx
@@ -1,25 +1,36 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
+import * as _ from 'underscore';
+import {callIfDefined} from '../../utils/FalsyValuesUtils';
 import {IItemBoxProps} from '../itemBox/ItemBox';
 import {ListBox} from '../listBox/ListBox';
 import {PopoverConnected} from '../popover/PopoverConnected';
 import {Svg} from '../svg/Svg';
 import {actionableItemContainer, actionableItemContent, actionableItemDots, actionableItemDotsWidthWithPadding} from './styles/ActionableItem.scss';
 
-export interface IActionableItemProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface IActionableItemProps {
     id: string;
+    onItemClick?: (e?: React.MouseEvent<HTMLDivElement>) => any;
     actions?: IItemBoxProps[];
 }
 
-export class ActionableItem extends React.Component<any> {
+export class ActionableItem extends React.Component<IActionableItemProps & React.HTMLAttributes<HTMLDivElement>> {
     static defaultProps: Partial<IActionableItemProps> = {
         actions: [],
     };
 
     render() {
         return (
-            <div {...this.props}>
-                <div className={classNames('actionable-item-content inline-block text-medium-blue border-color-medium-grey mod-border bg-pure-white', actionableItemContent, actionableItemContainer)}>
+            <div {..._.omit(this.props, 'actions', 'onItemClick')}>
+                <div
+                    className={classNames(
+                        {'cursor-pointer': !!this.props.onItemClick},
+                        'actionable-item-content inline-block text-medium-blue border-color-medium-grey mod-border bg-pure-white',
+                        actionableItemContent,
+                        actionableItemContainer,
+                    )}
+                    onClick={(e: React.MouseEvent<HTMLDivElement>) => callIfDefined(this.props.onItemClick, e)}
+                >
                     {this.props.children}
                 </div>
                 {
