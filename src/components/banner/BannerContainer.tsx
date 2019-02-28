789eedf7fa849191dc80045d822de0d5f215541f
Germain Bergeron <gbergeron@coveo.com>
2019-02-28 15:26:14 -0500

    Remove useless type

--- src/components/banner/BannerContainer.tsx
+++ src/components/banner/BannerContainer.tsx
@@ -4,7 +4,7 @@ import * as _ from 'underscore';
 
 import * as styles from './styles/Banner.scss';
 
-export const BannerContainer: React.FunctionComponent<React.HTMLProps<HTMLDivElement>> = (props: React.HTMLProps<HTMLDivElement>) => (
+export const BannerContainer: React.FunctionComponent<React.HTMLProps<HTMLDivElement>> = (props) => (
     <div
         {..._.omit(props, 'children')}
         className={classNames(props.className, styles.banner)}
