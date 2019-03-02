804528a3cca3fdd42c3c26b652a741231618648f
Pierre-Olivier Chretien <moipeter10@gmail.com>
2019-03-01 20:17:39 -0500

    change variables name

--- src/utils/react/PropsToOmitUtils.ts
+++ src/utils/react/PropsToOmitUtils.ts
@@ -1,8 +1,8 @@
-const childPropsToOmit: string[] = ['children', 'dangerouslySetInnerHTML'];
+const internalComponentPropsToOmit: string [] = ['children', 'dangerouslySetInnerHTML'];
 
-const inputTagPropsToOmit: string[] = [...childPropsToOmit, 'defaultValue', 'onClick', 'onChange', 'onBlur', 'value', 'valid'];
+const inputTagPropsToOmit: string[] = [...internalComponentPropsToOmit, 'defaultValue', 'onClick', 'onChange', 'onBlur', 'value', 'valid'];
 
 export const PropsToOmitUtils = {
-    child: childPropsToOmit,
+    internal: internalComponentPropsToOmit,
     input: inputTagPropsToOmit,
 };
