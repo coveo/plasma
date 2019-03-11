e5cd3d9f66d0db9725d951f6723966dd002c96da
Germain Bergeron <gbergeron@coveo.com>
2019-03-11 11:27:21 -0400

    Fix OptionsCycleSelector returning the startAt instead of 0

--- src/components/optionsCycle/OptionsCycleSelectors.ts
+++ src/components/optionsCycle/OptionsCycleSelectors.ts
@@ -4,7 +4,7 @@ import {IReactVaporState} from '../../ReactVapor';
 
 const getCurrentOption = (state: IReactVaporState, props: {id: string, startAt?: number}): number => {
     const cycle = _.findWhere(state.optionsCycles, {id: props.id});
-    return (cycle && cycle.currentOption) || (props.startAt || 0);
+    return _.result(cycle, 'currentOption', props.startAt || 0);
 };
 
 export const OptionsCycleSelectors = {
