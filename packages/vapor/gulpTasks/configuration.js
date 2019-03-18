6815b1d7a57a9b94a0547a5526add7cba575a2e0
GermainBergeron <GermainBergeron@users.noreply.github.com>
2019-03-18 17:00:25 -0400

    Merge pull request #648 from coveo/fix-snyk-vulnerabilities-2
    Fix Snyk Vulnerabilities

--- packages/vapor/gulpTasks/configuration.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    autoprefixerOptions: {
-        browsers: ['cover 90%', 'last 1 versions', 'IE 11', 'not dead'],
-    },
-    gzipOptions: {
-        append: false,
-    },
-};
