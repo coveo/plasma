72376910388eda124c3f958889d2707f791502c5
GermainBergeron <GermainBergeron@users.noreply.github.com>
2019-03-19 11:38:05 -0400

    Merge pull request #649 from coveo/rotate-s3-keys
    Rotate S3 Deployment Keys

--- packages/vapor/publish-pages.sh
+++ packages/vapor/publish-pages.sh
@@ -4,8 +4,8 @@ set -x
 
 ( cd _gh_pages
   git init
-  git config user.name "pochretien"
-  git config user.email "pochretien@coveo.com"
+  git config user.name "Travis CI"
+  git config user.email "$COMMIT_AUTHOR_EMAIL"
   git add .
   git commit -m "Autodeploy to Github Pages"
   git push --force --quiet "git@github.com:coveo/vapor.git" master:gh-pages
