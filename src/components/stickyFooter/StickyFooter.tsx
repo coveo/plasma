59da911a7e1f988fe154aa193d6d9577d6c9f53f
Francois Lachance-Guillemette <francois.lguillemette@gmail.com>
2019-03-19 15:57:13 -0400

    Allow to use StickyFooter outside of the project

--- src/components/stickyFooter/StickyFooter.tsx
+++ src/components/stickyFooter/StickyFooter.tsx
@@ -1,17 +1,25 @@
 import * as classNames from 'classnames';
 import * as React from 'react';
 import * as styles from './StickyFooter.scss';
+import { IClassName } from 'src/utils/ClassNameUtils';
 
 export interface IStickyFooterProps {
-    className?: string;
+    id?: string;
+    className?: IClassName;
+    isOpened: boolean;
 }
 
 export class StickyFooter extends React.Component<IStickyFooterProps> {
     static ID = 'StickyFooter';
 
     render() {
+        const { id, className, isOpened } = this.props;
         return (
-            <div id={StickyFooter.ID} className={classNames(styles.stickyFooter, this.props.className)}>
+            <div id={id || StickyFooter.ID} className={
+                classNames({
+                    [styles.stickyFooterOpened]: isOpened,
+                    [styles.stickyFooter]: true
+                }, className)}>
                 {this.props.children}
             </div>
         );
--- src/components/stickyFooter/examples/StickyFooterExamples.tsx
+++ src/components/stickyFooter/examples/StickyFooterExamples.tsx
@@ -3,7 +3,6 @@ import * as loremIpsum from 'lorem-ipsum';
 import * as React from 'react';
 import {Button} from '../../button/Button';
 import {StickyFooter} from '../../stickyFooter/StickyFooter';
-import * as styles from '../../stickyFooter/StickyFooter.scss';
 
 const lorem = loremIpsum({count: 200});
 
@@ -21,7 +20,7 @@ export class StickyFooterExamples extends React.Component<{}, {isOpened: boolean
             <div className='mt2'>
                 <Button name='toggle footer' onClick={() => this.setState({isOpened: !this.state.isOpened})} />
                 <div className='mt2'>{lorem}</div>
-                <StickyFooter className={classNames('sticky-footer-mod-header', {[styles.stickyFooterOpened]: this.state.isOpened})}>
+                <StickyFooter className={'sticky-footer-mod-header'} isOpened={this.state.isOpened} >
                     <Button primary name='Save' />
                 </StickyFooter>
             </div>
