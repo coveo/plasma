9a8393f31c4d672845d3dd8c5b41616bb49ee6da
Germain Bergeron <gbergeron@coveo.com>
2019-03-20 11:40:46 -0400

    Try to fix the demo branches
    Added deploy step to deploy master on surge

--- create-live-demo.sh
+++ create-live-demo.sh
@@ -3,17 +3,18 @@
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+echo "Syncing with gh-pages from branch: $TRAVIS_PULL_REQUEST_BRANCH"
+git pull --no-edit --strategy-option ours "$SSH_REPO" gh-pages
+
 echo "Creating live demo for branch: $TRAVIS_PULL_REQUEST_BRANCH";
 cp -R docs "$TRAVIS_PULL_REQUEST_BRANCH"
 git add .
 git commit -m "live demo at https://coveo.github.io/react-vapor/$TRAVIS_PULL_REQUEST_BRANCH/" --no-verify
 
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
-
-echo "Syncing with gh-pages from branch: $TRAVIS_PULL_REQUEST_BRANCH"
-git pull --no-edit --strategy-option ours "$SSH_REPO" gh-pages
 
 echo "Pushing live demo to gh-pages for branch: $TRAVIS_PULL_REQUEST_BRANCH"
 git push -f "$SSH_REPO" "$SHA:gh-pages"
