6c71d78211e3a5377170390f6ede9d1f0baa323e
Gaël Dostie <gdostie@coveo.com>
2019-04-01 10:00:14 -0400

    Implement TextAreaSelectors with unit tests

---
+++ src/components/textarea/TestAreaSelectors.ts
@@ -0,0 +1,13 @@
+import * as _ from 'underscore';
+
+import {IReactVaporState} from '../../ReactVapor';
+import {ITextAreaState} from './TextAreaReducers';
+
+const getValue = (state: IReactVaporState, id: string): string => {
+    const textArea: ITextAreaState = _.findWhere(state.textAreas, {id});
+    return textArea && textArea.value || '';
+};
+
+export const TextAreaSelectors = {
+    getValue,
+};
