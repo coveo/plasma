309d871760a77e0135eb3a829ea22bce6c431a12
Germain Bergeron <gbergeron@coveo.com>
2019-04-23 10:06:34 -0400

    Add props for X and Y grid color
    Fix small issue with YGrid position

--- src/components/chart/XGrid.tsx
+++ src/components/chart/XGrid.tsx
@@ -4,15 +4,16 @@ import {XYChartContext} from './XYChart';
 
 export interface XGridProps {
     padding?: number;
+    color?: string;
 }
 
-export const XGrid: React.FunctionComponent<XGridProps> = ({padding = 0}) => {
+export const XGrid: React.FunctionComponent<XGridProps> = ({padding = 0, color = 'rgba(0,0,0,0.2)'}) => {
     const context = React.useContext(XYChartContext);
     const {xDomain, yDomain, xScale, yScale, xTicksCount} = context;
 
     const lines = xScale.ticks(xTicksCount).map((tick: number) => (
         xScale(tick) >= 0 && xScale(tick) <= xScale(xDomain[1])
-            ? <line stroke='rgba(0,0,0,0.2)' x1={xScale(tick)} x2={xScale(tick)} y1={yScale(yDomain[0]) + padding} y2={yScale(yDomain[1]) - padding} />
+            ? <line stroke={color} x1={xScale(tick)} x2={xScale(tick)} y1={yScale(yDomain[0]) + padding} y2={yScale(yDomain[1]) - padding} />
             : null
     ));
 
