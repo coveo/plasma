7a6cc7af98596670f7a8ed2cdad27efb2f3e199f
Pierre-Olivier Chretien <moipeter10@gmail.com>
2019-05-14 15:44:54 -0400

    format

--- src/components/childForm/ChildForm.tsx
+++ src/components/childForm/ChildForm.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import * as classNames from 'classnames';
+import * as React from 'react';
 import * as _ from 'underscore';
 import {ValidComponentChildren} from '../../utils/ValidComponentChildren';
 
