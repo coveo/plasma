fa55e8441d4ae2b867b37b05499359034cacc7d4
Olivier Taillon <otaillon@coveo.com>
2019-05-20 15:19:00 -0400

    Removed unused tooltip import

--- src/components/color/examples/ColorExamples.tsx
+++ src/components/color/examples/ColorExamples.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as _ from 'underscore';
-// import {Tooltip} from '../../tooltip/Tooltip';
 import {Color} from '../Color';
 
 export class ColorExamples extends React.Component {
@@ -8,36 +7,36 @@ export class ColorExamples extends React.Component {
         const VaporColors: {[key: string]: string} = require('!sass-variable-loader?preserveVariableNames!coveo-styleguide/scss/common/palette.scss');
         const colors = _.map(VaporColors, (value, color) => (
             (value === "'category'") ? (
-                <tr className = 'bg-grey-2'>
-                    <td className = 'p2 center align-middle mod-border-bottom bold' colSpan = {3}>{color.toUpperCase()}</td>
+                <tr className='bg-grey-2'>
+                    <td className='p2 center align-middle mod-border-bottom bold' colSpan={3}>{color.toUpperCase()}</td>
                 </tr>
             ) : (
-                <tr>
-                    <td className = 'p2 mod-border-bottom'>
-                        <Color
-                            key={color}
-                            color={color}
-                            className='p2 material card'
-                        />
-                    </td>
-                    <td className = 'p2 align-middle mod-border-bottom'>
-                        <p>{color}</p>
-                    </td>
-                    <td className = 'p2 align-middle mod-border-bottom'>
-                        {value.toUpperCase()}
-                    </td>
-                </tr>
+                    <tr>
+                        <td className='p2 mod-border-bottom'>
+                            <Color
+                                key={color}
+                                color={color}
+                                className='p2 material card'
+                            />
+                        </td>
+                        <td className='p2 align-middle mod-border-bottom'>
+                            <p>{color}</p>
+                        </td>
+                        <td className='p2 align-middle mod-border-bottom'>
+                            {value.toUpperCase()}
+                        </td>
+                    </tr>
                 )
         ));
         return (
             <div className='mt2'>
                 <h1 className='text-medium-blue mb1 bold'>Colors</h1>
-                <table className = 'mod-border'>
+                <table className='mod-border'>
                     <thead>
-                        <tr  className = 'left-align bold mod-border-bottom'>
-                            <th className = 'p2 mod-border-bottom border-color-darker-blue'>Color</th>
-                            <th className = 'p2 mod-border-bottom border-color-darker-blue'>Name</th>
-                            <th className = 'p2 mod-border-bottom border-color-darker-blue'>Value</th>
+                        <tr className='left-align bold mod-border-bottom'>
+                            <th className='p2 mod-border-bottom border-color-darker-blue'>Color</th>
+                            <th className='p2 mod-border-bottom border-color-darker-blue'>Name</th>
+                            <th className='p2 mod-border-bottom border-color-darker-blue'>Value</th>
                         </tr>
                     </thead>
                     <tbody>
