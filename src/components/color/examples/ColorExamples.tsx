3003a4dac0543fa5c081d6eaf6e4ec33c8ef11d3
Olivier Taillon <otaillon@coveo.com>
2019-05-21 08:50:09 -0400

    Improvements

--- src/components/color/examples/ColorExamples.tsx
+++ src/components/color/examples/ColorExamples.tsx
@@ -8,7 +8,7 @@ export class ColorExamples extends React.Component {
         const colors = _.map(VaporColors, (value, color) => (
             (value === "'category'") ? (
                 <tr className='bg-grey-2'>
-                    <td className='p2 center align-middle mod-border-bottom bold' colSpan={3}>{color.toUpperCase()}</td>
+                    <td className='p1 center align-middle mod-border-bottom bold' colSpan={3}>{color.toUpperCase()}</td>
                 </tr>
             ) : (
                     <tr>
@@ -32,13 +32,6 @@ export class ColorExamples extends React.Component {
             <div className='mt2'>
                 <h1 className='text-medium-blue mb1 bold'>Colors</h1>
                 <table className='mod-border'>
-                    <thead>
-                        <tr className='left-align bold mod-border-bottom'>
-                            <th className='p2 mod-border-bottom border-color-darker-blue'>Color</th>
-                            <th className='p2 mod-border-bottom border-color-darker-blue'>Name</th>
-                            <th className='p2 mod-border-bottom border-color-darker-blue'>Value</th>
-                        </tr>
-                    </thead>
                     <tbody>
                         {colors}
                     </tbody>
