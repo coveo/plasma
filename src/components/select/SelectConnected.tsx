dbeae25a1d3a1bd043ad165977871fb665ba8857
Nasreddine Anani <nanani@coveo.com>
2019-05-27 11:53:30 -0400

    cleaning code

--- src/components/select/SelectConnected.tsx
+++ src/components/select/SelectConnected.tsx
@@ -190,8 +190,12 @@ export class SelectConnected extends React.PureComponent<ISelectProps & ISelectS
 
         if (_.contains([keyCode.enter, keyCode.tab], e.keyCode) && this.props.isOpened) {
             const actives = _.chain(this.props.items)
-                .filter((item: IItemBoxProps) => !item.hidden && (!this.props.multi
-                    || !_.contains(this.props.selectedValues, item.value)) && !item.disabled).value();
+                .filter((item: IItemBoxProps) => !item.hidden
+                    && !this.props.multi
+                    && !item.disabled
+                    || !_.contains(this.props.selectedValues, item.value)
+                )
+                .value();
             const active = actives[mod(this.props.active, actives.length)];
             if (active) {
                 this.props.onSelectValue(active.value, this.props.multi);
