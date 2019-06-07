334e31c6e4a9b3dd89fb3c9e44e2c778af13d4e0
Travis CI <>
2019-06-07 18:56:36 +0000

    Clean old build

--- feature-replace-jeyll-with-nextjs/members-example/models/Member.ts
+++
@@ -1,9 +0,0 @@
-export interface IMemberAttributes {
-    email?: string;
-    sendEmail?: boolean;
-}
-
-export const defaultMemberAttributes: IMemberAttributes = {
-    email: '',
-    sendEmail: false,
-};
