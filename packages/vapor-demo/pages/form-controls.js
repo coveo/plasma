1c53d26c7d561c24559ec27a1b94bdbd54471184
Germain Bergeron <gbergeron@coveo.com>
2019-06-11 15:32:12 -0400

    Create missing navigation structure and template for pages
    Remove useless stuff in vapor package

---
+++ packages/vapor-demo/pages/form-controls.js
@@ -0,0 +1,20 @@
+import Component from '../components/Component';
+import Layout from '../layouts/Layout';
+import FilterPicker from '../components/filtering/FilterPicker';
+
+const FormControls = () => {
+    return (
+        <Layout>
+            <Component
+                id='filter-picker'
+                title="FilterPicker"
+                usage='This is the standard filter picker control.'
+                stylesheet="scss/controls/filter-picker.scss"
+            >
+                <FilterPicker />
+            </Component>
+        </Layout>
+    );
+};
+
+export default FormControls;
