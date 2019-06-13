4a5a5333b9bbbecfbc8d35eedf5e017dd2cb0365
Germain Bergeron <gbergeron@coveo.com>
2019-06-13 09:53:45 -0400

    Replace Typography and Utility with Next.js components

---
+++ packages/vapor-demo/components/utility/SpacedBoxes.js
@@ -0,0 +1,25 @@
+export default function SpacedBoxes() {
+    return (
+        <div className="spaced-boxes-container flex flex-wrap" style={{maxWidth: 400}}>
+            <span className="p2 bg-red text-pure-white spaced-box">
+                A span
+            </span>
+
+            <button type="button" className="btn spaced-box">
+                A button
+            </button>
+
+            <span className="p2 bg-light-blue text-pure-white spaced-box">
+                Another span
+            </span>
+
+            <button type="button" className="btn mod-primary spaced-box">
+                Another button
+            </button>
+
+            <span className="p2 bg-green text-pure-white spaced-box">
+                More!
+            </span>
+        </div>
+    );
+}
--- packages/vapor-demo/pages/typography.js
+++ packages/vapor-demo/pages/typography.js
@@ -1,17 +1,63 @@
 import Component from '../components/Component';
 import Layout from '../layouts/Layout';
 import Picker from '../components/filtering/FilterPicker';
+import Headings from '../components/typography/Headings';
+import Colors from '../components/typography/Colors';
+import Utilities from '../components/typography/Utilities';
+import Lists from '../components/typography/Lists';
+import LineHeight from '../components/typography/LineHeight';
 
 const Typography = () => {
     return (
         <Layout>
             <Component
-                id="filter-picker"
-                title="FilterPicker"
-                usage="This is the standard filter picker control."
-                stylesheet="scss/controls/filter-picker.scss"
+                id="headings"
+                title="Headings"
+                usage="All HTML headings, `h1` through `h6`, are available."
+                stylesheet="scss/typography/headings.scss"
+                withSource
             >
-                <Picker />
+                <Headings />
+            </Component>
+
+            <Component
+                id="colors"
+                title="Colors"
+                usage="Use this suite of color utilities for text colors."
+                stylesheet="scss/common/palette.scss"
+                withSource
+            >
+                <Colors />
+            </Component>
+
+            <Component
+                id="utilities"
+                title="Utilities"
+                usage="Change typographic weights, styles, and alignment with these utility classes."
+                stylesheet="scss/typography/utility.scss"
+                withSource
+            >
+                <Utilities />
+            </Component>
+
+            <Component
+                id="lists"
+                title="Lists"
+                usage="Apply styles to unordered and ordered lists"
+                stylesheet="scss/typography/lists.scss"
+                withSource
+            >
+                <Lists />
+            </Component>
+
+            <Component
+                id="line-height"
+                title="Line-height"
+                usage="Change line-height for headings and paragraph."
+                stylesheet="scss/typography/line-height.scss"
+                withSource
+            >
+                <LineHeight />
             </Component>
         </Layout>
     );
