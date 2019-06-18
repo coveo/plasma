190de342195433cf18d28dd8a054fcd27bfe069e
Danny Gautier <dgauthier@coveo.com>
2019-06-18 07:51:14 -0400

    Changed IReduxAction to IDispatch in FlatSelectConnected.tsx

--- packages/react-vapor/src/components/flatSelect/FlatSelectConnected.tsx
+++ packages/react-vapor/src/components/flatSelect/FlatSelectConnected.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import * as _ from 'underscore';
-import {IReactVaporState, IReduxActionsPayload} from '../../ReactVapor';
-import {IReduxAction, ReduxUtils} from '../../utils/ReduxUtils';
+import {IReactVaporState} from '../../ReactVapor';
+import {IDispatch, ReduxUtils} from '../../utils/ReduxUtils';
 import {FlatSelect, IFlatSelectDispatchProps, IFlatSelectOwnProps, IFlatSelectProps, IFlatSelectStateProps} from './FlatSelect';
 import {addFlatSelect, removeFlatSelect, selectFlatSelect} from './FlatSelectActions';
 import {IFlatSelectOptionProps} from './FlatSelectOption';
@@ -18,10 +18,7 @@ const mapStateToProps = (state: IReactVaporState, ownProps: IFlatSelectOwnProps)
     return {selectedOptionId: ownProps.options.length && ownProps.options[0].id};
 };
 
-const mapDispatchToProps = (
-    dispatch: (action: IReduxAction<IReduxActionsPayload>) => void,
-    ownProps: IFlatSelectOwnProps,
-): IFlatSelectDispatchProps => ({
+const mapDispatchToProps = (dispatch: IDispatch, ownProps: IFlatSelectOwnProps): IFlatSelectDispatchProps => ({
     onRender: () => dispatch(addFlatSelect(ownProps.id, ownProps.defaultSelectedOptionId || ownProps.options.length && ownProps.options[0].id)),
     onDestroy: () => dispatch(removeFlatSelect(ownProps.id)),
     onOptionClick: (selected: IFlatSelectOptionProps) => dispatch(selectFlatSelect(ownProps.id, selected.id)),
