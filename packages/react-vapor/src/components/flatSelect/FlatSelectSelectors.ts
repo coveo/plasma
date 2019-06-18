3d233b4cec7f69ed2bc3f8104576121b9142acd2
Danny Gautier <dgauthier@coveo.com>
2019-06-18 07:49:10 -0400

    feature - Added a Selector for flatSelect + unit test [ADUI-4495]

---
+++ packages/react-vapor/src/components/flatSelect/FlatSelectSelectors.ts
@@ -0,0 +1,16 @@
+import {createSelector} from 'reselect';
+import * as _ from 'underscore';
+
+import {IReactVaporState} from '../../ReactVapor';
+import {flatSelectInitialState, IFlatSelectState } from './FlatSelectReducers';
+
+const getInput = (state: IReactVaporState, props: {id: string}): IFlatSelectState => {
+    return _.findWhere(state.flatSelect, {id: props.id}) || flatSelectInitialState;
+};
+
+const getSelectedOptionId = createSelector(getInput, (flatSelect: IFlatSelectState) => flatSelect && flatSelect.selectedOptionId);
+
+export const FlatSelectSelectors = {
+    getInput,
+    getSelectedOptionId,
+};
---
+++ packages/react-vapor/src/components/flatSelect/tests/FlatSelectSelectors.spec.tsx
@@ -0,0 +1,39 @@
+import { flatSelectInitialState, IFlatSelectState } from '../FlatSelectReducers';
+import { FlatSelectSelectors } from '../FlatSelectSelectors';
+
+describe('FlatSelectSelectors', () => {
+    describe('getInput', () => {
+        it('Should return the default flatSelect state', () => {
+            const FlatSelect = FlatSelectSelectors.getInput({ flatSelect: [] }, { id: 'nothing is selected by the user' });
+            expect(FlatSelect).toEqual(flatSelectInitialState);
+        });
+
+        it('should return the selected FlatSelect', () => {
+            const expectedSelectedFlatSelect: IFlatSelectState = {
+                id: 'the-id',
+                selectedOptionId: 'the-selected-option-id',
+            };
+            const selectedFlatSelect = FlatSelectSelectors.getInput({ flatSelect: [expectedSelectedFlatSelect] }, { id: expectedSelectedFlatSelect.id });
+            expect(selectedFlatSelect).toEqual(expectedSelectedFlatSelect);
+        });
+    });
+});
+
+describe('getSelectedOptionId', () => {
+    it('should return the value of the flatSelect initial state if no other value is chosen', () => {
+        flatSelectInitialState.id = 'the-first-id';
+        flatSelectInitialState.selectedOptionId = 'the-first-value';
+
+        const selectedOptionId = FlatSelectSelectors.getSelectedOptionId({ flatSelect: [] }, { id: 'the-user-did-not-select-anything' });
+        expect(selectedOptionId).toEqual(flatSelectInitialState.selectedOptionId);
+    });
+
+    it('should return the flatSelected value from the state that the user chose', () => {
+        const expectedSelectedFlatSelect: IFlatSelectState = {
+            id: 'the-id',
+            selectedOptionId: 'the-selected-option-id',
+        };
+        const selectedFlatSelectOptionId = FlatSelectSelectors.getSelectedOptionId({ flatSelect: [expectedSelectedFlatSelect] }, { id: expectedSelectedFlatSelect.id });
+        expect(expectedSelectedFlatSelect.selectedOptionId).toEqual(selectedFlatSelectOptionId);
+    });
+});
