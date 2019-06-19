d5997aa8a353f65dcabf4032b9b0262d314226f6
pocketpapa <danny.m.gauthier@gmail.com>
2019-06-19 15:01:13 -0400

    manually passed the prettier formater on the files

--- packages/react-vapor/src/components/flatSelect/FlatSelectSelectors.ts
+++ packages/react-vapor/src/components/flatSelect/FlatSelectSelectors.ts
@@ -1,15 +1,17 @@
-import { createSelector } from 'reselect';
+import {createSelector} from 'reselect';
 import * as _ from 'underscore';
 
-import { IReactVaporState } from '../../ReactVapor';
-import { IFlatSelectState } from './FlatSelectReducers';
+import {IReactVaporState} from '../../ReactVapor';
+import {IFlatSelectState} from './FlatSelectReducers';
 
-const getInput = (state: IReactVaporState, ownProps: { id: string }): IFlatSelectState => {
-    return _.findWhere(state.flatSelect, { id: ownProps.id });
+const getInput = (state: IReactVaporState, ownProps: {id: string}): IFlatSelectState => {
+    return _.findWhere(state.flatSelect, {id: ownProps.id});
 };
 
 const getSelectedOptionId = createSelector(
-    getInput, (flatSelectState: IFlatSelectState): string => flatSelectState && flatSelectState.selectedOptionId);
+    getInput,
+    (flatSelectState: IFlatSelectState): string => flatSelectState && flatSelectState.selectedOptionId
+);
 
 export const FlatSelectSelectors = {
     getInput,
