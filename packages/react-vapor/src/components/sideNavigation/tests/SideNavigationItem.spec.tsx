fdb0c9bd2f31876cbf45c150556abcddafd46fe7
Gaël Dostie <gdostie@coveo.com>
2019-08-20 11:36:18 -0400

    Add unit tests to SideNavigation things

--- packages/react-vapor/src/components/sideNavigation/tests/SideNavigationItem.spec.tsx
+++ packages/react-vapor/src/components/sideNavigation/tests/SideNavigationItem.spec.tsx
@@ -1,11 +1,26 @@
-import {shallow} from 'enzyme';
+import {shallow, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
-import {SideNavigationItem} from '../SideNavigationItem';
 
-describe('<SideNavigationItem />', () => {
-    it('should render without errors', () => {
+import {SideNavigationItem, SideNavigationItemProps} from '../SideNavigationItem';
+
+describe('SideNavigationItem', () => {
+    let item: ShallowWrapper<SideNavigationItemProps>;
+
+    afterEach(() => {
+        if (item && item.exists()) {
+            item.unmount();
+        }
+    });
+
+    it('should render and unmount without errors', () => {
         expect(() => {
-            shallow(<SideNavigationItem title="link to Coveo" href="http://coveo.com" />);
+            item = shallow(<SideNavigationItem />);
+            item.unmount();
         }).not.toThrow();
+    });
+
+    it('should have the .state-active class when "isActive" prop is set to true', () => {
+        item = shallow(<SideNavigationItem isActive />);
+        expect(item.hasClass('state-active')).toBe(true);
     });
 });
