9b41205228103846198eeced37d8635da930c33f
Rosalie Liu <rliu@coveo.com>
2019-08-23 20:13:59 -0400

    Format TableHOCUtils.spec.ts

--- packages/react-vapor/src/components/table-hoc/tests/TableHOCUtils.spec.ts
+++ packages/react-vapor/src/components/table-hoc/tests/TableHOCUtils.spec.ts
@@ -1,18 +1,12 @@
 import {TableHOCUtils} from '../TableHOCUtils';
 
 describe('TableHOCUtils', () => {
-    const defaultProps = {
-        id: 'some-id',
-        componentId: 'some-componentId',
-        tableId: 'some-tableId',
-    };
-
+    const defaultProps = {id: 'some-id', componentId: 'some-componentId', tableId: 'some-tableId'};
     const PREDICATE_SEPARATOR = '--';
 
     describe('getPredicateId', () => {
         it('should get predicate id', () => {
             const predicateId = TableHOCUtils.getPredicateId(defaultProps.tableId, defaultProps.componentId);
-
             expect(predicateId).toEqual(defaultProps.tableId + PREDICATE_SEPARATOR + defaultProps.componentId);
         });
     });
@@ -21,7 +15,6 @@ describe('TableHOCUtils', () => {
         it('should parse predicated id to table id', () => {
             const predicateId = TableHOCUtils.getPredicateId(defaultProps.tableId, defaultProps.componentId);
             const tableId = TableHOCUtils.getTableIdFromPredicateId(predicateId);
-
             expect(tableId).toEqual(defaultProps.tableId);
         });
     });
@@ -30,7 +23,6 @@ describe('TableHOCUtils', () => {
         it('should get pagination id', () => {
             const paginationId = TableHOCUtils.getPaginationId(defaultProps.tableId);
             const expectedResult = `pagination-${defaultProps.tableId}`;
-
             expect(paginationId).toEqual(expectedResult);
         });
     });
@@ -55,7 +47,6 @@ describe('TableHOCUtils', () => {
                 filter: 'some-text',
                 dateLimits: [null, null] as any,
             };
-
             expect(compositeState).toEqual(expectedResult);
         });
     });
