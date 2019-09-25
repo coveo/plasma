ebdd7f9b78b14ec974cadb04486f3dfcf1b6ba9a
Gaël Dostie <gdostie@coveo.com>
2019-09-25 14:38:56 -0400

    Silence the logs a little bit more

--- create-live-demo.sh
+++ create-live-demo.sh
@@ -10,15 +10,15 @@ BRANCH_FOLDER_NAME=`echo "$TRAVIS_PULL_REQUEST_BRANCH" | md5sum | awk '{print $1
 echo "Syncing with gh-pages from branch: $TRAVIS_PULL_REQUEST_BRANCH"
 git stash
 git pull --no-edit --quiet --strategy-option ours "$SSH_REPO" gh-pages
-git rm "$BRANCH_FOLDER_NAME" -r
-git commit -m "Clean old build" --no-verify
+git rm --quiet "$BRANCH_FOLDER_NAME" -r
+git commit --quiet -m "Clean old build" --no-verify
 git stash pop
 
 echo "Creating live demo for branch: $TRAVIS_PULL_REQUEST_BRANCH";
 cp -R packages/react-vapor/docs "$BRANCH_FOLDER_NAME"
 
 git add "$BRANCH_FOLDER_NAME"
-git commit -m "live demo at https://coveo.github.io/react-vapor/$BRANCH_FOLDER_NAME/" --no-verify
+git commit --quiet -m "live demo at https://coveo.github.io/react-vapor/$BRANCH_FOLDER_NAME/" --no-verify
 echo "Deploying demo at https://coveo.github.io/react-vapor/$BRANCH_FOLDER_NAME/"
 
 SHA=`git rev-parse --verify HEAD`
