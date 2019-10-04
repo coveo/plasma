84d2758604f745e76871f531e0393c9711fd74ea
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2019-10-04 10:23:14 -0400

    feat: improve header style

--- packages/react-vapor/src/components/headers/HeaderWrapper.tsx
+++ packages/react-vapor/src/components/headers/HeaderWrapper.tsx
@@ -20,43 +20,39 @@ export class HeaderWrapper extends React.Component<IHeaderWrapperProps, {}> {
 
     render() {
         return (
-            <div className={this.getContainerClasses()}>
-                <div className={this.getClasses()}>
+            <div className={this.containerClasses}>
+                <div className={this.classes}>
                     <div className="truncate mr2">
                         {this.props.children}
-                        <h4 className="mt1 text-dark-grey normal-white-space">{this.props.description}</h4>
+                        <h4 className="admin-description text-dark-grey normal-white-space">
+                            {this.props.description}
+                        </h4>
                     </div>
-                    <div className="flex">{this.getActions()}</div>
+                    <div className="action-bar">{this.actions}</div>
                 </div>
                 <TabsHeader tabs={this.props.tabs} />
             </div>
         );
     }
 
-    private getContainerClasses(): string {
+    private get containerClasses(): string {
         return classNames('header-wrapper-container', {
             'header-wrapper-container-with-tabs': !!this.props.tabs,
-            'mod-border-bottom': !!this.props.hasBorderBottom && !this.props.tabs,
         });
     }
 
-    private getClasses(): string {
+    private get classes(): string {
         return classNames(
-            'flex',
-            'flex-center',
-            'space-between',
-            'header-height',
-            'pt1',
+            'flex flex-center space-between header-height panel-header',
             {
-                pb2: !!this.props.tabs,
-                pb1: !this.props.tabs,
-                'mod-header-padding': this.props.hasPadding,
+                'mod-no-border-bottom': !this.props.hasBorderBottom || this.props.tabs,
+                px0: !this.props.hasPadding,
             },
             this.props.classes
         );
     }
 
-    private getActions(): JSX.Element[] {
+    private get actions(): JSX.Element[] {
         return this.props.actions
             ? _.map(this.props.actions, (action: IContentProps, index: number) => <Content key={index} {...action} />)
             : null;
