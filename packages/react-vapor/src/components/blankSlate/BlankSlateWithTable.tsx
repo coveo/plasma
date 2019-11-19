d319cdfeb1f2bfa7d41f1a1e3c710ccc3bb75619
pochretien <moipeter10@gmail.com>
2019-11-19 15:23:39 -0500

    update interface

--- packages/react-vapor/src/components/blankSlate/BlankSlateWithTable.tsx
+++ packages/react-vapor/src/components/blankSlate/BlankSlateWithTable.tsx
@@ -1,17 +1,19 @@
+import {ComponentClass} from 'react';
 import * as React from 'react';
 import {IBlankSlateProps} from './BlankSlate';
 
-export interface IBlankSlateWithTableProps extends IBlankSlateWithTableConfig, IBlankSlateProps {}
-
-export interface IBlankSlateWithTableConfig {
+export interface IBlankSlateWithTableProps {
     numberOfColumn?: number;
 }
 
-export const blankSlateWithTable = <P extends IBlankSlateProps>(Component: React.ComponentType<P>) => {
-    class ComponentWithTable extends React.PureComponent<IBlankSlateWithTableProps> {
-        static defaultProps = {
+export const blankSlateWithTable = <P extends IBlankSlateProps>(
+    Component: React.ComponentType<P>
+): ComponentClass<IBlankSlateWithTableProps & P> => {
+    class ComponentBlankSlateWithTable extends React.PureComponent<IBlankSlateWithTableProps & P> {
+        static defaultProps: Partial<IBlankSlateWithTableProps & P> = {
             numberOfColumn: 20,
-        };
+        } as Partial<IBlankSlateWithTableProps & P>;
+
         render() {
             const {numberOfColumn, ...componentProps} = this.props;
             return (
@@ -24,5 +26,5 @@ export const blankSlateWithTable = <P extends IBlankSlateProps>(Component: React
         }
     }
 
-    return ComponentWithTable;
+    return ComponentBlankSlateWithTable;
 };
