991782a9709f64c2e97c997b627b1f861be98497
Gaël Dostie <gdostie@coveo.com>
2019-11-21 16:51:57 -0500

    Fix invalidate script and cleanup bucket

--- invalidate-cloudfront.js
+++ invalidate-cloudfront.js
@@ -1,11 +1,12 @@
 const exec = require('child_process').exec;
-const colors = require('colors');
 const AWS = require('aws-sdk');
 
 const cloudfront = new AWS.CloudFront();
 const pathToInvalidate = `/react-vapor/*`;
 
-const shouldDoInvalidation = () => !!(process.env.TRAVIS && process.env.TRAVIS_TAG);
+const shouldDoInvalidation = () => !!process.env.TRAVIS;
+const logInRed = (str) => console.log('\x1b[31m%s\x1b[0m', str);
+const logInGreen = (str) => console.log('\x1b[32m%s\x1b[0m', str);
 
 if (shouldDoInvalidation()) {
     cloudfront
@@ -21,21 +22,21 @@ if (shouldDoInvalidation()) {
         })
         .send((error, success) => {
             if (error) {
-                console.log(colors.red('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT'));
-                console.log(colors.red('*************'));
-                console.log(colors.red(error.message));
-                console.log(colors.red('*************'));
+                logInRed('ERROR WHILE INVALIDATING RESSOURCES ON CLOUDFRONT');
+                logInRed('*************');
+                logInRed(error.message);
+                logInRed('*************');
                 exec('travis_terminate 1');
                 throw error;
             }
             if (success) {
-                console.log(colors.green('INVALIDATED CLOUDFRONT CACHE SUCCESSFULLY'));
-                console.log(colors.green('*************'));
-                console.log(colors.green(`PATH INVALIDATED: ${pathToInvalidate}`));
-                console.log(colors.green(`INVALIDATION ID : ${success.Invalidation.Id}`));
-                console.log(colors.green('*************'));
+                logInGreen('INVALIDATED CLOUDFRONT CACHE SUCCESSFULLY');
+                logInGreen('*************');
+                logInGreen(`PATH INVALIDATED: ${pathToInvalidate}`);
+                logInGreen(`INVALIDATION ID : ${success.Invalidation.Id}`);
+                logInGreen('*************');
             }
         });
 } else {
-    console.log(colors.white('INVALIDATION FROM CLOUDFRONT SKIPPED BECAUSE THIS IS NOT AN OFFICIAL RELEASE'));
+    console.log('INVALIDATION FROM CLOUDFRONT SKIPPED BECAUSE THIS IS NOT AN OFFICIAL RELEASE');
 }
