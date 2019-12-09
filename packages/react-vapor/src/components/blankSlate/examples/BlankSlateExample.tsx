024fe45096e07af8039edeb7d83fd89b3e527565
Olivier Taillon <otaillon@coveo.com>
2019-12-09 15:53:07 -0500

    Removed duplicate example

--- packages/react-vapor/src/components/blankSlate/examples/BlankSlateExample.tsx
+++ packages/react-vapor/src/components/blankSlate/examples/BlankSlateExample.tsx
@@ -16,32 +16,28 @@ export class BlankSlateExample extends React.Component<any, any> {
                     <BlankSlate title="Title test" />
                 </div>
                 <div className="form-group">
-                    <label className="form-control-label">BlankSlate with title</label>
-                    <BlankSlate title="title test" />
-                </div>
-                <div className="form-group">
                     <label className="form-control-label">BlankSlate with title and description</label>
-                    <BlankSlate title="title test" description="description test" />
+                    <BlankSlate title="Title test" description="description test" />
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">BlankSlate with title and description with a link</label>
                     <BlankSlate
-                        title="title test"
+                        title="Title test"
                         description={
                             <span>
-                                this is a description with a link to <a href="https//www.google.com">this website</a>
+                                This is a description with a link to <a href="https//www.google.com">this website</a>
                             </span>
                         }
                     />
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">BlankSlate to fit in a modal</label>
-                    <BlankSlate title="title test" description="description test" withModal={true} />
+                    <BlankSlate title="Title test" description="Description test" withModal={true} />
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">BlankSlate with a button</label>
                     <BlankSlate
-                        title="title test"
+                        title="Title test"
                         buttons={[
                             {
                                 name: 'button',
@@ -54,7 +50,7 @@ export class BlankSlateExample extends React.Component<any, any> {
                 <div className="form-group">
                     <label className="form-control-label">BlankSlate with two button with one disabled</label>
                     <BlankSlate
-                        title="title test"
+                        title="Title test"
                         buttons={[
                             {
                                 name: 'button 1',
