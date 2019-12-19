00eff286e5605c505124b0ad1d1a788d9d489c72
pochretien <pochretien@coveo.com>
2019-12-19 13:00:55 -0500

    Add the new pagination with the new loading

---
+++ packages/react-vapor/src/components/flatSelect/FlatSelectWithPrepend.tsx
@@ -0,0 +1,21 @@
+import * as classNames from 'classnames';
+import {FunctionComponent} from 'react';
+import * as React from 'react';
+import {IFlatSelectOwnProps} from './FlatSelect';
+import {FlatSelectConnected} from './FlatSelectConnected';
+
+export interface IFlatSelectWithPrependProps extends IFlatSelectOwnProps {
+    prepend?: React.ReactNode;
+    prependClassName?: string;
+}
+
+export const FlatSelectWithPrepend: FunctionComponent<IFlatSelectWithPrependProps> = ({
+    prepend,
+    prependClassName = '',
+    ...flatSelectProps
+}) => (
+    <div className={classNames('prepended-flat-select', prependClassName)}>
+        <div className="flat-select-prepend">{prepend}</div>
+        <FlatSelectConnected {...flatSelectProps} />
+    </div>
+);
