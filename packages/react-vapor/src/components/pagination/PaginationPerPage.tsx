4ab8aeb5d262d13994aa0307c5934d4d3acb185d
pochretien <pochretien@coveo.com>
2019-12-19 13:00:55 -0500

    Add the new pagination with the new loading

---
+++ packages/react-vapor/src/components/pagination/PaginationPerPage.tsx
@@ -0,0 +1,40 @@
+import * as classNames from 'classnames';
+import * as React from 'react';
+import * as _ from 'underscore';
+import {UrlUtils} from '../../utils/UrlUtils';
+import {FlatSelectWithPrepend} from '../flatSelect/FlatSelectWithPrepend';
+import {Params} from '../table-hoc/TableWithUrlState';
+import {PaginationUtils} from './PaginationUtils';
+import {TablePaginationDefaultValue} from './TablePagination';
+
+export interface IPaginationPerPageProps {
+    id: string;
+    perPage?: number[];
+    defaultPerPageSelected?: number;
+    label?: React.ReactNode;
+    disabled?: boolean;
+    hidden?: boolean;
+}
+
+export const PaginationPerPage = ({
+    id,
+    perPage = TablePaginationDefaultValue.PerPage,
+    defaultPerPageSelected = UrlUtils.getSearchParams?.()?.[Params.pageSize],
+    label = TablePaginationDefaultValue.perPageLabel,
+    disabled,
+    hidden,
+}: IPaginationPerPageProps) => (
+    <>
+        <FlatSelectWithPrepend
+            id={PaginationUtils.getPaginationPerPageId(id)}
+            options={_.map(perPage, (nb: number) => ({
+                id: nb?.toString(),
+                option: {content: nb},
+            }))}
+            defaultSelectedOptionId={defaultPerPageSelected?.toString()}
+            prependClassName={classNames('items-per-page', {hidden: hidden})}
+            prepend={label}
+            disabled={disabled}
+        />
+    </>
+);
--- packages/react-vapor/src/components/table-hoc/TableWithPagination.tsx
+++ packages/react-vapor/src/components/table-hoc/TableWithPagination.tsx
@@ -6,14 +6,15 @@ import {WithServerSideProcessingProps} from '../../hoc/withServerSideProcessing/
 import {IReactVaporState, IReduxActionsPayload} from '../../ReactVapor';
 import {ConfigSupplier, HocUtils} from '../../utils/HocUtils';
 import {IReduxAction, ReduxConnect} from '../../utils/ReduxUtils';
+import {PaginationLoading} from '../loading/components/PaginationLoading';
 import {turnOffLoading} from '../loading/LoadingActions';
 import {INavigationChildrenProps, INavigationOwnProps} from '../navigation/Navigation';
 import {NavigationConnected} from '../navigation/NavigationConnected';
 import {NavigationSelectors} from '../navigation/NavigationSelectors';
 import {TableWithPaginationActions} from './actions/TableWithPaginationActions';
 import {ITableHOCOwnProps} from './TableHOC';
-import {TableHOCUtils} from './TableHOCUtils';
 import {TableSelectors} from './TableSelectors';
+import {TableHOCUtils} from './utils/TableHOCUtils';
 
 export interface ITableWithPaginationConfig
     extends WithServerSideProcessingProps,
@@ -100,6 +101,16 @@ export const tableWithPagination = (supplier: ConfigSupplier<ITableWithPaginatio
 
         render() {
             const newProps = _.omit(this.props, [...TableWithPaginationProps]);
+
+            if (this.props.isLoading && _.isEmpty(this.props.data)) {
+                return (
+                    <Component {...newProps}>
+                        <PaginationLoading />
+                        {this.props.children}
+                    </Component>
+                );
+            }
+
             return (
                 <Component {...newProps}>
                     <NavigationConnected
