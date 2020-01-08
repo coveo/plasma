85da951a546419267fcafaa86e5cec610cd462ae
FelixBlaisThon <fblais@coveo.com>
2020-01-08 14:34:34 -0500

    fixed the dateTooltip test where compared date were not the same

--- packages/react-vapor/src/components/tooltip/tests/DateTooltip.spec.tsx
+++ packages/react-vapor/src/components/tooltip/tests/DateTooltip.spec.tsx
@@ -7,7 +7,7 @@ import {Tooltip} from '../Tooltip';
 
 describe('Tooltip', () => {
     let tooltipWrapper: ShallowWrapper<DateTooltipsProps>;
-    const defaultFormat = moment().format('LLL');
+    let defaultFormat: string;
     const stringFormat = 'ddd, hA';
 
     describe('<DateTooltip />', () => {
@@ -17,16 +17,19 @@ describe('Tooltip', () => {
         });
 
         it('should display content in LLL if no format is given on moment date', () => {
+            defaultFormat = moment().format('LLL');
             tooltipWrapper = shallow(<DateTooltip date={moment()} />);
             expect(tooltipWrapper.find(Tooltip).props().title).toEqual(defaultFormat);
         });
 
         it('should display the tooltip in the given tooltipformat', () => {
+            defaultFormat = moment().format('LLL');
             tooltipWrapper = shallow(<DateTooltip date={moment()} tooltipFormat={stringFormat} />);
             expect(tooltipWrapper.find(Tooltip).props().title).toEqual(moment(defaultFormat).format(stringFormat));
         });
 
         it('should display the children of the tooltip in the given format', () => {
+            defaultFormat = moment().format('LLL');
             tooltipWrapper = shallow(<DateTooltip date={moment()} format={stringFormat} />);
             expect(tooltipWrapper.find(Tooltip).props().children).toEqual(moment(defaultFormat).format(stringFormat));
         });
