48f24be457972697ed6912bf9cd74649c0e660da
Gaël Dostie <gdostie@coveo.com>
2020-01-23 13:58:08 -0500

    ci(travis): fix lerna authentication for publish

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 npm config set package-lock false
-npm config set "//registry.npmjs.org/:_authToken=${env.NPM_TOKEN}"
+npm config set "//registry.npmjs.org/:_authToken=$NPM_TOKEN"
 
-git remote add -t master -f origin git@github.com:$TRAVIS_REPO_SLUG.git
+git remote add -t master -f pub git@github.com:$TRAVIS_REPO_SLUG.git
 git checkout master
-npx lerna publish --yes
+npx lerna publish --yes --amend --git-remote pub 
