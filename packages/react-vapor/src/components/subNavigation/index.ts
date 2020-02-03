d76a7fff4bb813c663018121bb84e130412cc66a
Rosalie Liu <52677246+rosalie-liu@users.noreply.github.com>
2020-02-03 13:43:54 -0500

    fix(subnavigation): add selector export to index (#1426)

--- packages/react-vapor/src/components/subNavigation/index.ts
+++ packages/react-vapor/src/components/subNavigation/index.ts
@@ -2,3 +2,4 @@ export * from './SubNavigation';
 export * from './SubNavigationActions';
 export * from './SubNavigationConnected';
 export * from './SubNavigationReducers';
+export * from './SubNavigationSelector';
