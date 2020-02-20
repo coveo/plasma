b1f40e9b04bb2ddc55cbc20adfcc4cd3e8ae0c66
Rosalie Liu <52677246+rosalie-liu@users.noreply.github.com>
2020-02-20 10:57:51 -0500

    feat(blankslate): add classname props (#1449)

--- packages/react-vapor/src/components/blankSlate/BlankSlateWithTable.tsx
+++ packages/react-vapor/src/components/blankSlate/BlankSlateWithTable.tsx
@@ -1,9 +1,12 @@
+import * as classNames from 'classnames';
 import {ComponentClass} from 'react';
 import * as React from 'react';
 import {IBlankSlateProps} from './BlankSlate';
 
 export interface IBlankSlateWithTableProps extends IBlankSlateProps {
     numberOfColumn?: number;
+    blankSlateRowClassName?: string;
+    blankSlateCellClassName?: string;
 }
 
 export const blankSlateWithTable = <P extends IBlankSlateProps>(
@@ -17,8 +20,8 @@ export const blankSlateWithTable = <P extends IBlankSlateProps>(
         render() {
             const {numberOfColumn, ...componentProps} = this.props;
             return (
-                <tr className="blankslate-row no-hover">
-                    <td colSpan={numberOfColumn}>
+                <tr className={classNames(this.props.blankSlateRowClassName, 'blankslate-row no-hover')}>
+                    <td colSpan={numberOfColumn} className={classNames(this.props.blankSlateCellClassName)}>
                         <Component {...(componentProps as P)}>{this.props.children}</Component>
                     </td>
                 </tr>
