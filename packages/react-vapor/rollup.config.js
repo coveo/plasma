b933c48768c0fe16b5963c0168d15f11a409c109
Sami Sayegh <samisayegh1@gmail.com>
2020-04-07 16:51:02 -0400

    chore: add rollup config

---
+++ packages/react-vapor/rollup.config.js
@@ -0,0 +1,66 @@
+import typescript from 'rollup-plugin-typescript2';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import keysTransformer from 'ts-transformer-keys/transformer';
+import scssVariable from 'rollup-plugin-sass-variables';
+import replace from '@rollup/plugin-replace';
+
+export default {
+    input: 'src/Entry.ts',
+    output: {
+        file: 'dist/react-vapor.esm.js',
+        format: 'es',
+    },
+    external: [
+        'codemirror',
+        'd3',
+        'jquery',
+        'react',
+        'react-dom',
+        'react-dom/server',
+        'react-redux',
+        'redux',
+        'underscore',
+        'coveo-styleguide',
+        'underscore.string',
+        'react-dnd',
+    ],
+    plugins: [
+        replacePlugin(),
+        postcss({
+            extract: false,
+            modules: {localIdentName: '[name]-[local]-[hash:base64]'},
+            namedExports: true,
+            use: ['sass'],
+        }),
+        scssVariable(),
+        resolve(),
+        commonjs({
+            namedExports: {
+                'hogan.js': ['Template', 'compile'],
+                'react-modal': ['setAppElement'],
+            },
+        }),
+        tsPlugin(),
+    ],
+};
+
+function tsPlugin() {
+    return typescript({
+        transformers: [
+            (service) => ({
+                before: [keysTransformer(service.getProgram())],
+                after: [],
+            }),
+        ],
+    });
+}
+
+function replacePlugin() {
+    return replace({
+        'process.env.NODE_ENV': 'production',
+        'process.env.REACT_VAPOR_VERSION': JSON.stringify(require('./package.json').version),
+        // version: '5.28.1'
+    });
+}
