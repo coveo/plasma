febcc2b0d27b2669c7ca23442edeb7554bb354ae
Sylvie Allain <sallain@coveo.com>
2020-06-15 12:43:24 -0400

    improvement(limit): connexion to the Redux store and refactor
    Because it is now connect to the store, the progress bar class can be modified from the actual limit
    value.

--- packages/react-vapor/src/components/limit/Limit.tsx
+++ packages/react-vapor/src/components/limit/Limit.tsx
@@ -1,155 +1,210 @@
 import classNames from 'classnames';
 import * as VaporSVG from 'coveo-styleguide';
 import * as React from 'react';
-import {Input, Svg} from 'react-vapor';
+import {connect} from 'react-redux';
+import {InputConnected, Svg} from 'react-vapor';
+import {IReactVaporState} from '../../ReactVapor';
+import {IDispatch} from '../../utils/ReduxUtils';
+import {changeInputValue} from '../input/InputActions';
+import {InputSelectors} from '../input/InputSelectors';
 
-export interface LimitProps {
+export interface LimitOwnProps {
+    id?: string;
     title: string;
     limitLabel?: string;
-    usageNumber?: number;
-    limitNumber?: number;
-    isLimitModifiable?: boolean;
+    usage?: number;
+    limit?: number;
+    isLimitEditable?: boolean;
     isLimitTheGoalToReach?: boolean;
     isHistoryIncluded?: boolean;
 }
 
-export const Limit: React.FunctionComponent<LimitProps> = ({
+const mapStateToProps = (state: IReactVaporState, ownProps: LimitOwnProps) => {
+    const ownLimit: string = ownProps.limit.toString() || Limit.defaultProps.limit.toString();
+    const currentLimit: string = InputSelectors.getValue(state, {id: ownProps.id}) || ownLimit;
+    return {
+        currentLimit,
+    };
+};
+
+const mapDispatchToProps = (dispatch: IDispatch, ownProps: LimitOwnProps) => ({
+    onChangeLimit: (limitValue: string) => {
+        dispatch(changeInputValue(ownProps.id, limitValue, true));
+    },
+});
+
+export interface LimitProps
+    extends LimitOwnProps,
+        ReturnType<typeof mapDispatchToProps>,
+        ReturnType<typeof mapStateToProps> {}
+
+export const LimitDisconnect: React.FunctionComponent<LimitProps> = ({
+    id,
     title,
     limitLabel,
-    usageNumber,
-    limitNumber,
-    isLimitModifiable,
+    usage,
+    limit,
+    currentLimit,
+    isLimitEditable,
     isLimitTheGoalToReach,
     isHistoryIncluded,
+    onChangeLimit: onChangeLimit,
 }) => {
-    const HeaderDivision = ({limitTitle, isHistory}: {limitTitle: string; isHistory: boolean}) => {
-        return (
-            <div className="flex space-between">
-                <label className="form-control-label"> {limitTitle}</label>
-                <HistoryIcon isHistory={isHistory} />
-            </div>
-        );
-    };
-
-    const HistoryIcon = ({isHistory}: {isHistory: boolean}) => {
-        return (
-            isHistory && (
-                <span className="icon mod-lg limit-history-button">
-                    <Svg svgName={VaporSVG.svg.menuAnalytics.name} className="fill-medium-blue" />
-                </span>
-            )
-        );
-    };
-
-    const ContentDivision = ({
-        usageInt,
-        limitInt,
-        isModifiable,
-        limitTitle,
-    }: {
-        usageInt: number;
-        limitInt: number;
-        isModifiable: boolean;
-        limitTitle: string;
-    }) => {
-        return (
-            <div className="limit-box-numbers pt1 flex">
-                <UsageDivision usageInt={usageInt} />
-                <LimitDivision limitInt={limitInt} isModifiable={isModifiable} limitTitle={limitTitle} />
-            </div>
-        );
-    };
-
-    const UsageDivision = ({usageInt}: {usageInt: number}) => {
-        const isUsageRequired: boolean = !!usageInt;
-        return (
-            isUsageRequired && (
-                <div className="limit-box-usage">
-                    <label className="form-control-label">Usage</label>
-                    <span className="limit-box-usage-value">{usageInt}</span>
-                </div>
-            )
-        );
-    };
-
-    const LimitDivision = ({
-        limitInt,
-        isModifiable,
-        limitTitle,
-    }: {
-        limitInt: number;
-        isModifiable: boolean;
-        limitTitle: string;
-    }) => {
-        const limitValueString: string = limitInt.toString();
-        if (isModifiable) {
-            return (
-                <Input
-                    type="number"
-                    labelTitle={limitTitle}
-                    defaultValue={limitValueString}
-                    classes="limit-box-limit form-group input-field validate"
-                ></Input>
-            );
-        } else {
-            return (
-                <div className="limit-box-limit">
-                    <label className="form-control-label">{limitTitle}</label>
-                    <span className="limit-box-limit-value">{limitInt}</span>
-                </div>
-            );
-        }
-    };
-
-    const ProgressBar = ({
-        usageInt,
-        isLimitTheGoal,
-        limitInt,
-    }: {
-        usageInt: number;
-        isLimitTheGoal: boolean;
-        limitInt: number;
-    }) => {
-        const isUsageRequired: boolean = !!usageInt;
-        const progressClass: string = getProgressBarClass({usageInt, limitInt});
-        const progressClasses = classNames('limit-box-bar', progressClass, {
-            'mod-green': isLimitTheGoal,
-        });
-
-        return (
-            isUsageRequired && (
-                <div className="limit-box-footer">
-                    <div className={progressClasses}></div>
-                </div>
-            )
-        );
-    };
-
-    function getProgressBarClass({usageInt, limitInt}: {usageInt: number; limitInt: number}): string {
-        const progress: number = Math.round((usageInt / limitInt) * 100);
-        return `progress-${progress.toString()}`;
-    }
-
     return (
         <div className="limit-box mb2">
             <div className="limit-box-main p2 pb1">
                 <HeaderDivision limitTitle={title} isHistory={isHistoryIncluded} />
                 <ContentDivision
-                    usageInt={usageNumber}
-                    limitInt={limitNumber}
-                    isModifiable={isLimitModifiable}
+                    id={id}
+                    usageValue={usage}
+                    limitValue={limit}
+                    isEditable={isLimitEditable}
                     limitTitle={limitLabel}
+                    onChangeLimit={onChangeLimit}
                 />
             </div>
-            <ProgressBar usageInt={usageNumber} isLimitTheGoal={isLimitTheGoalToReach} limitInt={limitNumber} />
+            <ProgressBar usageValue={usage} isLimitTheGoal={isLimitTheGoalToReach} limitValue={+currentLimit} />
         </div>
     );
 };
 
-Limit.defaultProps = {
+const HeaderDivision = ({limitTitle, isHistory}: {limitTitle: string; isHistory: boolean}) => {
+    return (
+        <div className="flex space-between">
+            <label className="form-control-label"> {limitTitle}</label>
+            <HistoryIcon isHistory={isHistory} />
+        </div>
+    );
+};
+
+const HistoryIcon = ({isHistory}: {isHistory: boolean}) => {
+    return (
+        isHistory && (
+            <span className="icon mod-lg limit-history-button">
+                <Svg svgName={VaporSVG.svg.menuAnalytics.name} className="fill-medium-blue" />
+            </span>
+        )
+    );
+};
+
+const ContentDivision = ({
+    id,
+    usageValue,
+    limitValue,
+    isEditable,
+    limitTitle,
+    onChangeLimit,
+}: {
+    id: string;
+    usageValue: number;
+    limitValue: number;
+    isEditable: boolean;
+    limitTitle: string;
+    onChangeLimit: (limit: string) => void;
+}) => {
+    return (
+        <div className="limit-box-numbers pt1 flex">
+            <UsageDivision usageValue={usageValue} />
+            <LimitDivision
+                id={id}
+                onChangeLimit={onChangeLimit}
+                usageValue={usageValue}
+                limitValue={limitValue}
+                isEditable={isEditable}
+                limitTitle={limitTitle}
+            />
+        </div>
+    );
+};
+
+const UsageDivision = ({usageValue}: {usageValue: number}) => {
+    const isUsageRequired: boolean = !!usageValue;
+    return (
+        isUsageRequired && (
+            <div className="limit-box-usage">
+                <label className="form-control-label">Usage</label>
+                <span className="limit-box-usage-value">{usageValue}</span>
+            </div>
+        )
+    );
+};
+
+const LimitDivision = ({
+    id,
+    limitValue,
+    usageValue,
+    isEditable,
+    limitTitle,
+    onChangeLimit,
+}: {
+    id: string;
+    limitValue: number;
+    usageValue: number;
+    isEditable: boolean;
+    limitTitle: string;
+    onChangeLimit: (limit: string) => void;
+}) => {
+    const limitValueString: string = limitValue.toString();
+    const minLimitValue: number = usageValue || 0;
+    if (isEditable) {
+        return (
+            <InputConnected
+                id={id}
+                type="number"
+                labelTitle={limitTitle}
+                defaultValue={limitValueString}
+                min={minLimitValue}
+                classes="limit-box-limit form-group input-field validate"
+                onChange={(limit) => onChangeLimit(limit)}
+            ></InputConnected>
+        );
+    } else {
+        return (
+            <div className="limit-box-limit">
+                <label className="form-control-label">{limitTitle}</label>
+                <span className="limit-box-limit-value">{limitValue}</span>
+            </div>
+        );
+    }
+};
+
+const ProgressBar = ({
+    usageValue,
+    isLimitTheGoal,
+    limitValue,
+}: {
+    usageValue: number;
+    isLimitTheGoal: boolean;
+    limitValue: number;
+}) => {
+    const isUsageRequired: boolean = !!usageValue;
+    const progressClass: string = getProgressBarClass({usageValue, limitValue});
+    const progressClasses = classNames('limit-box-bar', progressClass, {
+        'mod-green': isLimitTheGoal,
+    });
+
+    return (
+        isUsageRequired && (
+            <div className="limit-box-footer">
+                <div className={progressClasses}></div>
+            </div>
+        )
+    );
+};
+
+function getProgressBarClass({usageValue, limitValue}: {usageValue: number; limitValue: number}): string {
+    const progress: number = Math.round((usageValue / limitValue) * 100);
+    return progress <= 100 ? `progress-${progress.toString()}` : `progress-100`;
+}
+
+export const Limit = connect(mapStateToProps, mapDispatchToProps)(LimitDisconnect);
+
+LimitDisconnect.defaultProps = {
     limitLabel: 'Limit',
-    limitNumber: 100,
-    isLimitModifiable: false,
+    limit: 100,
+    isLimitEditable: false,
     isLimitTheGoalToReach: false,
     isHistoryIncluded: false,
 };
+
+Limit.defaultProps = {...LimitDisconnect.defaultProps};
--- packages/react-vapor/src/components/limit/tests/Limit.spec.tsx
+++ packages/react-vapor/src/components/limit/tests/Limit.spec.tsx
@@ -1,42 +1,79 @@
 import {mount, ReactWrapper, shallow} from 'enzyme';
+import {shallowWithStore} from 'enzyme-redux';
 import * as React from 'react';
-
-import {Limit, LimitProps} from '../Limit';
+import {Provider} from 'react-redux';
+import {InputConnected} from 'react-vapor';
+import {any} from 'underscore';
+import _ from 'underscore';
+import {clearState} from '../../../utils';
+import {getStoreMock, ReactVaporMockStore} from '../../../utils/tests/TestUtils';
+import {Limit, LimitDisconnect, LimitProps} from '../Limit';
 
 describe('Limit', () => {
     let limit: ReactWrapper<LimitProps, any>;
+    let limitDisconnect: ReactWrapper<LimitProps, any>;
+    let store: ReactVaporMockStore;
+    const bufferCurrentLimit = '100';
+    const bufferFunction = () => any;
+    const anyId = 'limit1';
+    const anyTitle = 'test limit';
+    const anyUsage = 33;
+
+    beforeEach(() => {
+        store = getStoreMock();
+    });
+
+    afterEach(() => {
+        store.dispatch(clearState());
+    });
 
     const defaultProps: LimitProps = {
-        title: 'test limit',
+        id: anyId,
+        title: anyTitle,
+        currentLimit: bufferCurrentLimit,
+        onChangeLimit: bufferFunction,
     };
 
-    it('should render without errors', () => {
+    it('should render without errors when disconnected', () => {
         expect(() => {
-            shallow(<Limit {...defaultProps} />);
+            shallow(<LimitDisconnect {...defaultProps} />);
         }).not.toThrow();
     });
 
-    it('should unMount without errors', () => {
-        const wrapper = shallow(<Limit {...defaultProps} />);
+    it('should unMount without errors when disconnected', () => {
+        const wrapper = shallow(<LimitDisconnect {...defaultProps} />);
         expect(() => {
             wrapper.unmount();
         }).not.toThrow();
     });
 
-    describe('once mounted', () => {
+    it('should render without errors when connected', () => {
+        expect(() => {
+            shallowWithStore(<Limit {...defaultProps} />, store).dive();
+        }).not.toThrow();
+    });
+
+    it('should unMount without errors when connected', () => {
+        const wrapper = shallowWithStore(<Limit {...defaultProps} />, store).dive();
+        expect(() => {
+            wrapper.unmount();
+        }).not.toThrow();
+    });
+
+    describe('once mounted, but disconnect', () => {
         beforeEach(() => {
-            limit = mount(<Limit {...defaultProps} />, {attachTo: document.getElementById('App')});
+            limitDisconnect = mount(<LimitDisconnect {...defaultProps} />, {attachTo: document.getElementById('App')});
         });
 
         afterEach(() => {
-            limit.detach();
+            limitDisconnect.detach();
         });
 
         it('should display the custom limit label if the limitLabel prop is specified', () => {
             const expectedCustomTitle: string = 'Throttling limit';
-            limit.setProps({limitLabel: expectedCustomTitle});
+            limitDisconnect.setProps({limitLabel: expectedCustomTitle});
             expect(
-                limit
+                limitDisconnect
                     .find('.limit-box-limit')
                     .find('label')
                     .text()
@@ -46,142 +83,140 @@ describe('Limit', () => {
         it('should display the default limit label if the limitLabel prop is not specified', () => {
             const expectedDefaultLimitTitle: string = Limit.defaultProps.limitLabel;
             expect(
-                limit
+                limitDisconnect
                     .find('.limit-box-limit')
                     .find('label')
                     .text()
             ).toBe(expectedDefaultLimitTitle);
         });
 
-        it('should display the <div class="limit-box-usage"> if the usageNumber prop is specified', () => {
+        it('should display the <div class="limit-box-usage"> if the usage prop is specified', () => {
             const expectedIsLimitBoxUsageDivIsPresent: boolean = true;
-            const anyUsageNumber: number = 33;
-            limit.setProps({usageNumber: anyUsageNumber});
+            limitDisconnect.setProps({usage: anyUsage});
 
-            expect(limit.exists('.limit-box-usage')).toBe(expectedIsLimitBoxUsageDivIsPresent);
+            expect(limitDisconnect.exists('.limit-box-usage')).toBe(expectedIsLimitBoxUsageDivIsPresent);
         });
 
-        it('should not display the <div class="limit-box-usage"> if the usageNumber prop is not specified', () => {
+        it('should not display the <div class="limit-box-usage"> if the usage prop is not specified', () => {
             const expectedIsLimitBoxUsageDivIsPresent: boolean = false;
 
-            expect(limit.exists('.limit-box-usage')).toBe(expectedIsLimitBoxUsageDivIsPresent);
+            expect(limitDisconnect.exists('.limit-box-usage')).toBe(expectedIsLimitBoxUsageDivIsPresent);
         });
 
-        it('should display the usage number value if the usageNumber prop is specified', () => {
-            const expectedUsageNumber: number = 33;
-            limit.setProps({usageNumber: expectedUsageNumber});
+        it('should display the usage value if the usage prop is specified', () => {
+            const expectedUsage: number = anyUsage;
+            limitDisconnect.setProps({usage: expectedUsage});
 
-            expect(limit.find('.limit-box-usage-value').text()).toBe(expectedUsageNumber.toString());
+            expect(limitDisconnect.find('.limit-box-usage-value').text()).toBe(expectedUsage.toString());
         });
 
-        it('should display the custom limit number value if the limitNumber prop is specified', () => {
-            const expectedCustomLimitNumber: number = 130;
-            limit.setProps({limitNumber: expectedCustomLimitNumber});
+        it('should display the custom limit value if the limit prop is specified', () => {
+            const expectedCustomLimit: number = 130;
+            limitDisconnect.setProps({limit: expectedCustomLimit});
 
-            expect(limit.find('.limit-box-limit-value').text()).toBe(expectedCustomLimitNumber.toString());
+            expect(limitDisconnect.find('.limit-box-limit-value').text()).toBe(expectedCustomLimit.toString());
         });
 
-        it('should display the default limit number value if the limitNumber prop is not specified', () => {
-            const expectedDefaultLimitNumber: number = Limit.defaultProps.limitNumber;
+        it('should display the default limit value if the limit prop is not specified', () => {
+            const expectedDefaultLimit: number = Limit.defaultProps.limit;
 
-            expect(limit.find('.limit-box-limit-value').text()).toBe(expectedDefaultLimitNumber.toString());
+            expect(limitDisconnect.find('.limit-box-limit-value').text()).toBe(expectedDefaultLimit.toString());
         });
 
-        it('should render a <span class="limit-box-limit-value"> tag to display the limit value if the limit is not modifiable', () => {
+        it('should render a <span class="limit-box-limit-value"> tag to display the limit value if the limit is not editable', () => {
             const expectedIsLimitBoxLimitValueClassIsPresent: boolean = true;
 
-            expect(limit.exists('.limit-box-limit-value')).toBe(expectedIsLimitBoxLimitValueClassIsPresent);
-        });
-
-        it('should not render a <span class="limit-box-limit-value"> tag to display the limit value if the limit is modifiable', () => {
-            const expectedIsLimitBoxLimitValueClassIsPresent: boolean = false;
-            limit.setProps({isLimitModifiable: true});
-
-            expect(limit.exists('.limit-box-limit-value')).toBe(expectedIsLimitBoxLimitValueClassIsPresent);
-        });
-
-        it('should render a type "number" input with input default value as the limit number and the input label as the limit label if the limit value is modifiable', () => {
-            const expectedIsInputPresent: boolean = true;
-            const expectedInputType: string = 'number';
-            const aCustomLimitValue: number = 130;
-            const expectedInputDefaultValue: string = aCustomLimitValue.toString();
-            const expectedInputLabel: string = 'limit modifiable';
-            limit.setProps({isLimitModifiable: true, limitNumber: aCustomLimitValue, limitLabel: expectedInputLabel});
-
-            expect(limit.find('.limit-box-limit').exists('input')).toBe(expectedIsInputPresent);
-
-            expect(
-                limit
-                    .find('.limit-box-limit')
-                    .find('input') // Input
-                    .props().type
-            ).toBe(expectedInputType);
-
-            expect(
-                limit
-                    .find('.limit-box-limit')
-                    .find('input')
-                    .props().defaultValue
-            ).toBe(expectedInputDefaultValue);
-
-            expect(
-                limit
-                    .find('.limit-box-limit')
-                    .find('label')
-                    .text()
-            ).toBe(expectedInputLabel);
+            expect(limitDisconnect.exists('.limit-box-limit-value')).toBe(expectedIsLimitBoxLimitValueClassIsPresent);
         });
 
         it('should include the class "mod-green" in the progress bar if the isLimitTheGoalToReach prop is true', () => {
             const expectedIsModGreenClassPresent: boolean = true;
-            const anyUsageNumber: number = 33;
-            limit.setProps({isLimitTheGoalToReach: true, usageNumber: anyUsageNumber});
+            limitDisconnect.setProps({isLimitTheGoalToReach: true, usage: anyUsage});
 
-            expect(limit.find('.limit-box-bar').hasClass('mod-green')).toBe(expectedIsModGreenClassPresent);
+            expect(limitDisconnect.find('.limit-box-bar').hasClass('mod-green')).toBe(expectedIsModGreenClassPresent);
         });
 
         it('should not include the class "mod-green" in the progress bar if the isLimitTheGoalToReach prop is false', () => {
             const expectedIsModGreenClassPresent: boolean = false;
-            const anyUsageNumber: number = 33;
-            limit.setProps({usageNumber: anyUsageNumber});
+            limitDisconnect.setProps({usage: anyUsage});
 
-            expect(limit.find('.limit-box-bar').hasClass('mod-green')).toBe(expectedIsModGreenClassPresent);
+            expect(limitDisconnect.find('.limit-box-bar').hasClass('mod-green')).toBe(expectedIsModGreenClassPresent);
         });
 
         it('should display the progress-bar based on the percentage by usage at the given limit', () => {
-            const anUsageNumber: number = 33;
-            const expectedPercentage: number = Math.round((anUsageNumber / Limit.defaultProps.limitNumber) * 100);
-            const expectedProgressBarClassName: string = 'progress-' + expectedPercentage.toString();
-            limit.setProps({usageNumber: anUsageNumber});
+            const expectedPercentage: number = Math.round((anyUsage / Limit.defaultProps.limit) * 100);
+            const expectedProgressBarClassName: string = `progress-${expectedPercentage.toString()}`;
+            limitDisconnect.setProps({usage: anyUsage});
 
-            expect(limit.find('.limit-box-bar').hasClass(expectedProgressBarClassName)).toBe(true);
+            expect(limitDisconnect.find('.limit-box-bar').hasClass(expectedProgressBarClassName)).toBe(true);
         });
 
-        it('should not display a progress-bar if there is not an usage number assigned', () => {
+        it('should not display a progress-bar if there is not an usage assigned', () => {
             const expectedIsProgressBarDisplayed: boolean = false;
 
-            expect(limit.exists('.limit-box-bar')).toBe(expectedIsProgressBarDisplayed);
+            expect(limitDisconnect.exists('.limit-box-bar')).toBe(expectedIsProgressBarDisplayed);
         });
 
-        it('should display a progress-bar if there is an usage number assigned', () => {
+        it('should display a progress-bar if there is an usage assigned', () => {
             const expectedIsProgressBarDisplayed: boolean = true;
-            const anyUsageNumber: number = 33;
-            limit.setProps({usageNumber: anyUsageNumber});
+            limitDisconnect.setProps({usage: anyUsage});
 
-            expect(limit.exists('.limit-box-bar')).toBe(expectedIsProgressBarDisplayed);
+            expect(limitDisconnect.exists('.limit-box-bar')).toBe(expectedIsProgressBarDisplayed);
         });
 
         it('should display the history icon if the isHistoryIncluded prop value is true', () => {
             const expectedIsHistoryIconDisplayed: boolean = true;
-            limit.setProps({isHistoryIncluded: true});
+            limitDisconnect.setProps({isHistoryIncluded: true});
 
-            expect(limit.exists('.limit-history-button')).toBe(expectedIsHistoryIconDisplayed);
+            expect(limitDisconnect.exists('.limit-history-button')).toBe(expectedIsHistoryIconDisplayed);
         });
 
         it('should not display the history icon if the isHistoryIncluded prop value is false', () => {
             const expectedIsHistoryIconDisplayed: boolean = false;
 
-            expect(limit.exists('.limit-history-button')).toBe(expectedIsHistoryIconDisplayed);
+            expect(limitDisconnect.exists('.limit-history-button')).toBe(expectedIsHistoryIconDisplayed);
+        });
+    });
+    describe('once mounted and connected', () => {
+        const limitEditableProps: LimitProps = {
+            id: anyId,
+            title: anyTitle,
+            usage: anyUsage,
+            currentLimit: Limit.defaultProps.limit.toString(),
+            onChangeLimit: bufferFunction,
+            isLimitEditable: true,
+        };
+
+        beforeEach(() => {
+            limit = mount(
+                <Provider store={store}>
+                    <Limit {...limitEditableProps} />
+                </Provider>,
+                {attachTo: document.getElementById('App')}
+            );
+        });
+
+        afterEach(() => {
+            limit.detach();
+        });
+
+        it('should contains an InputConnected if limit is editable', () => {
+            expect(limit.find(InputConnected).length).toEqual(1);
+        });
+
+        it('should add the input in the store when limit is editable', () => {
+            const expectedStoreAction = 'ADD_INPUT';
+            expect(store.getActions().length).toBe(1);
+            expect(store.getActions()[0].type).toBe(expectedStoreAction);
+        });
+
+        it('should change the input value in the store when the current limit is changed', () => {
+            const newCurrentLimit = '150';
+            const expectedStoreAction = 'CHANGE_VALUE_INPUT';
+            const input = limit.find(InputConnected);
+            input.props().onChange(newCurrentLimit);
+
+            expect(store.getActions()[1].type).toBe(expectedStoreAction);
         });
     });
 });
