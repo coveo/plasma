240605b984d7e96ae464385094242281a77c256a
FelixBlaisThon <fblais@coveo.com>
2020-07-03 11:22:02 -0400

    feat(tabselector.ts): added a selector for selected tab

--- packages/react-vapor/src/components/tab/TabSelectors.ts
+++ packages/react-vapor/src/components/tab/TabSelectors.ts
@@ -1,12 +1,20 @@
+import {createSelector} from 'reselect';
 import _ from 'underscore';
 
 import {DEFAULT_GROUP_ID, ITabPaneOwnProps} from '.';
 import {IReactVaporState} from '../../ReactVapor';
+import {ITabGroupState} from './TabReducers';
 
 const getTabGroup = (state: IReactVaporState, ownProps: ITabPaneOwnProps) => {
     return _.findWhere(state.tabs, {id: ownProps.groupId ?? DEFAULT_GROUP_ID});
 };
 
+const getTabSelected = (tabId: string) =>
+    createSelector(
+        getTabGroup,
+        (tabGroup: ITabGroupState): boolean => _.findWhere(tabGroup.tabs, {id: tabId})?.isSelected
+    );
 export const TabSelectors = {
     getTabGroup,
+    getTabSelected,
 };
