45202239728af9db92849030d17f3c731647d1b8
François Lachance-Guillemette <flguillemette@coveo.com>
2020-11-24 15:10:46 -0500

    fix(validation): automatically select initial value in single select
    [COM-702]

--- packages/demo/src/components/examples/SingleSelectExamples.tsx
+++ packages/demo/src/components/examples/SingleSelectExamples.tsx
@@ -170,7 +170,18 @@ const SingleSelectConnectedExamples: React.ComponentType = () => (
         </Section>
         <Section level={3} title="A single select with dirty management">
             <SingleSelectWithDirty id="select-dirty" items={defaultItems} canClear />
-            <SaveButton enabled validationIds={['select-dirty']} name="An example button bound to the select" />
+            <SingleSelectWithDirty
+                id="select-dirty-with-initial-value"
+                items={defaultItems}
+                initialValue={defaultItems[1].value}
+                canClear
+                footer={<div className="select-footer">This one already has an initial value</div>}
+            />
+            <SaveButton
+                enabled
+                validationIds={['select-dirty', 'select-dirty-with-initial-value']}
+                name="An example button bound to the select"
+            />
         </Section>
     </Section>
 );
