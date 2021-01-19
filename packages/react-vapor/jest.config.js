8d95da0edf822c9f3c1d28e039a8dd6fd079bd14
Jack <jmcgregor@coveo.com>
2021-01-19 15:55:29 -0500

    test: updating tests for jest move

--- packages/react-vapor/jest.config.js
+++ packages/react-vapor/jest.config.js
@@ -1,24 +1,23 @@
 module.exports = {
     verbose: true,
     moduleNameMapper: {
-        '\\.(scss|css)$': '<rootDir>/jest/identity-obj-proxy-esm.js',
+        '\\.(scss|css)$': '<rootDir>/.jest/identity-obj-proxy-esm.js',
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
     },
-    setupFilesAfterEnv: ['<rootDir>/jest/setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
     reporters: ['default'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest'
     },
     testMatch: ['<rootDir>/src/**/*.spec.{ts,tsx}'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'scss'],
-    moduleDirectories: ['node_modules', 'jest/utils'],
     testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
     globals: {
         'ts-jest': {
             // relative path to the ts-jest-keys-transformer.js file
             tsconfig: '<rootDir>/tsconfig.test.json',
             astTransformers: {
-                before: ['<rootDir>/jest/ts-jest-keys-transformer.js'],
+                before: ['<rootDir>/.jest/ts-jest-keys-transformer.js'],
             },
         },
     },
