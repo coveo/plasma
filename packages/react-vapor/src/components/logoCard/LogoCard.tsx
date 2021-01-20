255d953d7ee037abf1e0d5ba5ce10e2294b7b256
Kien <kdang@coveo.com>
2021-01-20 17:28:59 -0500

    improvement(logo-card): adding disabled badge prop
    adding disabled prop badge and changing css for logo card to match mockup design
    
    style(logo card badges): badges style changes and logo card
    
    moving badges to the right as per new design for trial ux. Rounded corners of LogoCard and Badges.
    Badges no longer all capitalized and deprecating ribbon feature

--- packages/react-vapor/src/components/logoCard/LogoCard.tsx
+++ packages/react-vapor/src/components/logoCard/LogoCard.tsx
@@ -7,11 +7,6 @@ import {slugify} from 'underscore.string';
 
 import {TooltipPlacement} from '../../utils';
 import {Badge, IBadgeProps} from '../badge/Badge';
-import {
-    CornerRibbon,
-    DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME,
-    ICornerRibbonProps,
-} from '../cornerRibbon/CornerRibbon';
 import {Svg} from '../svg/Svg';
 import {Tooltip} from '../tooltip';
 
@@ -19,16 +14,14 @@ export const DEFAULT_LOGO_CARD_CLASSNAME: string = 'logo-card';
 export const DEFAULT_LOGO_ICON: string = VaporSVG.svg.sourceCustom.name;
 export const DEFAULT_LOGO_ICON_CLASSNAME: string = 'icon';
 export const DEFAULT_LOGO_ICON_SIZE: string = 'mod-4x';
-export const DEFAULT_DISABLED_RIBBON_LABEL: string = 'Unavailable';
+export const DEFAULT_DISABLED_BADGE: string = 'Unavailable';
 
 export interface ILogoCardProps {
     badges?: IBadgeProps[];
     description?: string;
     disabled?: boolean;
-    disabledRibbon?: ICornerRibbonProps;
     extraContainerClasses?: string[];
     onClick?: () => void;
-    ribbon?: ICornerRibbonProps;
     svgName?: string;
     title: string;
     tooltip?: string;
@@ -41,9 +34,6 @@ export class LogoCard extends React.Component<ILogoCardProps & React.HTMLProps<H
     static defaultProps: Partial<ILogoCardProps> = {
         badges: [],
         disabled: false,
-        disabledRibbon: {
-            label: DEFAULT_DISABLED_RIBBON_LABEL,
-        },
         extraContainerClasses: [],
         svgName: DEFAULT_LOGO_ICON,
     };
@@ -57,27 +47,12 @@ export class LogoCard extends React.Component<ILogoCardProps & React.HTMLProps<H
     render() {
         const containerClassName = classNames(
             DEFAULT_LOGO_CARD_CLASSNAME,
-            this.props.disabled || this.props.ribbon ? DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME : '',
             this.props.extraContainerClasses,
             this.props.disabled ? 'disabled' : ''
         );
         const logoIconClassName = classNames(DEFAULT_LOGO_ICON_CLASSNAME, DEFAULT_LOGO_ICON_SIZE);
-        const descriptionClassName = classNames(this.props.badges.length ? 'ml1' : '');
-        const logoCardContentClassName = classNames(
-            !this.props.badges.length && !this.props.description ? 'logo-card-content-center' : 'logo-card-content'
-        );
 
         const badges = this.props.badges.map((badgeProps) => <Badge {...badgeProps} key={slugify(badgeProps.label)} />);
-        const description = this.props.description ? (
-            <span className={descriptionClassName}>{this.props.description}</span>
-        ) : null;
-
-        let ribbon = null;
-        if (this.props.disabled) {
-            ribbon = <CornerRibbon {...this.props.disabledRibbon} />;
-        } else if (this.props.ribbon) {
-            ribbon = <CornerRibbon {...this.props.ribbon} />;
-        }
 
         const logoCard: JSX.Element = (
             <Tooltip title={this.props.tooltip} placement={this.props.tooltipPlacement}>
@@ -89,11 +64,11 @@ export class LogoCard extends React.Component<ILogoCardProps & React.HTMLProps<H
                     <div className="logo-card-logo">
                         <Svg svgName={this.props.svgName} className={logoIconClassName} />
                     </div>
-                    <div className={logoCardContentClassName}>
-                        <h2 className="logo-card-title">{this.props.title}</h2>
-                        {this.props.badges || this.props.description ? <div> {...badges} {description} </div> : ''}
+                    <div className="logo-card-content">
+                        <h2 className="logo-card-title mb1">{this.props.title}</h2>
+                        {this.props.description}
                     </div>
-                    {ribbon}
+                    {this.props.badges.length > 0 ? <div className="logo-card-badge">{...badges}</div> : null}
                 </div>
             </Tooltip>
         );
