39e1e100d3698a0f63d443b9a57e718bb5173b72
FelixBlaisThon <fblais@coveo.com>
2021-01-20 15:00:40 -0500

    refactor(hover.tsx): -hover everywhere in reactvapor
    since hover css style should be intrinsic to a component, we decided to remove completely the hover
    section of react-vapor and handle it directly from the component

--- packages/demo/src/styles/utility/index.tsx
+++ packages/demo/src/styles/utility/index.tsx
@@ -3,7 +3,6 @@ import {Redirect, Route, RouteComponentProps} from 'react-router-dom';
 
 import ColorDots from './ColorDot';
 import Cursor from './Cursor';
-import Hover from './Hover';
 import LineHeight from './LineHeight';
 import Whitespace from './Whitespace';
 
@@ -13,7 +12,6 @@ const Utility: React.FunctionComponent<RouteComponentProps> = ({match}) => (
         <Route path={`${match.url}/whitespace`} component={Whitespace} />
         <Route path={`${match.url}/cursor`} component={Cursor} />
         <Route path={`${match.url}/color-dots`} component={ColorDots} />
-        <Route path={`${match.url}/hover`} component={Hover} />
         <Route exact path={`${match.url}/`} component={() => <Redirect to={`${match.url}/whitespace`} />} />
     </>
 );
