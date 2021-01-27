d0ae4e85f2119a7814398c579113ab8990dd066e
aiheon <aiheon@coveo.com>
2021-01-27 09:48:24 -0500

    fix: removed bg-color util + started clean up of dead classes

--- packages/demo/src/components/examples/BadgeExamples.tsx
+++ packages/demo/src/components/examples/BadgeExamples.tsx
@@ -17,21 +17,6 @@ export class BadgeExamples extends React.Component<any, any> {
                         <Badge label="Badge label" icon="lock" />
                     </div>
                 </div>
-                <div className="form-group">
-                    <label className="form-control-label">Badge with extra classes</label>
-                    <div className="form-control">
-                        <Badge label="Badge blue background" extraClasses={['bg-blue']} />
-                        <Badge label="Badge mod-small" extraClasses={['mod-small']} />
-                    </div>
-                </div>
-                <div className="form-group">
-                    <label className="form-control-label">Badge group</label>
-                    <div className="form-control">
-                        <Badge label="Multiple" extraClasses={['bg-blue']} />
-                        <Badge label="Badges" extraClasses={['bg-medium-blue']} />
-                        <Badge label="Group" extraClasses={['bg-darker-blue']} />
-                    </div>
-                </div>
             </div>
         );
     }
--- packages/react-vapor/src/components/table-hoc/TableRowNumberColumn.tsx
+++ packages/react-vapor/src/components/table-hoc/TableRowNumberColumn.tsx
@@ -5,7 +5,7 @@ import * as styles from './styles/TableRowNumber.scss';
 
 // eslint-disable-next-line id-blacklist
 export const TableRowNumberColumn = (props: {number: React.ReactNode}) => (
-    <td key="table-row-number" className={classNames('bg-grey-1 mod-border-right', styles.tableNumberedRow)}>
+    <td key="table-row-number" className={classNames('mod-border-right', styles.tableNumberedRow)}>
         <div className={classNames('text-lynch flex flex-column flex-center center', styles.tableNumberedRowContainer)}>
             {props.number}
         </div>
