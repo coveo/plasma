4ddf631002667c95ec07366b66c4aacd660bb0dc
aiheon <aiheon@coveo.com>
2021-01-27 09:48:24 -0500

    fix: removed bg-color util + started clean up of dead classes

--- packages/demo/src/styles/index.tsx
+++ packages/demo/src/styles/index.tsx
@@ -20,7 +20,7 @@ import Utility from './utility';
 const Styles: React.FunctionComponent<RouteComponentProps> = ({match}) => (
     <>
         <SideMenu />
-        <div className="coveo-form flex-auto relative bg-pure-white shadow-2 ml4 overflow-auto demo-content">
+        <div className="coveo-form flex-auto relative shadow-2 ml4 overflow-auto demo-content">
             <Route path={`${match.url}/general-guidelines`} component={GeneralGuidelines} />
             <Route path={`${match.url}/borders`} component={Borders} />
             <Route path={`${match.url}/cards`} component={Cards} />
