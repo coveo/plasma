75d9bc0f37f58180513109d4903fc3b60fcec0c7
dmgauthier <dgauthier@coveo.com>
2021-01-27 09:08:52 -0500

    feat(rollup): minify code with terser plugin

--- packages/react-vapor/rollup.config.js
+++ packages/react-vapor/rollup.config.js
@@ -3,16 +3,18 @@ import inject from '@rollup/plugin-inject';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import path from 'path';
-import externalGlobals from 'rollup-plugin-external-globals';
 import postcss from 'rollup-plugin-postcss';
 import scssVariable from 'rollup-plugin-sass-variables';
 import {terser} from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript2';
 import keysTransformer from 'ts-transformer-keys/transformer';
 
+const isJenkins = !!process.env.JENKINS_HOME;
+
 export default [
     {format: 'es', file: 'dist/react-vapor.esm.js'},
     {format: 'umd', file: 'dist/react-vapor.js'},
+    ...(isJenkins ? [{format: 'umd', file: 'dist/react-vapor.min.js'}] : []),
 ].map(({format, file}) => ({
     input: 'src/Entry.ts',
     output: {
@@ -31,6 +33,7 @@ export default [
             underscore: '_',
             'coveo-styleguide': 'VaporSVG',
             'underscore.string': 's',
+            'react-dom/server': 'server',
         },
     },
     external: [
@@ -67,10 +70,11 @@ export default [
                 'hogan.js': ['Template', 'compile'],
                 'react-modal': ['setAppElement'],
                 'react-dnd': ['DragDropContext', 'DropTarget', 'DragSource'],
-                'diff/dist/diff.js': ['diffChars', 'diffWordsWithSpace']
-            }
+                diff2html: ['diffChars', 'diffWordsWithSpace'],
+            },
         }),
         tsPlugin(),
+        ...(file.includes('.min.') ? [terser()] : []),
     ],
 }));
 
