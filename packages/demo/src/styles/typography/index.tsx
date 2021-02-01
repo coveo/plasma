45c5612ae1336b31eb5ce0f69d420122e450deb1
aiheon <aiheon@coveo.com>
2021-02-01 15:33:13 -0500

    fix: removed typography color exmaple from style demo

--- packages/demo/src/styles/typography/index.tsx
+++ packages/demo/src/styles/typography/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {Redirect, Route, RouteComponentProps} from 'react-router-dom';
 
-import Colors from './Colors';
 import Headings from './Headings';
 import Lists from './Lists';
 import Utilities from './Utilities';
@@ -9,7 +8,6 @@ import Utilities from './Utilities';
 const Typography: React.FunctionComponent<RouteComponentProps> = ({match}) => (
     <>
         <Route path={`${match.url}/headings`} component={Headings} />
-        <Route path={`${match.url}/colors`} component={Colors} />
         <Route path={`${match.url}/utilities`} component={Utilities} />
         <Route path={`${match.url}/lists`} component={Lists} />
         <Route exact path={`${match.url}/`} component={() => <Redirect to={`${match.url}/headings`} />} />
