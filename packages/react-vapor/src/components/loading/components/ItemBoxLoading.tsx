c4fbaef518411c397b52cb85e5ef4dd6d1572355
aiheon <aiheon@coveo.com>
2021-02-02 14:22:02 -0500

    fix: created css class for loading item box

--- packages/react-vapor/src/components/loading/components/ItemBoxLoading.tsx
+++ packages/react-vapor/src/components/loading/components/ItemBoxLoading.tsx
@@ -7,15 +7,9 @@ const possibleWidths = ['mod-width-10', 'mod-width-20', 'mod-width-30', 'mod-wid
 
 export const ItemBoxLoading: React.FunctionComponent = () => (
     <li className="item-box disabled flex">
-        <div
-            className={classNames('mod-rounded-border-1', Random.pick(possibleWidths))}
-            style={{height: '1em', backgroundColor: '#e5e8e8'}}
-        />
+        <div className={classNames('mod-rounded-border-1 item-box-loading', Random.pick(possibleWidths))} />
         {Random.bool() && (
-            <div
-                className={classNames('ml1 mod-rounded-border-1', Random.pick(possibleWidths))}
-                style={{height: '1em', backgroundColor: '#e5e8e8'}}
-            />
+            <div className={classNames('ml1 mod-rounded-border-1 item-box-loading', Random.pick(possibleWidths))} />
         )}
     </li>
 );
