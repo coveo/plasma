6aa49fbce2998293cebb2a617e350af6480dc3ef
rosalie-liu <rliu@coveo.com>
2021-02-04 11:00:15 -0500

    fix(font): apply reviews

--- packages/demo/src/components/examples/CornerRibbonExamples.tsx
+++ packages/demo/src/components/examples/CornerRibbonExamples.tsx
@@ -1,52 +1,19 @@
 import * as React from 'react';
-import {CornerRibbon, DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME, PlacementX, PlacementY} from 'react-vapor';
+import {CornerRibbon, DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME, PlacementX, PlacementY, Section} from 'react-vapor';
 
-import {rectangleContainer, squareContainer} from './CornerRibbonExamplesCommon';
+import {squareContainer} from './CornerRibbonExamplesCommon';
 
 export class CornerRibbonExamples extends React.Component<any, any> {
     render() {
         return (
-            <div className="mt2">
-                <div className="form-group">
-                    <label className="form-control-label">CornerRibbon placement (default is top right)</label>
-                    <div className="form-control">
-                        <div
-                            style={squareContainer}
-                            className={`${DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME} bg-white`}
-                        >
-                            <CornerRibbon label="Top Right" />
-                            <CornerRibbon label="Top Left" placementX={PlacementX.Left} placementY={PlacementY.Top} />
-                            <CornerRibbon
-                                label="Bottom Left"
-                                placementX={PlacementX.Left}
-                                placementY={PlacementY.Bottom}
-                            />
-                            <CornerRibbon
-                                label="Bottom Right"
-                                placementX={PlacementX.Right}
-                                placementY={PlacementY.Bottom}
-                            />
-                        </div>
-                    </div>
+            <Section level={2} title="CornerRibbon placement (default is top right)">
+                <div style={squareContainer} className={`${DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME} bg-white`}>
+                    <CornerRibbon label="Top Right" />
+                    <CornerRibbon label="Top Left" placementX={PlacementX.Left} placementY={PlacementY.Top} />
+                    <CornerRibbon label="Bottom Left" placementX={PlacementX.Left} placementY={PlacementY.Bottom} />
+                    <CornerRibbon label="Bottom Right" placementX={PlacementX.Right} placementY={PlacementY.Bottom} />
                 </div>
-                <div className="form-group">
-                    <label className="form-control-label">CornerRibbon with extra classes</label>
-                    <div className="form-control">
-                        <div
-                            style={rectangleContainer}
-                            className={`${DEFAULT_CORNER_RIBBON_CONTAINER_CLASSNAME} bg-white`}
-                        >
-                            <CornerRibbon label="Ribbon" extraClasses={['bg-orange', 'bold']} />
-                            <CornerRibbon
-                                label="Ribbon"
-                                placementX={PlacementX.Left}
-                                placementY={PlacementY.Top}
-                                extraClasses={['bg-pure-white', 'mod-red', 'bold']}
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </Section>
         );
     }
 }
