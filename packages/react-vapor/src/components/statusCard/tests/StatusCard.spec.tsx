a3065eea48647da5916f5993545f65c35b97ac16
Gaël Dostie <gdostie@coveo.com>
2021-03-19 14:58:38 -0400

    fix(status-card): remove the css module and load the style with css
    BREAKING CHANGE: The color prop was removed from the StatusCard component, use mod-information,
    mod-success, mod-warning or mod-critical classes instead.

--- packages/react-vapor/src/components/statusCard/tests/StatusCard.spec.tsx
+++ packages/react-vapor/src/components/statusCard/tests/StatusCard.spec.tsx
@@ -8,36 +8,30 @@ import {StatusCard} from '../StatusCard';
 describe('StatusCard', () => {
     it('should render without errors', () => {
         expect(() => {
-            shallow(<StatusCard color="green" title="title" />);
+            shallow(<StatusCard title="title" />);
         }).not.toThrow();
     });
 
     it('should render a <Loading /> component when the prop loading is set to true', () => {
-        const card = shallow(<StatusCard color="green" title="title" loading />);
+        const card = shallow(<StatusCard title="title" loading />);
 
         expect(card.find(Loading).length).toBe(1);
     });
 
     it('has the "simple" class if the "simple" prop is true', () => {
-        const card = shallow(<StatusCard color="green" title="title" simple />);
+        const card = shallow(<StatusCard title="title" simple />);
 
         expect(card.hasClass('simple')).toBe(true);
     });
 
-    it('has the "isSuccess" class if not in loading', () => {
-        const card = shallow(<StatusCard color="green" title="title" />);
+    it('has the "loading" class if the "loading" prop is true', () => {
+        const card = shallow(<StatusCard title="title" loading />);
 
-        expect(card.hasClass('modSuccess')).toBe(true);
-    });
-
-    it('does not have the "isSuccess" class if in loading', () => {
-        const card = shallow(<StatusCard color="green" title="title" loading />);
-
-        expect(card.hasClass('modSuccess')).toBe(false);
+        expect(card.hasClass('loading')).toBe(true);
     });
 
     it('renders a Svg if the "icon" prop is specified', () => {
-        const card = shallow(<StatusCard color="green" title="title" icon="help" />);
+        const card = shallow(<StatusCard title="title" icon="help" />);
 
         expect(card.find(Svg).exists()).toBe(true);
         expect(card.find(Svg).prop('svgName')).toBe('help');
