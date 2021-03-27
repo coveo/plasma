9c59e61f9edb42f57b35960e28afedb684827c6c
aiheon <aiheon@coveo.com>
2021-03-27 09:30:13 -0400

    feat(banner): removed css module

--- packages/react-vapor/src/components/banner/BannerContainer.tsx
+++ packages/react-vapor/src/components/banner/BannerContainer.tsx
@@ -2,10 +2,8 @@ import classNames from 'classnames';
 import * as React from 'react';
 import * as _ from 'underscore';
 
-import * as styles from './styles/Banner.scss';
-
 export const BannerContainer: React.FunctionComponent<React.HTMLProps<HTMLDivElement>> = (props) => (
-    <div {..._.omit(props, 'children')} className={classNames(props.className, styles.banner)}>
+    <div {..._.omit(props, 'children')} className={classNames(props.className, 'banner')}>
         {props.children}
     </div>
 );
