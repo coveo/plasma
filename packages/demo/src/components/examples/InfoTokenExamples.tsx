fbf5d1496678334254596754f50f5d50f3b9c808
aiheon <aiheon@coveo.com>
2021-04-23 14:32:01 -0400

    feat(info-token): clean slate, starting from scratch

--- packages/demo/src/components/examples/InfoTokenExamples.tsx
+++ packages/demo/src/components/examples/InfoTokenExamples.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {InfoToken} from 'react-vapor';
 
 export class InfoTokenExamples extends React.Component<any, any> {
     render() {
@@ -8,49 +7,50 @@ export class InfoTokenExamples extends React.Component<any, any> {
                 <div className="form-group">
                     <label className="form-control-label">Warnings tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-warning" extraClasses={['ml1']} />
+                        <Svg svgName={'add'} className="icon mod-normal ml1" />
+                        {/* <InfoToken icon="info-token-warning" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-warning" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-warning" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-warning" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Success tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-success" extraClasses={['ml1']} />
+                        {/* <InfoToken icon="info-token-success" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-success" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-success" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-success" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Information tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-information" extraClasses={['ml1']} />
+                        {/* <InfoToken icon="info-token-information" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-information" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-information" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-information" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Tip tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-tip" extraClasses={['ml1']} />
+                        {/* <InfoToken icon="info-token-tip" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-tip" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-tip" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-tip" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Critical tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-critical" extraClasses={['ml1']} />
+                        {/* <InfoToken icon="info-token-critical" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-critical" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-critical" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-critical" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Question tokens</label>
                     <div className="form-control">
-                        <InfoToken icon="info-token-question" extraClasses={['ml1']} />
+                        {/* <InfoToken icon="info-token-question" extraClasses={['ml1']} />
                         <InfoToken icon="info-token-question" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-question" extraClasses={['mod-smaller ml1']} />
+                        <InfoToken icon="info-token-question" extraClasses={['mod-smaller ml1']} /> */}
                     </div>
                 </div>
             </div>
