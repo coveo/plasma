6012a8702b2c16aed06d57f531be8da4f0b655f5
Gaël Dostie <gdostie@coveo.com>
2021-04-23 11:38:49 -0400

    refactor(vapor): remove redesign color variables
    During the first version of the reskin, we created some new colors variables that contained early
    versions of the new colors. Since then, some of them changed names, changed tones or we completely
    replaced by another color. Here I did a cleanup of those so that everything uses the new colors.
    
    BREAKING CHANGE: Some css variables were removed or changed names. Have a look at the new colors
    declaration in scss/colors.scss to find a replacement for colors that were removed.

--- packages/demo/src/components/examples/BadgeExamples.tsx
+++ packages/demo/src/components/examples/BadgeExamples.tsx
@@ -8,29 +8,29 @@ export class BadgeExamples extends React.Component<any, any> {
                 <div className="form-group">
                     <label className="form-control-label">Default Badge</label>
                     <div className="form-control">
-                        <Badge label="Badge Generic/Grey" />
-                        <Badge label="Badge Generic/Navy" extraClasses={['mod-information ml1']} />
-                        <Badge label="Badge Semantic/Success" extraClasses={['mod-success ml1']} />
-                        <Badge label="Badge Semantic/Critical" extraClasses={['mod-critical ml1']} />
-                        <Badge label="Badge New" extraClasses={['mod-warning ml1']} />
-                        <Badge label="Badge Beta" extraClasses={['mod-beta mt1']} />
+                        <Badge label="Generic/Grey" />
+                        <Badge label="Generic/Navy" extraClasses={['mod-information ml1']} />
+                        <Badge label="Semantic/Success" extraClasses={['mod-success ml1']} />
+                        <Badge label="Semantic/Critical" extraClasses={['mod-critical ml1']} />
+                        <Badge label="New" extraClasses={['mod-warning ml1']} />
+                        <Badge label="Beta" extraClasses={['mod-beta ml1']} />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Badge with Icon</label>
                     <div className="form-control">
-                        <Badge label="Badge label" icon="lock" />
+                        <Badge label="Label" icon="lock" />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Small Badge</label>
                     <div className="form-control">
-                        <Badge label="Badge Generic/Grey" extraClasses={['mod-small']} />
-                        <Badge label="Badge Generic/Navy" extraClasses={['mod-small mod-information ml1']} />
-                        <Badge label="Badge Semantic/Success" extraClasses={[' mod-small mod-success ml1']} />
-                        <Badge label="Badge Semantic/Critical" extraClasses={['mod-small mod-critical ml1']} />
-                        <Badge label="Badge New" extraClasses={['mod-small mod-warning ml1']} />
-                        <Badge label="Badge Beta" extraClasses={['mod-small mod-beta ml1 mt1']} />
+                        <Badge label="Generic/Grey" extraClasses={['mod-small']} />
+                        <Badge label="Generic/Navy" extraClasses={['mod-small mod-information ml1']} />
+                        <Badge label="Semantic/Success" extraClasses={[' mod-small mod-success ml1']} />
+                        <Badge label="Semantic/Critical" extraClasses={['mod-small mod-critical ml1']} />
+                        <Badge label="New" extraClasses={['mod-small mod-warning ml1']} />
+                        <Badge label="Beta" extraClasses={['mod-small mod-beta ml1']} />
                     </div>
                 </div>
             </div>
