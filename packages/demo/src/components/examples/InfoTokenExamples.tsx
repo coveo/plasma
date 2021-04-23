238670456d98d543e19210eafe928d5487210aef
aiheon <aiheon@coveo.com>
2021-04-23 15:49:03 -0400

    feat(info-token): added all the svg + completed the demo page

--- packages/demo/src/components/examples/InfoTokenExamples.tsx
+++ packages/demo/src/components/examples/InfoTokenExamples.tsx
@@ -6,52 +6,69 @@ export class InfoTokenExamples extends React.Component<any, any> {
         return (
             <div className="mt2">
                 <div className="form-group">
-                    <label className="form-control-label">Warnings tokens</label>
+                    <label className="form-control-label">Information tokens</label>
                     <div className="form-control">
-                        <Svg svgName={'info-token-full-critical-16'} />
-                        {/* <InfoToken icon="info-token-warning" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-warning" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-warning" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-information-16'} />
+                        <Svg svgName={'info-token-full-information-24'} />
+                        <Svg svgName={'info-token-full-information-32'} />
+                        <Svg svgName={'info-token-stroked-information-16'} />
+                        <Svg svgName={'info-token-stroked-information-24'} />
+                        <Svg svgName={'info-token-stroked-information-32'} />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Success tokens</label>
                     <div className="form-control">
-                        {/* <InfoToken icon="info-token-success" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-success" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-success" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-success-16'} />
+                        <Svg svgName={'info-token-full-success-24'} />
+                        <Svg svgName={'info-token-full-success-32'} />
+                        <Svg svgName={'info-token-stroked-success-16'} />
+                        <Svg svgName={'info-token-stroked-success-24'} />
+                        <Svg svgName={'info-token-stroked-success-32'} />
                     </div>
                 </div>
                 <div className="form-group">
-                    <label className="form-control-label">Information tokens</label>
+                    <label className="form-control-label">Warnings tokens</label>
                     <div className="form-control">
-                        {/* <InfoToken icon="info-token-information" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-information" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-information" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-warning-16'} />
+                        <Svg svgName={'info-token-full-warning-24'} />
+                        <Svg svgName={'info-token-full-warning-32'} />
+                        <Svg svgName={'info-token-stroked-warning-16'} />
+                        <Svg svgName={'info-token-stroked-warning-24'} />
+                        <Svg svgName={'info-token-stroked-warning-32'} />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Tip tokens</label>
                     <div className="form-control">
-                        {/* <InfoToken icon="info-token-tip" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-tip" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-tip" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-tip-16'} />
+                        <Svg svgName={'info-token-full-tip-24'} />
+                        <Svg svgName={'info-token-full-tip-32'} />
+                        <Svg svgName={'info-token-stroked-tip-16'} />
+                        <Svg svgName={'info-token-stroked-tip-24'} />
+                        <Svg svgName={'info-token-stroked-tip-32'} />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Critical tokens</label>
                     <div className="form-control">
-                        {/* <InfoToken icon="info-token-critical" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-critical" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-critical" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-critical-16'} />
+                        <Svg svgName={'info-token-full-critical-24'} />
+                        <Svg svgName={'info-token-full-critical-32'} />
+                        <Svg svgName={'info-token-stroked-critical-16'} />
+                        <Svg svgName={'info-token-stroked-critical-24'} />
+                        <Svg svgName={'info-token-stroked-critical-32'} />
                     </div>
                 </div>
                 <div className="form-group">
                     <label className="form-control-label">Question tokens</label>
                     <div className="form-control">
-                        {/* <InfoToken icon="info-token-question" extraClasses={['ml1']} />
-                        <InfoToken icon="info-token-question" extraClasses={['mod-small ml1']} />
-                        <InfoToken icon="info-token-question" extraClasses={['mod-smaller ml1']} /> */}
+                        <Svg svgName={'info-token-full-question-16'} />
+                        <Svg svgName={'info-token-full-question-24'} />
+                        <Svg svgName={'info-token-full-question-32'} />
+                        <Svg svgName={'info-token-stroked-question-16'} />
+                        <Svg svgName={'info-token-stroked-question-24'} />
+                        <Svg svgName={'info-token-stroked-question-32'} />
                     </div>
                 </div>
             </div>
