6da23ab5db34c352cc982e9c7565ab9b1e4e0201
Gaël Dostie <gdostie@coveo.com>
2021-05-10 16:36:13 -0400

    feat(links): improve links style
    Links style have changed in the designs. Since their inception is not finished by the design team,
    here is a early refresh of the links styles to allow me to continue with the IconCard.

--- packages/demo/src/styles/typography/index.tsx
+++ packages/demo/src/styles/typography/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Redirect, Route, RouteComponentProps} from 'react-router-dom';
 
 import Headings from './Headings';
+import Links from './Links';
 import Lists from './Lists';
 import Utilities from './Utilities';
 
@@ -10,6 +11,7 @@ const Typography: React.FunctionComponent<RouteComponentProps> = ({match}) => (
         <Route path={`${match.url}/headings`} component={Headings} />
         <Route path={`${match.url}/utilities`} component={Utilities} />
         <Route path={`${match.url}/lists`} component={Lists} />
+        <Route path={`${match.url}/links`} component={Links} />
         <Route exact path={`${match.url}/`} component={() => <Redirect to={`${match.url}/headings`} />} />
     </>
 );
