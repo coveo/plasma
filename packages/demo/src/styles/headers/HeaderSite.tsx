17a108499e2f7e5a484a8fd3dc5de5b1e93581ab
Rosalie Liu <52677246+rosalie-liu@users.noreply.github.com>
2021-05-27 10:52:12 -0400

    feat(header): update style and add icons (#2004)

--- packages/demo/src/styles/headers/HeaderSite.tsx
+++ packages/demo/src/styles/headers/HeaderSite.tsx
@@ -9,7 +9,7 @@ export const HeaderSite = () => (
         usage="Use as your web application header. Note that the 9px gap at the bottom of the header is normal and should be hidden by the application container."
         withSource
     >
-        <div className="header flex">
+        <div className="header">
             <div className="header-section mod-padded flex flex-center">[LOGO]</div>
             <div className="flex-auto" />
             <div className="header-section mod-padded flex flex-center">Section 1, mod-padded</div>
