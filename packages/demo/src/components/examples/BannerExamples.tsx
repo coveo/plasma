ea388c6390291bdcce16fbcbbd6d851b46ed1d2f
aiheon <aiheon@coveo.com>
2021-06-08 15:41:49 -0400

    fix: remove useless tag

--- packages/demo/src/components/examples/BannerExamples.tsx
+++ packages/demo/src/components/examples/BannerExamples.tsx
@@ -4,10 +4,8 @@ import {BannerContainer, Section} from 'react-vapor';
 export class BannerExamples extends React.Component {
     render() {
         return (
-            <Section>
-                <Section level={3} title="Empty banner">
-                    <BannerContainer />
-                </Section>
+            <Section level={3} title="Empty banner">
+                <BannerContainer />
             </Section>
         );
     }
