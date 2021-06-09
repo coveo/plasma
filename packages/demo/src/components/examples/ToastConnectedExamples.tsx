7425e416f713e9874dbc012b7163bd4e6fab5abf
aiheon <aiheon@coveo.com>
2021-06-09 10:46:13 -0400

    fix: applied review part one

--- packages/demo/src/components/examples/ToastConnectedExamples.tsx
+++ packages/demo/src/components/examples/ToastConnectedExamples.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {addToast, ReduxConnect, Svg, ToastContainerConnected, ToastType} from 'react-vapor';
+import {addToast, ReduxConnect, Section, Svg, ToastContainerConnected, ToastType} from 'react-vapor';
 
 import {ToastContentExample} from './ToastContentExample';
 
@@ -18,122 +18,120 @@ export class ToastConnectedExamples extends React.Component<IToastConnectedExamp
 
     render() {
         return (
-            <div className="mt2">
-                <div className="form-group">
-                    <div className="flex flex-wrap">
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Success!', {content: toastDescription});
-                            }}
-                        >
-                            Basic
-                        </button>
+            <Section>
+                <div className="flex flex-wrap">
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Success!', {content: toastDescription});
+                        }}
+                    >
+                        Basic
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Information!', {
-                                    type: ToastType.Info,
-                                    content: toastDescription,
-                                });
-                            }}
-                        >
-                            Information
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Information!', {
+                                type: ToastType.Info,
+                                content: toastDescription,
+                            });
+                        }}
+                    >
+                        Information
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Warning!', {
-                                    type: ToastType.Warning,
-                                    content: toastDescription,
-                                });
-                            }}
-                        >
-                            Warning
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Warning!', {
+                                type: ToastType.Warning,
+                                content: toastDescription,
+                            });
+                        }}
+                    >
+                        Warning
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Error!', {
-                                    type: ToastType.Error,
-                                    content: toastDescription,
-                                });
-                            }}
-                        >
-                            Error
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Error!', {
+                                type: ToastType.Error,
+                                content: toastDescription,
+                            });
+                        }}
+                    >
+                        Error
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Timed Success!', {
-                                    dismiss: 1000,
-                                    content: toastDescription,
-                                });
-                            }}
-                        >
-                            Timed
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Timed Success!', {
+                                dismiss: 1000,
+                                content: toastDescription,
+                            });
+                        }}
+                    >
+                        Timed
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Success!', {content: 'String content'});
-                            }}
-                        >
-                            String Content
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Success!', {content: 'String content'});
+                        }}
+                    >
+                        String Content
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Success!', {
-                                    content: () => <a href="#">JSX Element</a>,
-                                });
-                            }}
-                        >
-                            JSX Content
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Success!', {
+                                content: () => <a href="#">JSX Element</a>,
+                            });
+                        }}
+                    >
+                        JSX Content
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Success!', {content: ToastContentExample});
-                            }}
-                        >
-                            React Component Content
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Success!', {content: ToastContentExample});
+                        }}
+                    >
+                        React Component Content
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(containerId, 'Success!', {children: <ToastContentExample />});
-                            }}
-                        >
-                            React Component Content with children
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(containerId, 'Success!', {children: <ToastContentExample />});
+                        }}
+                    >
+                        React Component Content with children
+                    </button>
 
-                        <button
-                            className="btn m0 mr1 mb1"
-                            onClick={() => {
-                                this.props.addToast(downloadContainerId, 'Preparing file for download...', {
-                                    children: <div>Some file.csv</div>,
-                                    isDownload: true,
-                                });
-                            }}
-                        >
-                            <Svg svgName="download" svgClass="icon mod-lg mr1" />
-                            Prepare Download
-                        </button>
+                    <button
+                        className="btn m0 mr1 mb1"
+                        onClick={() => {
+                            this.props.addToast(downloadContainerId, 'Preparing file for download...', {
+                                children: <div>Some file.csv</div>,
+                                isDownload: true,
+                            });
+                        }}
+                    >
+                        <Svg svgName="download" svgClass="icon mod-lg mr1" />
+                        Prepare Download
+                    </button>
 
-                        <ToastContainerConnected id={containerId} />
-                        <ToastContainerConnected id={downloadContainerId} bottom left />
-                    </div>
+                    <ToastContainerConnected id={containerId} />
+                    <ToastContainerConnected id={downloadContainerId} bottom left />
                 </div>
-            </div>
+            </Section>
         );
     }
 }
