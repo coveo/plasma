3b7304044bd57421485b078c9fc01d189f281d8e
Gaël Dostie <gdostie@coveo.com>
2021-06-30 14:21:13 -0400

    refactor(tabs): merge connected and unconnected components together
    Merge connected and unconnected components in the same files and do some cleanup.

--- packages/react-vapor/src/components/tab/TabPane.tsx
+++ packages/react-vapor/src/components/tab/TabPane.tsx
@@ -1,25 +1,25 @@
 import classNames from 'classnames';
 import * as React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import {ConnectedProps} from '../../utils';
 import {IClassName} from '../../utils/ClassNameUtils';
+import {TabSelectors} from './TabSelectors';
 
 export interface ITabPaneOwnProps {
     groupId?: string;
     id?: string;
     className?: IClassName;
+    children?: React.ReactNode;
 }
 
-export interface ITabPaneStateProps {
-    isActive?: boolean;
-}
+const enhance = connect(createStructuredSelector({isActive: TabSelectors.getIsTabSelected}));
 
-export interface ITabPaneDispatchProps {}
+export interface ITabPaneProps extends ITabPaneOwnProps, Partial<ConnectedProps<typeof enhance>> {}
 
-export interface ITabPaneProps extends ITabPaneOwnProps, ITabPaneStateProps, ITabPaneDispatchProps {}
+export const TabPane: React.FunctionComponent<ITabPaneProps> = ({children, className, isActive}) => (
+    <div className={classNames('tab-pane', className, {active: isActive})}>{children}</div>
+);
 
-export class TabPane extends React.Component<ITabPaneProps, any> {
-    render() {
-        const classes = classNames('tab-pane', this.props.className, {active: this.props.isActive});
-
-        return <div className={classes}>{this.props.children}</div>;
-    }
-}
+export const TabPaneConnected = enhance(TabPane);
