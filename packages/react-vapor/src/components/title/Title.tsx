55da5e0263bb7e04614346316d94ce73724d49f7
dmgauthier <dgauthier@coveo.com>
2021-09-15 12:59:35 -0400

    fix(title): tooltip appear if truncated

--- packages/react-vapor/src/components/title/Title.tsx
+++ packages/react-vapor/src/components/title/Title.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import classNames from 'classnames';
 import * as React from 'react';
 import * as _ from 'underscore';
@@ -15,23 +16,33 @@ export interface ITitleProps {
 }
 
 export const Title: React.FunctionComponent<ITitleProps> = (props) => {
+    const ref = React.useRef<HTMLHRElement>();
+    const [isTruncated, setIsTruncated] = React.useState(false);
     const linkClasses = classNames('inline-doc-link', props.documentationLink && props.documentationLink.linkClasses);
     const titleClasses: string = classNames('bolder', 'mr1', 'truncate', props.classes);
     const prefixClasses: string = classNames({mr1: !_.isEmpty(props.prefix)});
-
     const linkIcon = props.documentationLink && <LinkSvg {...props.documentationLink} linkClasses={[linkClasses]} />;
     const tooltipProps = _.isString(props.text) ? {title: props.text} : {};
-    const title = props.withTitleTooltip ? (
-        <Tooltip {...tooltipProps} placement="left">
-            {props.text}
-        </Tooltip>
-    ) : (
-        props.text
-    );
+
+    const detection = () => {
+        const titleOffSetWidth = ref.current.offsetWidth;
+        const titleScrollWidth = ref.current.scrollWidth;
+
+        setIsTruncated(titleOffSetWidth < titleScrollWidth);
+    };
+
+    const title =
+        props.withTitleTooltip || isTruncated ? (
+            <Tooltip {...tooltipProps} placement="left">
+                {props.text}
+            </Tooltip>
+        ) : (
+            props.text
+        );
 
     return (
-        <div className="flex flex-center full-content-x">
-            <h4 className={titleClasses} id={props.htmlId}>
+        <div className="flex flex-center full-content-x" onMouseEnter={detection}>
+            <h4 ref={ref} className={titleClasses} id={props.htmlId}>
                 <span className={prefixClasses}>{props.prefix}</span>
                 {title}
             </h4>
