d0d2ada53a85adbab88c97dc77ca9a7012f1ae28
Gaël Dostie <gdostie@coveo.com>
2021-10-25 16:09:03 -0400

    ci: refactor/clarify jenkinsfile

--- packages/react-vapor/src/Version.ts
+++ packages/react-vapor/src/Version.ts
@@ -1,3 +1,2 @@
-// process.env.REACT_VAPOR_VERSION is a variable defined in bundler files: webpack.config.js, rollup.config.js
-// The version is loaded from the package.json and exported
-export const version = process.env.REACT_VAPOR_VERSION;
+// This is a placeholder that gets replaced by the actual version number in the postversion script
+export const version = '||||VERSION||||';
