d87ac0813a242fc74ccc0a98d3f8177be4407da7
Jack <jmcgregor@coveo.com>
2021-11-17 16:58:03 -0500

    feat(notfound): updated notfound and index

--- packages/demo/src/components/NotFound.tsx
+++ packages/demo/src/components/NotFound.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import {useRouteMatch} from 'react-router';
 
-export const NotFound: React.FC = () => (
-    <div className="mod-width-100 p20 flex center-align">
-        <h3>Page Not Found</h3>
-    </div>
-);
+export const NotFound: React.FC = () => {
+    const match = useRouteMatch();
+
+    return (
+        <div className="mod-width-100 p20 flex flex-column center-align">
+            <h3>Page Not Found</h3>
+            <p>{match.url}</p>
+        </div>
+    );
+};
