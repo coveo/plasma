ee7a0dd78c439be1d94e923cf339c6313249f567
Jack <jmcgregor@coveo.com>
2021-11-22 12:47:45 -0500

    feat(routes): foundation

--- packages/demo/src/plasma/routes/foundation/index.tsx
+++ packages/demo/src/plasma/routes/foundation/index.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, useRouteMatch} from 'react-router-dom';
 
-export const FoundationsRoutes: React.FunctionComponent = () => (
-    <Switch>
-        <Route exact path="/Effects" component={() => <div />} />
-        <Route exact path="/Iconography" component={() => <div />} />
-        <Route exact path="/Illustration" component={() => <div />} />
-        <Route exact path="/Palette" component={() => <div />} />
-        <Route exact path="/Typekit" component={() => <div />} />
-    </Switch>
-);
+import {IconsList} from './pages/IconsList';
+
+export const FoundationsRoutes: React.FunctionComponent = () => {
+    const {path} = useRouteMatch();
+
+    return (
+        <Switch>
+            <Route path={`${path}/Effects`} component={() => <div />} />
+            <Route path={`${path}/Iconography`} component={() => <IconsList />} />
+            <Route path={`${path}/Illustration`} component={() => <div />} />
+            <Route path={`${path}/Palette`} component={() => <div />} />
+            <Route path={`${path}/Typekit`} component={() => <div />} />
+        </Switch>
+    );
+};
