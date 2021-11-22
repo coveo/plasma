185b0d930df36dc6c6d515649d0f14244f270d72
Jack <jmcgregor@coveo.com>
2021-11-22 12:45:58 -0500

    feat(route): display and utilities

--- packages/demo/src/plasma/routes/display-and-utilities/index.tsx
+++ packages/demo/src/plasma/routes/display-and-utilities/index.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, useRouteMatch} from 'react-router-dom';
+import {Borders} from './pages/Borders';
+import {Collapsible} from './pages/CollapsibleExamples';
 
-export const DisplayAndUtilitiesRoutes: React.FunctionComponent = () => (
-    <Switch>
-        <Route path="/borders" component={() => <div />} />
-        <Route path="/Collapsible" component={() => <div />} />
-        <Route path="/CoveoExpLoader" component={() => <div />} />
-        <Route path="/RichPopover" component={() => <div />} />
-        <Route path="/SkeletonBlur" component={() => <div />} />
-    </Switch>
-);
+export const DisplayAndUtilitiesRoutes: React.FunctionComponent = () => {
+    const {path} = useRouteMatch();
+
+    return (
+        <Switch>
+            <Route path={`${path}/Borders`} component={() => <Borders />} />
+            <Route path={`${path}/Collapsible`} component={() => <Collapsible />} />
+            <Route path={`${path}/CoveoExpLoader`} component={() => <div />} />
+            <Route path={`${path}/RichPopover`} component={() => <div />} />
+            <Route path={`${path}/SkeletonBlur`} component={() => <div />} />
+        </Switch>
+    );
+};
