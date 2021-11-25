4b90108fcc25282b6d41f9194f9d80b0f8bb356c
aiheon <aiheon@coveo.com>
2021-11-25 16:08:30 -0500

    feat(homepage): wIP intro section

--- packages/demo/src/plasma/Home.tsx
+++ packages/demo/src/plasma/Home.tsx
@@ -1,8 +1,18 @@
+import '@demo-styling/home.scss';
+
 import React from 'react';
 
+const MAIN_DESCRIPTION: string =
+    'Coveo’s platform design system & ionized Vapor.\n Learn more about our brand, our values and our story by visiting our brand page';
+
+const MAIN_TITLE: string = 'PLASMA';
+const WELCOME: string = 'Welcome to';
+
 const Home: React.FC = () => (
-    <div>
-        <h2>Plasma</h2>
+    <div className="intro-section">
+        <h5 className="welcome-to">{WELCOME}</h5>
+        <div className="page-title">{MAIN_TITLE}</div>
+        <div className="body-l-book plasma-description">{MAIN_DESCRIPTION}</div>
     </div>
 );
 
