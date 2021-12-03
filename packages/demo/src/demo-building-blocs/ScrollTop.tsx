f8c669fa5573a80d02317f93aa0c0414c8642731
aiheon <aiheon@coveo.com>
2021-12-03 13:17:09 -0500

    chore(typescript): bump to typescript 4 for the demo package

--- packages/demo/src/demo-building-blocs/ScrollTop.tsx
+++ packages/demo/src/demo-building-blocs/ScrollTop.tsx
@@ -14,4 +14,4 @@ const ScrollToTop: React.FunctionComponent<RouteComponentProps> = ({history}) =>
     return null;
 };
 
-export default withRouter(ScrollToTop);
+export default withRouter(ScrollToTop) as any;
