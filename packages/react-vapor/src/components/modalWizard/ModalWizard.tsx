1631ca466f2ad05e0b4cd6bf87dc73042433c491
nlafrance-coveo <nlafrance@coveo.com>
2021-12-07 10:25:31 -0500

    feat(modal): passing modalBodyClasses to actual rendered body container

--- packages/react-vapor/src/components/modalWizard/ModalWizard.tsx
+++ packages/react-vapor/src/components/modalWizard/ModalWizard.tsx
@@ -77,7 +77,11 @@ const ModalWizardDisconneted: React.FunctionComponent<ModalWizardProps & Connect
                             {steps.map((step: React.ReactElement, index: number) => {
                                 const hidden = index !== currentStep;
                                 return (
-                                    <div className={classNames({hidden})} hidden={hidden} key={index}>
+                                    <div
+                                        className={classNames({hidden}, modalProps.modalBodyClasses)}
+                                        hidden={hidden}
+                                        key={index}
+                                    >
                                         {step}
                                     </div>
                                 );
