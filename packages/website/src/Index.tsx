146c7cdd3c6ff9c884924e37d6cccc9f02ef2358
Gaël Dostie <gdostie@coveo.com>
2022-02-09 12:02:34 -0500

    temporary: just to capture the loading animation

--- packages/website/src/Index.tsx
+++ packages/website/src/Index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as ReactRedux from 'react-redux';
-import {HashRouter} from 'react-router-dom';
+// import {HashRouter} from 'react-router-dom';
 import * as PlasmaReact from '@coveord/plasma-react';
 
-import {App} from './OneDemoToRuleThemAll';
-import {Store} from './Store';
+// import {App} from './OneDemoToRuleThemAll';
+// import {Store} from './Store';
 
 PlasmaReact.Defaults.APP_ELEMENT = '#App';
 PlasmaReact.Defaults.MODAL_ROOT = '#Modals';
@@ -20,11 +20,11 @@ if (window.location.host === 'vapor.coveo.com') {
     window.location.href = window.location.href.replace('vapor.coveo.com', 'plasma.coveo.com');
 }
 
-ReactDOM.render(
-    <HashRouter>
-        <ReactRedux.Provider store={Store}>
-            <App />
-        </ReactRedux.Provider>
-    </HashRouter>,
-    document.getElementById('App')
-);
+// ReactDOM.render(
+//     <HashRouter>
+//         <ReactRedux.Provider store={Store}>
+//             <App />
+//         </ReactRedux.Provider>
+//     </HashRouter>,
+//     document.getElementById('App')
+// );
