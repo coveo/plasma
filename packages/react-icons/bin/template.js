4faefdb406187a3f87c131c666f1523da2617375
Gaël Dostie <gdostie@coveo.com>
2022-02-25 15:38:59 -0500

    feat(react-icons): add verticalAlign text-bottom to all icons

---
+++ packages/react-icons/bin/template.js
@@ -0,0 +1,52 @@
+const t = require('@babel/types');
+
+const findSvgTag = (variables) => variables.jsx.openingElement;
+
+const addAttributeToSvgTag = (svgTag, attribute) => {
+    svgTag.attributes.push(attribute);
+};
+
+const deconstructSvgProps = (props) => {
+    const svgProps = t.objectPattern([
+        ...props.map((prop) => t.objectProperty(t.identifier(prop), t.identifier(prop), false, true)),
+        t.restElement(t.identifier('svgProps')),
+    ]);
+    svgProps.typeAnnotation = t.tsTypeAnnotation(
+        t.tsTypeReference(
+            t.identifier('SVGProps'),
+            t.tsTypeParameterInstantiation([t.tsTypeReference(t.identifier('SVGSVGElement'))])
+        )
+    );
+    return svgProps;
+};
+
+const template = (variables, {tpl}) => {
+    const svgTag = findSvgTag(variables);
+    const styleAttr = t.jsxAttribute(
+        t.jsxIdentifier('style'),
+        t.jsxExpressionContainer(
+            t.objectExpression([
+                t.objectProperty(t.identifier('verticalAlign'), t.stringLiteral('text-bottom')),
+                t.spreadElement(t.identifier('style')),
+            ])
+        )
+    );
+    const svgPropsAttr = t.jsxSpreadAttribute(t.identifier('svgProps'));
+    
+    addAttributeToSvgTag(svgTag, styleAttr);
+    addAttributeToSvgTag(svgTag, svgPropsAttr);
+
+    const props = [deconstructSvgProps(['width', 'height', 'style'])];
+
+    return tpl`
+${variables.imports};
+
+import {SVGProps} from 'react';
+
+const ${variables.componentName} = (${props}) => (${variables.jsx});
+ 
+${variables.exports};
+`;
+};
+
+module.exports = template;
