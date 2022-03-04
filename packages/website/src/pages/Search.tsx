497789400d460ff31a3560f013db70657e941c2e
aiheon <aiheon@coveo.com>
2022-03-04 11:54:07 -0500

    feat(atomic): wIP search is working but everything is ugly

--- packages/website/src/pages/Search.tsx
+++ packages/website/src/pages/Search.tsx
@@ -1,81 +1,84 @@
-import {
-    buildResultList,
-    loadClickAnalyticsActions,
-    loadPaginationActions,
-    loadQueryActions,
-    loadSearchActions,
-    loadSearchAnalyticsActions,
-    ResultList as HeadlessResultList,
-    SearchEngine,
-} from '@coveo/headless';
 import {Section} from '@coveord/plasma-react';
-import {useRouter} from 'next/router';
 
 import {FunctionComponent, useContext, useEffect, useState} from 'react';
 import React from 'react';
 
+import {AtomicNoResults, AtomicQuerySummary, AtomicResultList, AtomicSearchInterface} from '@coveo/atomic-react';
 import {Tile, TileProps} from '../building-blocs/Tile';
 import {EngineContext} from '../search/engine/EngineContext';
 import {NoSearchResultTemplate} from '../search/NoSearchResultTemplate';
 
-interface ResultListProps {
-    controller: HeadlessResultList;
-    engine: SearchEngine;
-    query: string;
-}
+// interface ResultListProps {
+//     controller: HeadlessResultList;
+//     engine: SearchEngine;
+//     query: string;
+// }
 
-const ResultListRenderer: FunctionComponent<ResultListProps> = (props) => {
-    const {controller, engine, query} = props;
-    const [state, setState] = useState(controller.state);
-    const {logDocumentOpen} = loadClickAnalyticsActions(engine);
+// const ResultListRenderer: FunctionComponent<ResultListProps> = (props) => {
+//     const {controller, engine, query} = props;
+//     const [state, setState] = useState(controller.state);
+//     const {logDocumentOpen} = loadClickAnalyticsActions(engine);
 
-    useEffect(() => controller.subscribe(() => setState(controller.state)), [controller]);
+//     useEffect(() => controller.subscribe(() => setState(controller.state)), [controller]);
 
-    return (
-        <>
-            {!state.hasResults && !state.isLoading ? (
-                <NoSearchResultTemplate engine={engine} query={query} />
-            ) : (
-                <Section className="home flex-auto overflow-auto demo-content">
-                    <Section className="section">
-                        <h4 className="h4-book">
-                            Showing results for <span className="h4">{query}</span>
-                        </h4>
-                        <p>
-                            <span className="body-m">{state.results.length}</span>
-                            {state.results.length === 1 ? ' result' : ' results'}
-                        </p>
-                        <div className="tile-grid">
-                            {state.results.map((result) => (
-                                <Tile
-                                    key={result.uniqueId}
-                                    title={result.title}
-                                    href={result.clickUri.replace(/.+plasma\.coveo\.com\//, '')}
-                                    description={result.raw.description as string}
-                                    thumbnail={result.raw.thumbnail as TileProps['thumbnail']}
-                                    sendAnalytics={() => engine.dispatch(logDocumentOpen(result))}
-                                />
-                            ))}
-                        </div>
-                    </Section>
-                </Section>
-            )}
-        </>
-    );
-};
+//     return (
+//         <>
+//             {!state.hasResults && !state.isLoading ? (
+//                 <NoSearchResultTemplate engine={engine} query={query} />
+//             ) : (
+//                 <Section className="home flex-auto overflow-auto demo-content">
+//                     <Section className="section">
+//                         <h4 className="h4-book">
+//                             Showing results for <span className="h4">{query}</span>
+//                         </h4>
+//                         <p>
+//                             <span className="body-m">{state.results.length}</span>
+//                             {state.results.length === 1 ? ' result' : ' results'}
+//                         </p>
+//                         <div className="tile-grid">
+//                             {state.results.map((result) => (
+//                                 <Tile
+//                                     key={result.uniqueId}
+//                                     title={result.title}
+//                                     href={result.clickUri.replace(/.+plasma\.coveo\.com\//, '')}
+//                                     description={result.raw.description as string}
+//                                     thumbnail={result.raw.thumbnail as TileProps['thumbnail']}
+//                                     sendAnalytics={() => engine.dispatch(logDocumentOpen(result))}
+//                                 />
+//                             ))}
+//                         </div>
+//                     </Section>
+//                 </Section>
+//             )}
+//         </>
+//     );
+// };
 
 const Search = () => {
     const engine = useContext(EngineContext);
-    const {query} = useRouter();
-    const {updateQuery} = loadQueryActions(engine);
-    const {logInterfaceLoad} = loadSearchAnalyticsActions(engine);
-    const {registerNumberOfResults} = loadPaginationActions(engine);
-    const {executeSearch} = loadSearchActions(engine);
-    engine.dispatch(registerNumberOfResults(1000));
-    engine.dispatch(updateQuery({q: query.q as string}));
-    // engine.dispatch(executeSearch(logInterfaceLoad()));
-    const controller = buildResultList(engine);
-    return <ResultListRenderer controller={controller} engine={engine} query={(query?.q as string) ?? ''} />;
+    return (
+        <Section className="home flex-auto overflow-auto demo-content">
+            <AtomicSearchInterface engine={engine}>
+                <Section className="section">
+                    <AtomicQuerySummary></AtomicQuerySummary>
+                </Section>
+                <AtomicNoResults enableCancelLastAction={true}></AtomicNoResults>
+
+                <AtomicResultList
+                    display={'grid'}
+                    template={(result) => (
+                        <Tile
+                            key={result.uniqueId}
+                            title={result.title}
+                            href={result.clickUri.replace(/.+plasma\.coveo\.com\//, '')}
+                            description={result.raw.description as string}
+                            thumbnail={result.raw.thumbnail as TileProps['thumbnail']}
+                        />
+                    )}
+                />
+            </AtomicSearchInterface>
+        </Section>
+    );
 };
 
 export default Search;
--- packages/website/src/search/StandaloneSearchBar.tsx
+++ packages/website/src/search/StandaloneSearchBar.tsx
@@ -1,123 +1,28 @@
-import {buildSearchBox, SearchBox, SearchBoxState} from '@coveo/headless';
-import {Button, IItemBoxProps, ListBox, Svg} from '@coveord/plasma-react';
-import classNames from 'classnames';
-import {useRouter} from 'next/router';
-import {FunctionComponent, useContext, useEffect, useState} from 'react';
+import {AtomicSearchBox, AtomicSearchInterface} from '@coveo/atomic-react';
+
+import {useContext} from 'react';
 import React from 'react';
 
-import {AtomicSearchBox, AtomicSearchInterface} from '@coveo/atomic-react';
-import {FeatureFlags} from '../FeatureFlags';
 import {EngineContext} from './engine/EngineContext';
-
-// interface SearchBarProps {
-//     controller: SearchBox;
-// }
-
-// const SearchBoxRerender: FunctionComponent<SearchBarProps> = (props) => {
-//     const {controller} = props;
-//     const [state, setState] = useState<SearchBoxState>(controller.state);
-//     const [focused, setFocused] = useState(false);
-//     const [showQuerySuggesttions, setShowQuerySuggesttions] = useState(false);
-//     const router = useRouter();
-
-//     const navigateToResultPage = (query: string) => {
-//         router.push(`/Search?q=${query}`);
-//     };
-
-//     useEffect(() => {
-//         setShowQuerySuggesttions(FeatureFlags.get('query-suggestions') as boolean);
-//     }, []);
-
-//     useEffect(() => controller.subscribe(() => setState(controller.state)), [controller]);
-
-//     const ClearButton = () => (
-//         <button
-//             disabled={state.value === ''}
-//             className={classNames('clear-button', {
-//                 'search-not-empty': state.value !== '',
-//             })}
-//             onClick={() => controller.clear()}
-//         >
-//             <Svg svgName="cross" svgClass="icon" />
-//         </button>
-//     );
-
-//     const SearchButton = () => (
-//         <Button
-//             classes={['search-button']}
-//             onClick={() => {
-//                 navigateToResultPage(state.value);
-//             }}
-//         >
-//             <Svg svgName="search" className="icon mod-stroke" />
-//         </Button>
-//     );
-
-//     const SuggestionListBox = () => {
-//         const results: IItemBoxProps[] = state.suggestions.map((result) => ({
-//             value: result.rawValue,
-//             displayValue: result.highlightedValue,
-//         }));
-
-//         return (
-//             <>
-//                 {focused && (state.suggestions.length > 0 || state.isLoadingSuggestions) && (
-//                     <ListBox
-//                         classes={['search-results-container sentence-case']}
-//                         isLoading={state.isLoadingSuggestions}
-//                         items={results}
-//                         onOptionClick={(item) => {
-//                             controller.selectSuggestion(item.value);
-//                             navigateToResultPage(item.value);
-//                         }}
-//                     />
-//                 )}
-//             </>
-//         );
-//     };
-
-//     return (
-//         <div className="plasmaSearchBar">
-//             <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-//                 <input
-//                     className="search-bar"
-//                     type="search"
-//                     placeholder={'Find a component...'}
-//                     value={state.value}
-//                     onChange={(event) => controller.updateText(event.target.value)}
-//                     onKeyDown={(event) => {
-//                         if (event.key === 'Enter') {
-//                             navigateToResultPage(state.value);
-//                         } else if (event.key === 'Escape') {
-//                             controller.clear();
-//                             (event.target as HTMLInputElement).blur();
-//                         }
-//                     }}
-//                     onFocus={() => setFocused(true)}
-//                     onBlur={() => setFocused(false)}
-//                 />
-//                 <ClearButton />
-//                 <SearchButton />
-//                 {/*
-//                     To toggle the feature flag, copy and paste those commands in the dev tool console: *
-
-//                         localStorage.setItem('query-suggestions', true) to show the query suggestions *
-//                         localStorage.setItem('query-suggestions', false) to hide the query suggestions *
-
-//                     You need to reload the page for it to take effect.
-//                 */}
-//                 {showQuerySuggesttions ? <SuggestionListBox /> : null}
-//             </form>
-//         </div>
-//     );
-// };
 
 const StandaloneSearchBar = () => {
     const engine = useContext(EngineContext);
     return (
-        <AtomicSearchInterface engine={engine}>
-            <AtomicSearchBox placeholder="Find a component..." numberOfQueries={5}></AtomicSearchBox>
-        </AtomicSearchInterface>
+        <div className="plasmaSearchBar form search-bar-patate">
+            <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+                <AtomicSearchInterface
+                    engine={engine}
+                    reflectStateInUrl={true}
+                    onReady={async () => console.log('chuis ready bitch')} // cancel first search here
+                >
+                    <AtomicSearchBox
+                        numberOfQueries={5}
+                        redirectionUrl={'/Search'}
+                        placeholder={'coucou'}
+                    ></AtomicSearchBox>
+                </AtomicSearchInterface>
+            </form>
+        </div>
     );
 };
 
