6a99db2683cdac35e9ab1dd70b27a0ffa6bdaf04
aiheon <aiheon@coveo.com>
2022-03-04 11:49:52 -0500

    perf(nextjs): add watchOptions to improve hot reload

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -2,20 +2,24 @@ const withImages = require('next-images');
 const basePath = require('./build/getBasePath');
 const {patchWebpackConfig} = require('next-global-css');
 
+const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['@coveo/atomic-react']);
 
-module.exports = withTM(
-    withImages({
-        basePath: basePath.replace(/\/$/, ''), // remove last slash
-        env: {
-            basePath,
-        },
-        images: {
-            disableStaticImages: true,
-        },
-        webpack: (config, options) => {
-            patchWebpackConfig(config, options);
-            return config;
-        },
-    })
-);
+module.exports = withPlugins([withTM, withImages], {
+    basePath: basePath.replace(/\/$/, ''), // remove last slash
+    env: {
+        basePath,
+    },
+    images: {
+        disableStaticImages: true,
+    },
+    webpack: (config, options) => {
+        patchWebpackConfig(config, options);
+        if (process.env.NODE_ENV === 'development') {
+            config.watchOptions = {
+                poll: 10000,
+            };
+        }
+        return config;
+    },
+});
