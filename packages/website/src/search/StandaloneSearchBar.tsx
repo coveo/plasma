43307244ffd85080d6b774360f91b2b4f5c4bb15
aiheon <aiheon@coveo.com>
2022-03-08 13:43:48 -0500

    feat(atomic): wIP conditional redirection url

--- packages/website/src/search/StandaloneSearchBar.tsx
+++ packages/website/src/search/StandaloneSearchBar.tsx
@@ -3,10 +3,15 @@ import {AtomicSearchBox, AtomicSearchInterface} from '@coveo/atomic-react';
 import {useContext} from 'react';
 import React from 'react';
 
+import {useRouter} from 'next/router';
 import {EngineContext} from './engine/EngineContext';
 
 const StandaloneSearchBar = () => {
     const engine = useContext(EngineContext);
+    const {pathname} = useRouter();
+
+    const isSearchPage: boolean = pathname === '/Search';
+
     return (
         <div className="plasmaSearchBar form search-bar-patate">
             <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
@@ -14,12 +19,12 @@ const StandaloneSearchBar = () => {
                     engine={engine}
                     reflectStateInUrl={true}
                     // eslint-disable-next-line no-console
-                    onReady={async () => console.log('chuis ready')} // cancel first search here
+                    // onReady={async () => console.log(pathname)} // find what value is pathname
                 >
                     <AtomicSearchBox
                         numberOfQueries={5}
-                        redirectionUrl={'/Search'} // always redirect to real website
-                        placeholder={'coucou'}
+                        redirectionUrl={isSearchPage ? undefined : '/Search'} // always redirect to real website
+                        placeholder={'coucou'} // not working
                     ></AtomicSearchBox>
                 </AtomicSearchInterface>
             </form>
