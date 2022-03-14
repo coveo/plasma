8b8176102ef207260d446a4bb568f4c53f25c6a2
aiheon <aiheon@coveo.com>
2022-03-14 10:26:34 -0400

    chore: applied review to pr

--- packages/website/src/pages/navigation/Tabs.tsx
+++ packages/website/src/pages/navigation/Tabs.tsx
@@ -11,15 +11,6 @@ const withIcons = `
             <TabConnected groupId="banane" id="tab5" title="Pikachu" icon={'lightning'} />
             <TabConnected groupId="banane" id="tab6" title="Gyarados" icon={'crawlingBotStroked16'} iconModStroke />
             <TabConnected groupId="banane" id="tab7" title="Charmander" icon={'check'} />
-            <TabConnected
-                groupId="banane"
-                id="tab14"
-                title="Mr. Mime"
-                tooltip="I have an icon and a badge!"
-                icon="email"
-                badge={<Badge label="Success" extraClasses={['mod-success mod-small ml1']} />}
-            />
-            <TabConnected groupId="banane" id="tab8" title="Rapidash" icon={'details'} disabled />
         </TabNavigation>
         <TabContent>
             <TabPaneConnected groupId="banane" id="tab5">
@@ -30,12 +21,6 @@ const withIcons = `
             </TabPaneConnected>
             <TabPaneConnected groupId="banane" id="tab7">
                 <div className="mod-header-padding mod-form-top-bottom-padding">Content of the third tab .</div>
-            </TabPaneConnected>
-            <TabPaneConnected groupId="banane" id="tab8">
-                <div className="mod-header-padding mod-form-top-bottom-padding">Content of the fourth tab.</div>
-            </TabPaneConnected>
-            <TabPaneConnected groupId="banane" id="tab14">
-                <div className="mod-header-padding mod-form-top-bottom-padding">Content of the last tab.</div>
             </TabPaneConnected>
         </TabContent>
     </>
@@ -49,9 +34,7 @@ const code = `
     export default () => (
         <>
             <TabNavigation>
-                <TabConnected groupId="patate" id="tab1" title="Pikachu" />
                 <TabConnected groupId="patate" id="tab2" title="Gyarados" tooltip="I have a toolip!" />
-                <TabConnected groupId="patate" id="tab3" title="Charmander" />
                 <TabConnected
                     groupId="patate"
                     id="tab13"
@@ -62,20 +45,11 @@ const code = `
                 <TabConnected groupId="patate" id="tab4" title="Rapidash" tooltip="I'm disabled" disabled />
             </TabNavigation>
             <TabContent>
-                <TabPaneConnected groupId="patate" id="tab1">
-                    <div className="mod-header-padding mod-form-top-bottom-padding">Content of the first tab.</div>
-                </TabPaneConnected>
                 <TabPaneConnected groupId="patate" id="tab2">
                     <div className="mod-header-padding mod-form-top-bottom-padding">Content of the second tab.</div>
                 </TabPaneConnected>
-                <TabPaneConnected groupId="patate" id="tab3">
-                    <div className="mod-header-padding mod-form-top-bottom-padding">Content of the third tab .</div>
-                </TabPaneConnected>
                 <TabPaneConnected groupId="patate" id="tab4">
                     <div className="mod-header-padding mod-form-top-bottom-padding">Content of the fourth tab.</div>
-                </TabPaneConnected>
-                <TabPaneConnected groupId="patate" id="tab13">
-                    <div className="mod-header-padding mod-form-top-bottom-padding">Content of the fifth tab.</div>
                 </TabPaneConnected>
             </TabContent>
         </>
