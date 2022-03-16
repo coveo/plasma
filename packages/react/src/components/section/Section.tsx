10a3939a090a77af2159ad816a656c67ed3b2bcf
aiheon <aiheon@coveo.com>
2022-03-16 15:09:53 -0400

    feat(demo): update Section page

--- packages/react/src/components/section/Section.tsx
+++ packages/react/src/components/section/Section.tsx
@@ -4,10 +4,27 @@ import * as React from 'react';
 type SectionMods = 'mod-header-padding' | 'mod-form-top-bottom-padding' | 'material-card';
 
 export interface ISectionProps {
+    /**
+     * The text or custom JSX content to the title of the Section
+     */
     title?: React.ReactNode;
+    /**
+     * The text or custom JSX content to the description of the Section
+     */
     description?: React.ReactNode;
+    /**
+     * Additionnal CSS class to stle the Section
+     */
     className?: string;
+    /**
+     * Visual modifiers to apply on the component
+     */
     mods?: SectionMods | SectionMods[];
+    /**
+     * Determine the style of the Section
+     *
+     * @default 1
+     */
     level?: 1 | 2 | 3;
 }
 
