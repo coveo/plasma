502534301cc80bb4deb4017819efa62ddb31f559
Gaël Dostie <gdostie@coveo.com>
2022-03-16 17:06:28 -0400

    refactor(website): update ChildForm component demo page

--- packages/react/src/components/childForm/ChildForm.tsx
+++ packages/react/src/components/childForm/ChildForm.tsx
@@ -2,8 +2,14 @@ import classNames from 'classnames';
 import * as React from 'react';
 
 export interface IChildFormProps extends React.HTMLAttributes<HTMLDivElement> {
+    /**
+     * CSS classes to set on the container element
+     */
     className?: string;
-    disabled?: boolean; // @deprecated set your disabled state on each child component instead
+    /**
+     * @deprecated set your disabled state on each child component instead
+     */
+    disabled?: boolean;
 }
 
 export const ChildForm: React.FunctionComponent<IChildFormProps> = ({className, children}) => (
