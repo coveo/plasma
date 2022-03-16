bf9a3af56e1c53936a419304c11242e2a1a7c124
Gaël Dostie <gdostie@coveo.com>
2022-03-16 14:28:42 -0400

    refactor(website): update StickyFooter demo page

--- packages/react/src/components/stickyFooter/StickyFooter.tsx
+++ packages/react/src/components/stickyFooter/StickyFooter.tsx
@@ -2,8 +2,17 @@ import classNames from 'classnames';
 import * as React from 'react';
 
 export interface IStickyFooterProps {
+    /**
+     * Whether the footer is visible
+     */
     isOpened: boolean;
+    /**
+     * The unique identifier of the footer
+     */
     id?: string;
+    /**
+     * CSS classes to set on the footer element
+     */
     className?: string;
 }
 
