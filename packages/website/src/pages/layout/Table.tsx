201970296c03318c8d86dd6105b97faf59ba8322
aiheon <aiheon@coveo.com>
2022-03-17 15:41:33 -0400

    refactor(demo): first demo working

--- packages/website/src/pages/layout/Table.tsx
+++ packages/website/src/pages/layout/Table.tsx
@@ -4,11 +4,62 @@ import {PageLayout} from '../../building-blocs/PageLayout';
 
 const code = `
     import * as React from 'react';
-    import {TableHOC} from '@coveord/plasma-react';
+    import {Badge, TableHOC, TableRowConnected} from '@coveord/plasma-react';
 
     export default () => (
-        <div>hihi</div>
+        <TableHOC
+            id={'tableId'}
+            className="table"
+            data={data}
+            renderBody={() => renderRows}
+            tableHeader={renderHeader()}
+            showBorderTop
+            showBorderBottom
+        />
     );
+
+    const renderHeader = () => (
+        <thead>
+            <tr>
+                <th>City</th>
+                <th>Username</th>
+                <th>Badge</th>
+            </tr>
+        </thead>
+    );
+
+    const data = [
+        {
+            city: 'Québec',
+            username: 'germinator',
+            id: 'id-1',
+        },
+        {
+            city: 'Ottawa',
+            username: 'canitalktoyouaboutvisualtesting',
+            id: 'id-2',
+        },
+        {
+            city: 'Toronto',
+            username: 'kienposter',
+            id: 'id-3',
+        },
+        {
+            city: 'Montréal',
+            username: 'notfound',
+            id: 'id-3',
+        },
+    ];
+
+    const renderRows = data?.map((item) => (
+        <TableRowConnected id={item.id} tableId={'tableId'} key={item.id}>
+            <td key="city">{item.city}</td>
+            <td key="username">{item.username.toLowerCase()}</td>
+            <td>
+                <Badge label={'🥔 King'} extraClasses={['mod-small mod-success']} />
+            </td>
+        </TableRowConnected>
+    ));
 `;
 
 const TableExamples = () => (
