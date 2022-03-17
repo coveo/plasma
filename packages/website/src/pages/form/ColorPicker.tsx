c4ca7e9c11d0b696bc1d8757fe9efef1f0e2cd35
Germain Bergeron <gbergeron@coveo.com>
2022-03-17 15:50:16 -0400

    refactor(website, color picker): update example layout

--- packages/website/src/pages/form/ColorPicker.tsx
+++ packages/website/src/pages/form/ColorPicker.tsx
@@ -1,61 +1,67 @@
 import * as React from 'react';
-import {Button, ColorPicker, InputSelectors} from '@coveord/plasma-react';
-import {debounce} from 'underscore';
+import {PageLayout} from '../../building-blocs/PageLayout';
 
-import {Store} from '../../Store';
-import PlasmaComponent from '../../building-blocs/PlasmaComponent';
+const code = `
+    import * as React from 'react';
+    import {ColorPicker} from '@coveord/plasma-react';
+    
+    export default () => {
+        const logColor = (color) => console.log(color);
+        return (
+            <ColorPicker id="color-picker-id" defaultColor="#F37231" onChangeComplete={logColor} />            
+        );
+    }
+`;
 
-const logColorPicked = debounce((colorPicked: any) => {
-    // eslint-disable-next-line no-console
-    console.log(colorPicked);
-}, 500);
+const hiddenControls = `
+    import * as React from 'react';
+    import {ColorPicker} from '@coveord/plasma-react';
+    
+    export default () => {
+        return (
+            <ColorPicker id="color-picker-id-2" styles={{default: {controls: {display: 'none'}}}} />            
+        );
+    }
+`;
 
-// start-print
+const selector = `
+    import * as React from 'react';
+    import {useSelector} from 'react-redux';
+    import {ColorPicker, PlasmaState, InputSelectors} from '@coveord/plasma-react';
+    
+    export default () => {
+        const selected = useSelector((state: PlasmaState) =>
+            InputSelectors.getValue(state, {
+                id: 'color-picker-id-3',
+            })
+        );
+        return (
+            <>
+                <ColorPicker id="color-picker-id-3" />
+                Selected value: {selected}
+            </>
+        );
+    };
+`;
 
-export const ColorPickerExamples = () => (
-    <PlasmaComponent
+const ColorPickerExamples = () => (
+    <PageLayout
         id="ColorPicker"
         title="Color Picker"
-        usage="A color picker is a visual interface that allows users to select a color."
-        withSource
-    >
-        <div className="mt2">
-            <h2 className="mb2">
-                Built using <a href="https://github.com/casesandberg/react-color/">React Color</a>
-            </h2>
-            <div className="form-group">
-                <label className="form-control-label">Basic ColorPicker</label>
-                <div className="flex">
-                    <ColorPicker id="color-picker-example-1" defaultColor="#F37231" onChangeComplete={logColorPicked} />
-                </div>
-            </div>
-            <div className="form-group">
-                <label className="form-control-label">ColorPicker with different picker styling, hiding controls</label>
-                <div className="flex">
-                    <ColorPicker id="color-picker-example-2" styles={{default: {controls: {display: 'none'}}}} />
-                </div>
-            </div>
-
-            <div className="form-group">
-                <label className="form-control-label">
-                    ColorPickerConnected with color as hex available from state
-                </label>
-                <div className="flex">
-                    <ColorPicker id="color-picker-example-3" defaultColor="#47FF21" />
-                    <Button
-                        className="btn mod-primary ml2"
-                        name="Click me to get color from state"
-                        onClick={() =>
-                            alert(
-                                InputSelectors.getValue(Store.getState(), {
-                                    id: 'color-picker-example-3',
-                                })
-                            )
-                        }
-                    />
-                </div>
-            </div>
-        </div>
-    </PlasmaComponent>
+        section="Form"
+        description={
+            <>
+                A color picker is a visual interface that allows users to select a color. Built using{' '}
+                <a href="https://github.com/casesandberg/react-color/">React Color</a>
+            </>
+        }
+        componentSourcePath="/color-picker/ColorPicker.tsx"
+        code={code}
+        examples={{
+            hiddenControls: {code: hiddenControls, title: 'Hidden Controls'},
+            selector: {code: selector, title: 'Selector'},
+        }}
+    />
 );
+
 export default ColorPickerExamples;
