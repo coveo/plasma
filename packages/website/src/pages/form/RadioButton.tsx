bdd1f6ed2fcc44fa4022231215c34596e108a131
Gaël Dostie <gdostie@coveo.com>
2022-03-21 11:46:18 -0400

    refactor(website): move RadioButton examples to external files

--- packages/website/src/pages/form/RadioButton.tsx
+++ packages/website/src/pages/form/RadioButton.tsx
@@ -1,62 +1,8 @@
+import code from '@examples/RadioButton/main.example.tsx';
+import radioCard from '@examples/RadioButton/radioCard.example.tsx';
 import * as React from 'react';
 
 import {PageLayout} from '../../building-blocs/PageLayout';
-
-const code = `
-    import * as React from 'react';
-    import {RadioSelectConnected, Radio, Label} from '@coveord/plasma-react';
-
-    export default () => (
-        <RadioSelectConnected
-            id="radio-select-id"
-            valueOnMount="1"
-            disabledValuesOnMount={['3']}
-            disabledTooltip="This option is disabled"
-        >
-            <Radio id="Option1" name="enabledOptions" value="1">
-                <Label>Option 1</Label>
-            </Radio>
-            <Radio id="Option2" name="enabledOptions" value="2">
-                <Label>Option 2</Label>
-                <div className="mod-align-with-radio-label">An optional description.</div>
-            </Radio>
-            <Radio id="Option3" name="enabledOptions" value="3">
-                <Label>Option 3</Label>
-            </Radio>
-        </RadioSelectConnected>
-    );
-`;
-
-const radioCard = `
-    import * as React from 'react';
-    import {RadioSelectConnected, RadioCard, Label} from '@coveord/plasma-react';
-
-    export default () => (
-        <RadioSelectConnected
-            id="radio-card-select-example"
-            valueOnMount="1"
-            className="flex flex-wrap"
-            disabledValuesOnMount={['2']}
-            disabledTooltip="This option is disabled"
-        >
-            <RadioCard id="Option1" name="card-option" value="1">
-                <img className="mb2" src="https://via.placeholder.com/150x100" />
-                <Label>Option 1</Label>
-                <div>Description for the first option.</div>
-            </RadioCard>
-            <RadioCard id="Option2" name="card-option" value="2">
-                <img className="mb2" src="https://via.placeholder.com/150x100" />
-                <Label>Option 2</Label>
-                <div>Description for the second option.</div>
-            </RadioCard>
-            <RadioCard id="Option3" name="card-option" value="3">
-                <img className="mb2" src="https://via.placeholder.com/150x100" />
-                <Label>Option 3</Label>
-                <div>Description for the third option.</div>
-            </RadioCard>
-        </RadioSelectConnected>
-    );
-`;
 
 export default () => (
     <PageLayout
