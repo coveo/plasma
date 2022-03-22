c856824beb643503289b7d2d3f081f157facdf8d
Germain Bergeron <gbergeron@coveo.com>
2022-03-22 14:39:00 -0400

    refactor(website, flat select): update demo layout
    docs(react, flat select): added JsDoc on props

---
+++ packages/website/src/examples/FlatSelect/FlatSelectGroup.example.tsx
@@ -0,0 +1,23 @@
+import {FlatSelectConnected} from '@coveord/plasma-react';
+import * as React from 'react';
+
+export default () => (
+    <FlatSelectConnected
+        id="flat-select-group"
+        options={[
+            {
+                id: 'item-group-1',
+                option: {content: 'Option 1'},
+            },
+            {
+                id: 'item-group-2',
+                option: {content: 'Option 2'},
+            },
+            {
+                id: 'item-group-3',
+                option: {content: 'Option 3'},
+            },
+        ]}
+        group
+    />
+);
