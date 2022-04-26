d94a550aef9a13d3dc14726b56f20e686c3d544f
Asma Elfaleh <aelfaleh@coveo.com>
2022-04-26 07:45:41 -0400

    feat(checkbox): enhance CheckboxSelectors

--- packages/react/src/components/checkbox/CheckboxSelectors.ts
+++ packages/react/src/components/checkbox/CheckboxSelectors.ts
@@ -1,10 +1,21 @@
 import {createSelector} from 'reselect';
 import * as _ from 'underscore';
-
 import {PlasmaState} from '../../PlasmaState';
 import {ICheckboxState} from './CheckboxReducers';
+import {IGroupableCheckboxesState} from './GroupableCheckboxConstants';
 
-const get = (state: PlasmaState, {id}: {id: string}) => _.findWhere(state.checkboxes, {id});
+const get = (state: PlasmaState, {id}: {id: string}, isGroupableState: boolean = false, isParent: boolean = false) => {
+    if (isGroupableState) {
+        const groupableCheckboxesState: IGroupableCheckboxesState = _.findWhere(state.groupableCheckboxes);
+        if (groupableCheckboxesState) {
+            const checkboxState: ICheckboxState = isParent
+                ? groupableCheckboxesState.parent
+                : _.findWhere(groupableCheckboxesState.checkboxes, {id: id});
+            return checkboxState;
+        }
+    }
+    return _.findWhere(state.checkboxes, {id});
+};
 
 const getIsSelected = createSelector(get, (checkbox: ICheckboxState): boolean => checkbox && checkbox?.checked);
 const getIsDisabled = createSelector(get, (checkbox: ICheckboxState): boolean => checkbox && checkbox?.disabled);
