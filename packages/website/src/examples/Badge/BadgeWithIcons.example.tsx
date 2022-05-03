f78cf332e3efcd87ded31f9c24a549dd69635cd8
Ariane-Isabel <aiheon@coveo.com>
2022-05-03 08:33:21 -0400

    fix: adding flex to aligne example

--- packages/website/src/examples/Badge/BadgeWithIcons.example.tsx
+++ packages/website/src/examples/Badge/BadgeWithIcons.example.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Badge, BadgeIconPlacement} from '@coveord/plasma-react';
 
 export default () => (
-    <>
+    <div className="flex">
         <Badge icon="lock" label={'Icon is right'} iconPlacement={BadgeIconPlacement.Right} extraClasses={['ml1']} />
         <Badge icon="lock" label={'Icon is Left'} iconPlacement={BadgeIconPlacement.Left} extraClasses={['ml1']} />
         <Badge
@@ -20,5 +20,5 @@ export default () => (
             extraClasses={['ml1']}
         />
         <Badge isSmall icon="lock" iconPlacement={BadgeIconPlacement.Right} extraClasses={['ml1']} />
-    </>
+    </div>
 );
