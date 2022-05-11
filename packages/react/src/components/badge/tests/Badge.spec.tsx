cd20d6d5bafcc81103116d5068864c8b10095d09
Gaël Dostie <gdostie@coveo.com>
2022-05-11 16:16:45 -0400

    fix(react): fix failing badge test

--- packages/react/src/components/badge/tests/Badge.spec.tsx
+++ packages/react/src/components/badge/tests/Badge.spec.tsx
@@ -1,14 +1,15 @@
+import {LockSize16Px} from '@coveord/plasma-react-icons';
 import {render, screen, within} from '@test-utils';
 
 import {Badge} from '../Badge';
 
 describe('Badge', () => {
-    it('renders a badge', () => {
-        render(<Badge label="label" icon="lock" />);
+    it('renders a badge', async () => {
+        render(<Badge label="label" icon={LockSize16Px} />);
 
         const badge = screen.getByLabelText('badge');
         expect(badge).toBeInTheDocument();
         expect(within(badge).getByText('label')).toBeInTheDocument();
-        expect(within(badge).getByRole('img', {name: 'lock icon'})).toBeInTheDocument();
+        expect(await within(badge).findByRole('img', {name: 'lock'})).toBeInTheDocument();
     });
 });
