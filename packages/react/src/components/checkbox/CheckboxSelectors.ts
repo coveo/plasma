39fd817fbcb092710e34bbbb3ae9a5b58ffbe55d
Asma Elfaleh <aelfaleh@coveo.com>
2022-05-13 16:35:26 -0400

    fix(checkbox): pr changes

--- packages/react/src/components/checkbox/CheckboxSelectors.ts
+++ packages/react/src/components/checkbox/CheckboxSelectors.ts
@@ -13,17 +13,17 @@ const get = (
         groupableCheckboxProps,
     }: {id: string; isGroupableState?: boolean; groupableCheckboxProps?: IGroupableCheckboxOwnProps}
 ): ICheckboxState | undefined => {
-    if (isGroupableState) {
-        const groupableCheckboxesState: IGroupableCheckboxesState = _.findWhere(state.groupableCheckboxes, {
-            parentId: groupableCheckboxProps.parentId || groupableCheckboxProps.id,
-        });
-        if (groupableCheckboxesState) {
-            return groupableCheckboxProps.isParent
-                ? groupableCheckboxesState.parent
-                : _.findWhere(groupableCheckboxesState.checkboxes, {id: id});
-        }
+    if (!isGroupableState) {
+        return _.findWhere(state.checkboxes, {id});
     }
-    return _.findWhere(state.checkboxes, {id});
+    const groupableCheckboxesState: IGroupableCheckboxesState = _.findWhere(state.groupableCheckboxes, {
+        parentId: groupableCheckboxProps.parentId || groupableCheckboxProps.id,
+    });
+    if (groupableCheckboxesState) {
+        return groupableCheckboxProps.isParent
+            ? groupableCheckboxesState.parent
+            : _.findWhere(groupableCheckboxesState.checkboxes, {id: id});
+    }
 };
 
 const getIsSelected = createSelector(get, (checkbox: ICheckboxState): boolean => checkbox && checkbox?.checked);
