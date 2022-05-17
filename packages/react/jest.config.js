3ff7b320bf3e333743d129a913e61d8bce90265a
Gaël Dostie <gdostie@coveo.com>
2022-05-17 14:53:28 -0400

    refactor(react): remove Svg from BrowserPreviewError
    Use an img tag to load the previewError svg from plasma-style

--- packages/react/jest.config.js
+++ packages/react/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     verbose: true,
     moduleNameMapper: {
-        '\\.(scss|css)$': '<rootDir>/jest/identity-obj-proxy-esm.js',
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
+        '\\.(scss|css|svg)$': '<rootDir>/jest/identity-obj-proxy-esm.js',
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'identity-obj-proxy',
         '^@test-utils$': '<rootDir>/jest/utils.tsx',
     },
     setupFiles: ['<rootDir>/jest/setup.ts'],
