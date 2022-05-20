f2f1d0cb5c49158b994358357c7f05eab42f2054
Gaël Dostie <gdostie@coveo.com>
2022-05-20 17:59:32 -0400

    fix(react): make ActionableItem display better
    The ActionableItem component looks a little weird, so I fixed that. Refactored its implementation
    and redid all its tests while at it.
    
    feat(actionable-item): open button background colour
    
    Let the hover background colour visible if the menu is opened

--- packages/website/src/pages/form/ActionableItem.tsx
+++ packages/website/src/pages/form/ActionableItem.tsx
@@ -6,24 +6,8 @@ const code = `
     
     export default () => (
         <ActionableItem
-            id="actionable-item-id"
-            actions={[
-                {value: 'action 1', onOptionClick: () => alert('you triggered the first action')},
-                {value: 'action 2', onOptionClick: () => alert('you triggered the second action')},
-            ]}
-        >
-            click on the dots
-        </ActionableItem>
-    );
-`;
-
-const withOnItemClick = `
-    import {ActionableItem} from '@coveord/plasma-react';
-    
-    export default () => (
-        <ActionableItem
-            id="actionable-item-2"
-            onItemClick={(e: React.MouseEvent<HTMLDivElement>) => alert('you triggered the onItemClick method')}
+            id="🆔"
+            onItemClick={() => alert('you triggered the main button')}
             actions={[
                 {value: 'action 1', onOptionClick: () => alert('you triggered the first action')},
                 {value: 'action 2', onOptionClick: () => alert('you triggered the second action')},
@@ -41,9 +25,6 @@ const ActionableItemExamples: FunctionComponent = () => (
         section="Form"
         description="An actionable item is a dropdown menu listing actions associated with an element."
         code={code}
-        examples={{
-            withOnItemClick: {code: withOnItemClick, title: 'With OnItemClick Prop'},
-        }}
         componentSourcePath="/actionable-item/ActionableItem.tsx"
     />
 );
