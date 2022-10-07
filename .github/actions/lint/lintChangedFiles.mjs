c636a99d3a15ef176f781b5e26cb4fd293864016
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2022-10-07 16:09:53 -0400

    ci: lint PRs on github actions (#2852)
    * build: move lint-staged config to root
    
    * ci(github): add initial CI workflow (lint only)
    
    ci(github): move checkout step in CI workflow
    
    ci: fetch depth of 0 when doing checkout
    
    ci(lint): fix path to js script
    
    * ci(git): remove eslint on pre-commit
    
    * refactor: apply suggestion in lintChangedFiled.mjs
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>
    
    * ci(lint): split lint action in 2 conditional steps
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>

---
+++ .github/actions/lint/lintChangedFiles.mjs
@@ -0,0 +1,38 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
+import {ESLint} from 'eslint';
+
+const changedFiles = process.argv.slice(2);
+const changedFilesWithoutDuplicates = [...new Set(changedFiles)];
+
+const ALLOWED_EXTENSIONS = ['js', 'ts', 'jsx', 'tsx'];
+
+(async function main() {
+    const eslint = new ESLint();
+
+    console.log('\x1b[32mList of changed files', changedFilesWithoutDuplicates);
+
+    // remove all files that are not an allowed extension from the array
+    const filteredArray = changedFilesWithoutDuplicates.filter((element) => {
+        const extension = element.split('.').pop();
+        return ALLOWED_EXTENSIONS.includes(extension);
+    });
+
+    console.log('\x1b[32mFiltered list of files that will be linted', filteredArray);
+
+    const results = await eslint.lintFiles(filteredArray);
+
+    const formatter = await eslint.loadFormatter('stylish');
+    const resultText = formatter.format(results);
+
+    console.log('\x1b[32mLint results:', resultText || '\x1b[32mNo error or warning were found');
+
+    // Force exit 1 if error are found to make GitHub runner fail the step
+    results.some((result) => {
+        if (result.errorCount > 0) {
+            process.exit(1);
+        }
+    });
+})().catch((error) => {
+    process.exitCode = 1;
+    console.error(error);
+});
