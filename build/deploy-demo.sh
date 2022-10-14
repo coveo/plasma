5c83af8e8e2c9c09fdbcadbdedae0af8fba2a522
Gaël Dostie <gdostie@coveo.com>
2022-10-14 12:16:53 -0400

    feat: my amazing feature

--- build/deploy-demo.sh
+++ build/deploy-demo.sh
@@ -7,6 +7,6 @@ DESTINATION=react-vapor/feature
 BUCKET=coveo-nrd-jsadmin
 
 echo "Pushing new demo of ${BRANCH} to ${BUCKET}/${DESTINATION}/${BRANCH}."
-aws s3 sync ./packages/website/out s3://${BUCKET}/${DESTINATION}/${BRANCH}
+aws s3 sync ./donut s3://${BUCKET}/${DESTINATION}/${BRANCH}
 
 echo "Branch successfully deployed to https://plasma.coveo.com/feature/${BRANCH}/"
