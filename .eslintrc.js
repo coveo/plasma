823b1177a9bc30edf7d0bc7f7cd1ed5aecc1872c
Gaël Dostie <gdostie@coveo.com>
2022-10-14 11:36:37 -0400

    ci(publish): fix lint errors in publish script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
     extends: [require.resolve('tsjs/eslint-config')],
+    overrides: [
+        {
+            files: ['build/**/*.mjs'],
+            rules: {
+                'no-console': 'off',
+            },
+        },
+    ],
 };
--- build/getLastTag.mjs
+++ build/getLastTag.mjs
@@ -4,9 +4,7 @@ import url from 'url';
 const VERSION_PREFIX = 'v';
 const lastTag = await getLastTag(VERSION_PREFIX);
 
-export default function() {
-    return lastTag;
-};
+export default () => lastTag;
 
 // detect if that file is run directly instead of being imported
 if (import.meta.url === url.pathToFileURL(process.argv[1]).href) {
