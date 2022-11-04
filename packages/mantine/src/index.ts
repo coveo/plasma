7b1a512ddc59714905d101df8436dbd1baff6642
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2022-11-04 10:43:39 -0400

    Revert "feat(carousel): +carousel component for ML feature (#2895)"
    This reverts commit 870b1de91e654a3942496998ab1c928801890346.

--- packages/mantine/src/index.ts
+++ packages/mantine/src/index.ts
@@ -2,16 +2,18 @@ import {Tuple} from '@mantine/core';
 
 import {PlasmaColors} from './theme/PlasmaColors';
 
-export * from '@mantine/carousel';
+export {type ColumnDef, createColumnHelper} from '@tanstack/table-core';
+
 export * from '@mantine/core';
 export * from '@mantine/form';
 export type {FormValidateInput} from '@mantine/form/lib/types';
 export * from '@mantine/hooks';
-export {createColumnHelper, type ColumnDef} from '@tanstack/table-core';
+
 export * from './components';
+export * from './theme';
+
 // explicitly overriding mantine components
 export {Header, Table} from './components';
-export * from './theme';
 
 declare module '@mantine/core' {
     export interface MantineThemeColorsOverride {
