ef6ebc4d1bae106fc43b2ef3fda47f5be7fe9f80
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2022-11-07 16:22:03 -0500

    feat: create components-props-analyzer package (#2903)
    * feat: create components-props-analyzer package
    
    I extracted the logic of PropsTable into a separate package
    That way it shifts the work of pasing the props to compile time
    instead of runtime.
    
    * chore(deps): update lockfile
    
    * refactor(props): delete auto generated files
    
    * refactor(props): ignore auto generated files
    
    * refactor(website): apply suggestions review
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>
    
    Co-authored-by: Ariane-Isabel Héon <63734941+aiheon@users.noreply.github.com>

---
+++ packages/components-props-analyzer/bin/index.ts
@@ -0,0 +1,26 @@
+import {appendFile, ensureDirSync, outputFile, outputFileSync} from 'fs-extra';
+import * as rimraf from 'rimraf';
+
+import componentsList from '../src/ComponentsList';
+import {getPropsOfComponent} from './getPropsOfComponent';
+
+const generateProps = async () => {
+    const promises = componentsList.map(async (component) => {
+        const props = await getPropsOfComponent(component);
+        await outputFile(
+            `./src/components/${component.name}.ts`,
+            `// Don't edit this file, it is automatically generated on each build
+            import {ComponentMetadata} from '../ComponentsList';
+            export const ${component.name}Metadata: ComponentMetadata[] = ${JSON.stringify(props)};`
+        );
+        appendFile('./src/components/index.ts', `export * from './${component.name}';\n`);
+    });
+
+    await Promise.all(promises);
+};
+
+rimraf.sync('./src/components');
+ensureDirSync('./src/components');
+outputFileSync('./src/components/index.ts', '');
+
+generateProps();
