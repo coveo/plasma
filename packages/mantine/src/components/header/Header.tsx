b9c4ec4b8c748c4fa500debc8b7cc64d9f09d913
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2022-11-16 17:25:37 -0500

    feat(website): add jsdocs for mantine page header

--- packages/mantine/src/components/header/Header.tsx
+++ packages/mantine/src/components/header/Header.tsx
@@ -1,16 +1,37 @@
 import {QuestionSize24Px} from '@coveord/plasma-react-icons';
 import {Anchor, Breadcrumbs, DefaultProps, Divider, Group, Stack, Text, Title, Tooltip} from '@mantine/core';
-import {FunctionComponent, PropsWithChildren, ReactNode} from 'react';
+import {FunctionComponent, ReactNode} from 'react';
 
 interface HeaderProps extends DefaultProps {
+    /**
+     * The description text displayed inside the header underneath the title
+     */
     description?: ReactNode;
+    /**
+     * Action buttons that can be displayed on the right of the header
+     */
     actions?: ReactNode;
+    /**
+     * Whether the header should have a border on the bottom
+     */
     borderBottom?: boolean;
+    /**
+     * A href pointing to documentation related to the current panel.
+     * When provided, an info icon is rendered next to the title as link to this documentation
+     */
     docLink?: string;
+    /**
+     * The tooltip text shown when hovering over the doc link icon
+     */
     docLinkTooltipLabel?: string;
+    /**
+     * The title of the header.
+     * If more than one children are provided, each of them act as parts of a breadcrumb
+     */
+    children: ReactNode;
 }
 
-export const Header: FunctionComponent<PropsWithChildren<HeaderProps>> = ({
+export const Header: FunctionComponent<HeaderProps> = ({
     description,
     actions,
     borderBottom,
