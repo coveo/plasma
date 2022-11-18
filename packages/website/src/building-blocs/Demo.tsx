4751a54821a9fcad8c6ddf4c0373b18305cfae39
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2022-11-18 10:17:21 -0500

    feat(website): add open in code sandbox button (#2931)
    Add button to open a code snippet in code sandbox

--- packages/website/src/building-blocs/Demo.tsx
+++ packages/website/src/building-blocs/Demo.tsx
@@ -1,9 +1,11 @@
 import {ActionIcon, createStyles, ScrollArea, SimpleGrid, Stack, Tooltip, useClipboard} from '@coveord/plasma-mantine';
-import {CheckSize16Px, CopySize16Px} from '@coveord/plasma-react-icons';
+import {CheckSize16Px, CopySize16Px, PlaySize16Px} from '@coveord/plasma-react-icons';
 import {Prism} from '@mantine/prism';
 import vsDark from 'prism-react-renderer/themes/vsDark';
 import vsLight from 'prism-react-renderer/themes/vsLight';
 import {ReactNode} from 'react';
+
+import getCodeSandboxLink from './getCodeSandboxLink';
 
 export interface StaticDemoProps {
     snippet?: string;
@@ -19,7 +21,7 @@ const useStyles = createStyles((theme) => ({
         top: theme.spacing.xs,
         right: theme.spacing.xs,
         zIndex: 2,
-        'button:hover': {
+        'button:hover, a:hover': {
             backgroundColor: theme.colors.gray[8],
         },
     },
@@ -30,6 +32,7 @@ const height = 500;
 const Demo = ({children, snippet}: StaticDemoProps) => {
     const {classes} = useStyles();
     const clipboard = useClipboard();
+    const sandboxLink = getCodeSandboxLink(snippet);
     return (
         <SimpleGrid cols={2} className={classes.root}>
             <ScrollArea style={{height}}>{children}</ScrollArea>
@@ -50,14 +53,12 @@ const Demo = ({children, snippet}: StaticDemoProps) => {
                         {clipboard.copied ? <CheckSize16Px height={16} /> : <CopySize16Px height={16} />}
                     </ActionIcon>
                 </Tooltip>
-                {/* 
-                    // TODO
-                    <Tooltip label="Open in CodeSanbox" position="left">
-                        <ActionIcon radius="sm">
-                            <PlaySize16Px height={16} />
-                        </ActionIcon>
-                    </Tooltip> 
-                */}
+
+                <Tooltip label="Open in CodeSanbox" position="left">
+                    <ActionIcon<'a'> radius="sm" href={sandboxLink} target="_blank" component="a">
+                        <PlaySize16Px height={16} />
+                    </ActionIcon>
+                </Tooltip>
             </Stack>
         </SimpleGrid>
     );
---
+++ packages/website/src/building-blocs/getCodeSandboxLink.tsx
@@ -0,0 +1,66 @@
+import {getParameters} from 'codesandbox/lib/api/define';
+
+const packageJson = {
+    dependencies: {
+        react: 'latest',
+        'react-dom': 'latest',
+        '@coveord/plasma-mantine': 'latest',
+        '@emotion/react': 'latest',
+        '@mantine/core': 'latest',
+        '@mantine/dates': 'latest',
+        '@mantine/form': 'latest',
+        '@mantine/hooks': 'latest',
+        '@mantine/modals': 'latest',
+        '@mantine/carousel': 'latest',
+        'embla-carousel-react': 'latest',
+    },
+    devDependencies: {
+        tslib: 'latest',
+        typescript: 'latest',
+        '@types/react': 'latest',
+        '@types/react-dom': 'latest',
+    },
+};
+
+const indexTs = `
+import {createRoot} from 'react-dom/client';
+import {Container, Plasmantine} from '@coveord/plasma-mantine';
+import Demo from './Demo';
+import './font.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <Plasmantine>
+        <Container p="md">
+            <Demo />
+        </Container>
+    </Plasmantine>
+);
+`;
+
+const getSandboxLink = (snippet: string): string => {
+    const parameters = getParameters({
+        template: 'create-react-app',
+        files: {
+            'package.json': {
+                content: packageJson as unknown as string, // https://github.com/codesandbox/codesandbox-importers/issues/137
+                isBinary: false,
+            },
+            'src/index.tsx': {
+                content: indexTs,
+                isBinary: false,
+            },
+            'src/Demo.tsx': {
+                content: snippet,
+                isBinary: false,
+            },
+            'src/font.css': {
+                content: '@import url("https://use.typekit.net/wqe4zqp.css");',
+                isBinary: false,
+            },
+        },
+    });
+    return `https://codesandbox.io/api/v1/sandboxes/define?parameters=${parameters}&query=module=/src/Demo.tsx`;
+};
+
+export default getSandboxLink;
