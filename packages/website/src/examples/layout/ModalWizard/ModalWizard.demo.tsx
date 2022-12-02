a617dfa23d043ed0ef10939752cb0f13a0629d95
JTangCoveo <gtang@coveo.com>
2022-12-02 09:30:03 -0500

    feat(plasma-mantine): add demo page for ModalWizard

---
+++ packages/website/src/examples/layout/ModalWizard/ModalWizard.demo.tsx
@@ -0,0 +1,47 @@
+import {Button, ModalWizard} from '@coveord/plasma-mantine';
+import {useState} from 'react';
+
+const divStyles = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: 100,
+};
+
+const modalWizardSteps = [
+    {
+        docLink: 'https://coveo.com',
+        title: (currentStep: string, numberOfSteps: any) => 'Current Step is: ' + currentStep,
+        validateStep: () => ({isValid: true}),
+        element: <div style={divStyles}> Slide 1</div>,
+    },
+    {
+        docLink: 'https://coveo.com',
+        title: (currentStep: string, numberOfSteps: any) => 'Current Step is: ' + currentStep,
+        validateStep: () => ({isValid: true}),
+        element: <div style={divStyles}> Slide 2</div>,
+    },
+    {
+        docLink: 'https://coveo.com',
+        title: (currentStep: string, numberOfSteps: any) => 'Current Step is: ' + currentStep,
+        validateStep: () => ({isValid: true}),
+        element: <div style={divStyles}> Slide 3</div>,
+    },
+];
+
+export default () => {
+    const [opened, setOpened] = useState(false);
+
+    return (
+        <>
+            <Button onClick={() => setOpened(true)}> Open ModalWizard </Button>
+            <ModalWizard onClose={() => setOpened(false)} opened={opened} onFinish={() => setOpened(false)}>
+                {modalWizardSteps.map((step) => (
+                    <ModalWizard.Step docLink={step.docLink} title={step.title} validateStep={step.validateStep}>
+                        {step.element}
+                    </ModalWizard.Step>
+                ))}
+            </ModalWizard>
+        </>
+    );
+};
