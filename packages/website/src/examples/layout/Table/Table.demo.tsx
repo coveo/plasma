42173a873decb3fd4885bb54c8c27f493676191a
Simon Milord <simon.milord@gmail.com>
2022-12-07 15:42:30 -0500

    Feat UI tool1024 add demo page tableMantineImprovement example (#2962)
    * feat(table): added support for doubleclick on a row
    
    * feat(table): added support for doubleclick on a row
    
    Co-authored-by: simonmilord <smilord@coveo.com>

--- packages/website/src/examples/layout/Table/Table.demo.tsx
+++ packages/website/src/examples/layout/Table/Table.demo.tsx
@@ -1,39 +1,104 @@
-import {ColumnDef, createColumnHelper, Table} from '@coveord/plasma-mantine';
-import {loremIpsum} from 'lorem-ipsum';
+import {
+    BlankSlate,
+    Button,
+    ColumnDef,
+    createColumnHelper,
+    DateRangePickerPreset,
+    Table,
+    Title,
+} from '@coveord/plasma-mantine';
+import {FunctionComponent, useState} from 'react';
+
+interface IExampleRowData {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+}
 
 export default () => {
     const columnHelper = createColumnHelper<IExampleRowData>();
     const columns: Array<ColumnDef<IExampleRowData>> = [
-        columnHelper.accessor('username', {
-            header: 'Name',
-            cell: (info) => info.row.original.username,
-            enableSorting: false,
+        columnHelper.accessor('userId', {
+            header: 'User ID',
+            cell: (info) => info.row.original.userId,
         }),
-        columnHelper.accessor('city', {
-            header: 'City',
-            cell: (info) => info.row.original.city,
-            enableSorting: false,
+        columnHelper.accessor('id', {
+            header: 'Post ID',
+            cell: (info) => info.row.original.id,
         }),
-        columnHelper.accessor('password', {
-            header: 'Password',
-            cell: (info) => info.row.original.password,
-            enableSorting: false,
+        columnHelper.accessor('title', {
+            header: 'Title',
+            cell: (info) => info.row.original.title,
         }),
+        Table.CollapsibleColumn as ColumnDef<IExampleRowData>,
     ];
-    return <Table data={dataForRows} columns={columns} />;
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [pages, setPages] = useState(1);
+
+    const fetchData = (state: any) => {
+        setLoading(true);
+        fetch(
+            `https://jsonplaceholder.typicode.com/posts?_sort=${
+                state.sorting?.[0]?.id ? state.sorting?.[0]?.id : 'userId'
+            }&_order=${state.sorting?.[0]?.desc ? 'desc' : 'asc'}&_page=${
+                state.pagination.pageIndex ? state.pagination.pageIndex + 1 : 1
+            }&_limit=${state.pagination.pageSize}`
+        )
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .then(() => setPages(Math.ceil(100 / state.pagination?.pageSize)))
+            .catch((e) => console.log(e));
+        setLoading(false);
+    };
+
+    return (
+        <Table
+            data={data}
+            columns={columns}
+            noDataChildren={<NoData />}
+            onMount={(state) => {
+                fetchData(state);
+            }}
+            onChange={(state) => {
+                fetchData(state);
+            }}
+            loading={loading}
+            initialState={{dateRange: [previousDay, today]}}
+            getExpandChildren={(datum) => <div className="py2">{datum.body}</div>}
+        >
+            {/* you can override background color with: sx={{backgroundColor: 'white'}} for Header and Footer */}
+            <Table.Header>
+                <Table.Actions>{(datum: IExampleRowData) => <TableActions datum={datum} />}</Table.Actions>
+                <Table.DateRangePicker presets={DatePickerPresets} />
+            </Table.Header>
+            <Table.Footer>
+                <Table.PerPage />
+                <Table.Pagination totalPages={pages} />
+            </Table.Footer>
+        </Table>
+    );
 };
 
-interface IExampleRowData {
-    city: string;
-    username: string;
-    password: string;
-}
+const NoData: FunctionComponent = () => (
+    <BlankSlate>
+        <Title order={4}>No Data</Title>
+    </BlankSlate>
+);
 
-const generateData = (length: number): IExampleRowData[] =>
-    Array.from(Array(length)).map(() => ({
-        city: loremIpsum({count: 1, units: 'word'}),
-        username: loremIpsum({count: 2, units: 'word'}),
-        password: loremIpsum({count: 1, units: 'word'}),
-    }));
+const today: Date = new Date();
+const previous: Date = new Date(new Date().getTime());
+const previousDay: Date = new Date(previous.setDate(previous.getDate() - 1));
+const previousWeek: Date = new Date(previous.setDate(previous.getDate() - 7));
 
-const dataForRows = generateData(5);
+const DatePickerPresets: Record<string, DateRangePickerPreset> = {
+    lastDay: {label: 'Last 24 hours', range: [previousDay, today]},
+    lastWeek: {label: 'Last week', range: [previousWeek, today]},
+};
+
+const TableActions: FunctionComponent<{datum: IExampleRowData}> = ({datum}) => {
+    const actionCondition = datum.id % 2 === 0 ? true : false;
+    const pressedAction = () => alert('Some action is triggered!');
+    return <>{actionCondition ? <Button onClick={pressedAction}>Action!</Button> : null}</>;
+};
