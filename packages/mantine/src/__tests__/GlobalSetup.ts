b88907cd1efb88d315523e46b643a785ca3c70bb
Felix <fblais@coveo.com>
2023-02-08 10:25:46 -0500

    chore(vitest): include src at build

--- packages/mantine/src/__tests__/GlobalSetup.ts
+++
@@ -1,5 +0,0 @@
-// This file is executed once, before everything else.
-export default () => {
-    // @ts-ignore
-    process.env.TZ = 'UTC';
-};
--- packages/mantine/src/__tests__/Setup.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This file is executed once per test file.
- * It is executed before executing SetupAfterEnv and before the test code itself.
- */
