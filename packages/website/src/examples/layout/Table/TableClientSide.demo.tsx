1d3e182d41657d1dc814ab644174809fd1817f89
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2023-02-21 16:34:15 -0500

    refactor(website): cleanup table demos (#3041)
    * refactor(website): cleanup table demos
    
    The table demos were using patterns that are best avoided.
    
    * ci(lint): ignore website build artifacts from lint
    
    * refactor: apply suggestions
    
    Co-authored-by: Germain Bergeron <GermainBergeron@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Germain Bergeron <GermainBergeron@users.noreply.github.com>

--- packages/website/src/examples/layout/Table/TableClientSide.demo.tsx
+++ packages/website/src/examples/layout/Table/TableClientSide.demo.tsx
@@ -1,58 +1,70 @@
 import {
     ColumnDef,
     createColumnHelper,
-    Table,
-    getSortedRowModel,
-    TableProps,
-    getPaginationRowModel,
-    getFilteredRowModel,
     FilterFn,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    Table,
+    TableProps,
 } from '@coveord/plasma-mantine';
 import {faker} from '@faker-js/faker';
 import {rankItem} from '@tanstack/match-sorter-utils';
-import {useState} from 'react';
+import {useMemo} from 'react';
 
 export type Person = {
+    id: string;
     firstName: string;
     lastName: string;
     age: number;
 };
 
-const makeData = (len: number) =>
+const makeData = (len: number): Person[] =>
     Array(len)
         .fill(0)
-        .map(() => ({
+        .map((a, index) => ({
+            id: faker.datatype.uuid(),
             firstName: faker.name.firstName(),
             lastName: faker.name.lastName(),
             age: faker.datatype.number(40),
         }));
 
 const fuzzyFilter: FilterFn<Person> = (row, columnId, value) => rankItem(row.getValue(columnId), value).passed;
+
+const columnHelper = createColumnHelper<Person>();
+
+const columns: Array<ColumnDef<Person>> = [
+    columnHelper.accessor('firstName', {
+        header: 'First name',
+        cell: (info) => info.row.original.firstName,
+    }),
+    columnHelper.accessor('lastName', {
+        header: 'Last name',
+        cell: (info) => info.row.original.lastName,
+    }),
+    columnHelper.accessor('age', {
+        header: 'Age',
+        cell: (info) => info.row.original.age,
+    }),
+];
+
+const options: TableProps<Person>['options'] = {
+    globalFilterFn: fuzzyFilter,
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+};
+
 export default () => {
-    const columnHelper = createColumnHelper<Person>();
-    const columns: Array<ColumnDef<Person>> = [
-        columnHelper.accessor('firstName', {
-            header: 'First name',
-            cell: (info) => info.row.original.firstName,
-        }),
-        columnHelper.accessor('lastName', {
-            header: 'Last name',
-            cell: (info) => info.row.original.lastName,
-        }),
-        columnHelper.accessor('age', {
-            header: 'Age',
-            cell: (info) => info.row.original.age,
-        }),
-    ];
-    const [data] = useState(() => makeData(45));
-    const options: TableProps<Person>['options'] = {
-        globalFilterFn: fuzzyFilter,
-        getFilteredRowModel: getFilteredRowModel(),
-        getPaginationRowModel: getPaginationRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-    };
+    const data = useMemo(() => makeData(45), []);
     return (
-        <Table data={data} columns={columns} options={options} initialState={{pagination: {pageSize: 5}}}>
+        <Table
+            data={data}
+            columns={columns}
+            options={options}
+            initialState={{pagination: {pageSize: 5}}}
+            getRowId={({id}) => id}
+        >
             <Table.Header>
                 <Table.Filter placeholder="Search" />
             </Table.Header>
