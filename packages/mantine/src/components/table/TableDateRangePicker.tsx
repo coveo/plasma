dc8fef54d06892715cf0f94d69f345e8ad2dd841
Antoine Boissinot <aboissinot@coveo.com>
2023-05-18 17:22:56 -0400

    fix(tabledaterangepicker): close datepicker when clicking back on calendar button
    UITOOL-8787

--- packages/mantine/src/components/table/TableDateRangePicker.tsx
+++ packages/mantine/src/components/table/TableDateRangePicker.tsx
@@ -45,15 +45,15 @@ export const TableDateRangePicker: FunctionComponent<TableDateRangePickerProps> 
     };
 
     const formatDate = (date: Date) => dayjs(date).format('MMM DD, YYYY');
-    const formatedRange = `${formatDate(form.values.dateRange[0])} - ${formatDate(form.values.dateRange[1])}`;
+    const formattedRange = `${formatDate(form.values.dateRange[0])} - ${formatDate(form.values.dateRange[1])}`;
 
     return (
         <Grid.Col span="content" order={TableComponentsOrder.DateRangePicker} py="sm">
             <Group spacing="xs">
-                <Text span>{formatedRange}</Text>
+                <Text span>{formattedRange}</Text>
                 <Popover opened={opened} onChange={setOpened} withinPortal>
                     <Popover.Target>
-                        <Button variant="outline" color="gray" onClick={() => setOpened(true)} px="xs">
+                        <Button variant="outline" color="gray" onClick={() => setOpened((o) => !o)} px="xs">
                             <CalendarSize24Px width={24} height={24} />
                         </Button>
                     </Popover.Target>
