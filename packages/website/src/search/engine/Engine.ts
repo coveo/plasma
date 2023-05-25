2338d75157e71479d656c497d76969d6e3c9b1c0
Germain Bergeron <gbergeron@coveo.com>
2023-05-25 10:38:46 -0400

    chore(snyk): remove snyk since we use codeql

--- packages/website/src/search/engine/Engine.ts
+++ packages/website/src/search/engine/Engine.ts
@@ -5,7 +5,6 @@ export const searchEngine = () =>
         configuration: {
             platformUrl: 'https://platform.cloud.coveo.com',
             organizationId: 'pwsadimtnwqkrvyrbxjuocz2g6m',
-            // file deepcode ignore HardcodedNonCryptoSecret: <respect the documentation suggestion https://docs.coveo.com/en/105/build-a-search-ui/api-key-authentication>
             accessToken: 'xx4dfa233d-3c49-4ade-b07a-e85277a23e9b',
             analytics: {enabled: true, deviceId: 'dummyDeviceId', userDisplayName: 'anonymous'},
             search: {
