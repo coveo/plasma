4f613bdfdfa81da5dc300452e6aa260af65b2897
Antoine Boissinot <aboissinot@coveo.com>
2023-08-07 09:08:30 -0400

    chore(deps): try workaround to swc issue

--- packages/mantine/src/components/table/Table.tsx
+++ packages/mantine/src/components/table/Table.tsx
@@ -1,4 +1,4 @@
-import {Box, Center, Loader} from '@mantine/core';
+import {Box, Center, Checkbox, Loader, Tooltip} from '@mantine/core';
 import {useForm} from '@mantine/form';
 import {useDidUpdate} from '@mantine/hooks';
 import {
@@ -29,8 +29,35 @@ import {TableLoading} from './TableLoading';
 import {TablePagination} from './TablePagination';
 import {TablePerPage} from './TablePerPage';
 import {TablePredicate} from './TablePredicate';
-import {TableSelectableColumn} from './TableSelectableColumn';
 import {useRowSelection} from './useRowSelection';
+
+const TableSelectableColumn: ColumnDef<unknown> = {
+    id: 'select',
+    enableSorting: false,
+    header: ({table}) => {
+        const label = table.getIsAllRowsSelected() ? 'Unselect all from this page' : 'Select all from this page';
+        return (
+            <Tooltip label={label}>
+                <Checkbox
+                    checked={table.getIsAllPageRowsSelected()}
+                    indeterminate={table.getIsSomePageRowsSelected()}
+                    onChange={table.getToggleAllPageRowsSelectedHandler()}
+                    sx={{display: 'flex'}}
+                    aria-label={label}
+                />
+            </Tooltip>
+        );
+    },
+    cell: ({row}) => (
+        <Checkbox
+            checked={row.getIsSelected()}
+            indeterminate={row.getIsSomeSelected()}
+            onChange={row.getToggleSelectedHandler()}
+            sx={{display: 'flex'}}
+            aria-label="Select row"
+        />
+    ),
+};
 
 export const Table: TableType = <T,>({
     data,
