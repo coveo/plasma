516705f8190d30a1b540cdc46ea80d10f1c5fb69
Danny Gauthier <dgauthier@WKS-002169.corp.coveo.com>
2023-08-08 10:14:53 -0400

    fix(type): propswithchildren generic

--- packages/mantine/src/theme/Plasmantine.tsx
+++ packages/mantine/src/theme/Plasmantine.tsx
@@ -3,7 +3,7 @@ import {FunctionComponent, PropsWithChildren} from 'react';
 
 import {plasmaTheme} from './Theme';
 
-export const Plasmantine: FunctionComponent<PropsWithChildren> = ({children}) => (
+export const Plasmantine: FunctionComponent<PropsWithChildren<unknown>> = ({children}) => (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={plasmaTheme}>
         {children}
     </MantineProvider>
