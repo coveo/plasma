e54a7cfba69a53da1e9da5847510db1a7ccb82bc
Danny Gauthier <danny.m.gauthier@gmail.com>
2023-10-11 08:51:25 -0400

    fix(table inline prompt): overflow (#3565)
    Co-authored-by: Danny Gauthier <dgauthier@WKS-002169.corp.coveo.com>

--- packages/mantine/src/components/table/table-actions/TableActions.styles.ts
+++ packages/mantine/src/components/table/table-actions/TableActions.styles.ts
@@ -1,7 +1,7 @@
 import {createStyles} from '@mantine/core';
 
 export default createStyles((theme) => ({
-    root: {},
+    root: {maxWidth: '100%'},
     wrapper: {
         display: 'inline-flex',
     },
