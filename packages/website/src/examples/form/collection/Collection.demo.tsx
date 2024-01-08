ae965b8153fe21fe295d1f2858c391121739c907
Felix <fblais@coveo.com>
2024-01-08 14:35:13 -0500

    chore(useForm): removed console.log

--- packages/website/src/examples/form/collection/Collection.demo.tsx
+++ packages/website/src/examples/form/collection/Collection.demo.tsx
@@ -14,7 +14,6 @@ const Demo = () => {
             todoList: (value) => (value.length < 2 ? `Don't be lazy, add just ${2 - value.length} more tasks` : null),
         },
     });
-    console.log(form.getInputProps('todoList.0.done', {type: 'checkbox'}));
 
     return (
         <Collection<{name: string; done: boolean}>
