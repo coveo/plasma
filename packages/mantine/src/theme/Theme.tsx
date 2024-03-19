ec5bfb38eb33e7bd54d500d39c85f8fb3b38c212
Danny Gauthier <danny.m.gauthier@gmail.com>
2024-03-19 08:07:23 -0400

    fix(popover): zindex (#3642)
    Co-authored-by: Danny Gauthier <dgauthier@WKS-002169.corp.coveo.com>

--- packages/mantine/src/theme/Theme.tsx
+++ packages/mantine/src/theme/Theme.tsx
@@ -14,6 +14,7 @@ import {
     InputWrapper,
     List,
     Loader,
+    LoadingOverlay,
     MantineThemeOverride,
     MenuItem,
     Modal,
@@ -116,6 +117,9 @@ export const plasmaTheme: MantineThemeOverride = createTheme({
         Button: Button.extend({
             classNames: {root: ButtonClasses.root, label: ButtonClasses.label},
         }),
+        LoadingOverlay: LoadingOverlay.extend({
+            defaultProps: {zIndex: 1400},
+        }),
         Modal: Modal.extend({
             classNames: {
                 root: ModalClasses.root,
@@ -184,6 +188,7 @@ export const plasmaTheme: MantineThemeOverride = createTheme({
             defaultProps: {
                 shadow: 'md',
                 withArrow: true,
+                zIndex: 1300,
             },
         }),
         Badge: Badge.extend({
