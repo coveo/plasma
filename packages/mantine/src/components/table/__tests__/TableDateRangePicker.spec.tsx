14e6d4de1a6abc25120784cf8957b2aede733e81
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2024-04-10 09:19:03 -0400

    feat(table): visual update on datepicker (#3673)
    * feat(table): visual update on datepicker
    
    * feat(datepicker): changed visual, +button

--- packages/mantine/src/components/table/__tests__/TableDateRangePicker.spec.tsx
+++ packages/mantine/src/components/table/__tests__/TableDateRangePicker.spec.tsx
@@ -1,5 +1,5 @@
 import {ColumnDef, createColumnHelper} from '@tanstack/table-core';
-import {render, screen, userEvent, waitFor, within} from '@test-utils';
+import {render, screen, waitFor} from '@test-utils';
 
 import {Table} from '../Table';
 
@@ -23,64 +23,12 @@ describe('Table.DateRangePicker', () => {
     it('displays the initial dates', async () => {
         render(basicTableWithDateRangePicker);
 
-        expect(screen.getByText('Jan 01, 2022 - Jan 07, 2022')).toBeVisible();
-    });
-
-    it.skip('opens the dialog when clicking on the calendar button', async () => {
-        const user = userEvent.setup();
-        render(basicTableWithDateRangePicker);
-
-        await user.click(screen.getByRole('button', {name: 'calendar'}));
-        expect(screen.getByRole('dialog', {name: 'calendar'})).toBeVisible();
-    });
-
-    it.skip('closes the dialog when clicking back on the calendar button', async () => {
-        const user = userEvent.setup();
-        render(basicTableWithDateRangePicker);
-
-        await user.click(screen.getByRole('button', {name: 'calendar'}));
-        await user.click(screen.getByRole('button', {name: 'calendar'}));
-        expect(screen.queryByRole('dialog', {name: 'calendar'})).not.toBeInTheDocument();
-    }, 10000);
-
-    it.skip('displays the selected date range in the table', async () => {
-        vi.useFakeTimers().setSystemTime(new Date(2022, 0, 15));
-        const user = userEvent.setup({delay: null});
-        const onChange = vi.fn();
-        render(
-            <Table
-                data={[{name: 'fruit'}, {name: 'vegetable'}]}
-                columns={columns}
-                onChange={onChange}
-                initialState={{dateRange: [new Date(2022, 0, 1), new Date(2022, 0, 7)]}}
-            >
-                <Table.Header data-testid="table-header">
-                    <Table.DateRangePicker
-                        presets={{preset: {label: 'Preset', range: [new Date(2022, 0, 8), new Date(2022, 0, 14)]}}}
-                    />
-                </Table.Header>
-            </Table>,
+        await waitFor(() =>
+            expect(
+                screen.getByRole('button', {
+                    name: /jan 01, 2022 – jan 07, 2022/i,
+                }),
+            ).toBeVisible(),
         );
-        const tableHeader = screen.getByTestId('table-header');
-
-        expect(within(tableHeader).getByText('Jan 01, 2022 - Jan 07, 2022')).toBeInTheDocument();
-        await user.click(within(tableHeader).getByRole('button', {name: 'calendar'}));
-
-        const calendar = await screen.findByRole('dialog', {name: 'calendar'});
-
-        // select a preset
-        await user.click(within(calendar).getByRole('searchbox', {name: 'Date range'}));
-        await user.click(within(calendar).getByRole('option', {name: 'Preset'}));
-        await user.click(within(calendar).getByRole('button', {name: 'Apply'}));
-
-        await waitFor(() => {
-            expect(onChange).toHaveBeenCalledTimes(1);
-        });
-        expect(onChange).toHaveBeenCalledWith(
-            expect.objectContaining({dateRange: [new Date(2022, 0, 8), new Date(2022, 0, 14)]}),
-        );
-        expect(within(tableHeader).getByText('Jan 08, 2022 - Jan 14, 2022')).toBeInTheDocument();
-
-        vi.useRealTimers();
-    }, 20000);
+    });
 });
