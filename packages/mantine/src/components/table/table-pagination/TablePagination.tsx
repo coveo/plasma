431940a30632a73e07cfc1d2458f9c5c4fa1936f
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2024-05-07 14:28:48 -0400

    refactor(mantine)!: adjust Table.Pagination & Table.PerPage to the breaking changes
    BREAKING CHANGE: Table.PerPage no longer accept totalPages prop.
    Use the table store to set the total number of entries instead.

--- packages/mantine/src/components/table/table-pagination/TablePagination.tsx
+++ packages/mantine/src/components/table/table-pagination/TablePagination.tsx
@@ -2,32 +2,32 @@ import {Pagination} from '@mantine/core';
 import {useDidUpdate} from '@mantine/hooks';
 import {FunctionComponent} from 'react';
 
-import {useTable} from '../TableContext';
 import {TablePaginationProps} from './TablePagination.types';
+import {useTableContext} from '../TableContext';
 
-export const TablePagination: FunctionComponent<TablePaginationProps> = ({totalPages, onPageChange}) => {
-    const {state, setState, containerRef, getPageCount} = useTable();
+export const TablePagination: FunctionComponent<TablePaginationProps> = ({onPageChange}) => {
+    const {store, table, containerRef} = useTableContext();
 
     const updatePage = (newPage: number) => {
         onPageChange?.(newPage - 1);
-        setState((prevState) => ({
-            ...prevState,
-            pagination: {...prevState.pagination, pageIndex: newPage - 1},
-        }));
+        store.setPagination((prev) => ({...prev, pageIndex: newPage - 1}));
         containerRef.current.scrollIntoView({behavior: 'smooth'});
     };
 
-    const total = totalPages == null ? getPageCount() : totalPages;
+    const total =
+        store.state.totalEntries == null
+            ? table.getPageCount()
+            : Math.ceil(store.state.totalEntries / store.state.pagination.pageSize);
 
     useDidUpdate(() => {
-        if (state.pagination.pageIndex + 1 > total && total > 0) {
+        if (store.state.pagination.pageIndex + 1 > total && total > 0) {
             updatePage(total);
         }
-    }, [state.pagination.pageIndex, total]);
+    }, [store.state.pagination.pageIndex, total]);
 
     return (
         <Pagination
-            value={state.pagination.pageIndex + 1}
+            value={store.state.pagination.pageIndex + 1}
             onChange={updatePage}
             total={total}
             boundaries={0}
