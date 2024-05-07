505a76001f0a7d480f3a9dd598ec33adee40f380
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2024-05-07 14:38:22 -0400

    feat(website): add a Table demo with React Query

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,8 +1,10 @@
 import {AppShell, Notifications, Plasmantine} from '@coveord/plasma-mantine';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {Outlet} from 'react-router-dom';
 import {Navigation} from './Navigation';
 import TopBar from './TopBar';
 import {EngineProvider} from './search/engine/EngineProvider';
+
 import './styles/colors.css';
 import './styles/home.css';
 import './styles/loading-screen.css';
@@ -12,21 +14,25 @@ import './styles/props-table.css';
 import './styles/spacing.css';
 import './styles/tile.css';
 
+const queryClient = new QueryClient();
+
 const App = () => (
-    <EngineProvider>
-        <Plasmantine>
-            <Notifications position="top-center" />
-            <AppShell navbar={{width: 245, breakpoint: undefined}} header={{height: 100}}>
-                <AppShell.Header>
-                    <TopBar />
-                </AppShell.Header>
-                <AppShell.Navbar>
-                    <Navigation />
-                </AppShell.Navbar>
-                <Outlet />
-            </AppShell>
-        </Plasmantine>
-    </EngineProvider>
+    <QueryClientProvider client={queryClient}>
+        <EngineProvider>
+            <Plasmantine>
+                <Notifications position="top-center" />
+                <AppShell navbar={{width: 245, breakpoint: undefined}} header={{height: 100}}>
+                    <AppShell.Header>
+                        <TopBar />
+                    </AppShell.Header>
+                    <AppShell.Navbar>
+                        <Navigation />
+                    </AppShell.Navbar>
+                    <Outlet />
+                </AppShell>
+            </Plasmantine>
+        </EngineProvider>
+    </QueryClientProvider>
 );
 
 export default App;
