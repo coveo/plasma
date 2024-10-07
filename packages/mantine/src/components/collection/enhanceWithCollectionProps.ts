d076ebfe5bd8319e52a1106a74f66cd4a1fda3a7
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2024-10-07 08:52:51 -0400

    fix(form): +generic tranformvalue for enhancewithcollection (#3912)

--- packages/mantine/src/components/collection/enhanceWithCollectionProps.ts
+++ packages/mantine/src/components/collection/enhanceWithCollectionProps.ts
@@ -1,8 +1,12 @@
 import {UseFormInput} from '@mantine/form';
+import {_TransformValues} from '@mantine/form/lib/types';
 import {CollectionProps} from './Collection';
 
-export const enhanceWithCollectionProps = <FormValues>(
-    payload: Parameters<UseFormInput<FormValues>['enhanceGetInputProps']>[0],
+export const enhanceWithCollectionProps = <
+    FormValues,
+    TransformValues extends _TransformValues<FormValues> = (values: FormValues) => FormValues,
+>(
+    payload: Parameters<UseFormInput<FormValues, TransformValues>['enhanceGetInputProps']>[0],
     field: (typeof payload)['field'],
 ): Pick<CollectionProps<unknown>, 'onReorderItem' | 'onInsertItem' | 'onRemoveItem'> => {
     if (payload.field === field) {
