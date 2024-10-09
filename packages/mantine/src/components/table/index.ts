85ca26945f6a0e2eb3f41463e85112d1885b853c
Felix <fblais@coveo.com>
2024-10-09 08:35:37 -0400

    fix(table): type only import for table predicate props

--- packages/mantine/src/components/table/index.ts
+++ packages/mantine/src/components/table/index.ts
@@ -1,6 +1,6 @@
 export {flexRender as renderTableCell} from '@tanstack/react-table';
 export * from './Table';
-export {TablePredicateProps} from './table-predicate/TablePredicate';
+export {type TablePredicateProps} from './table-predicate/TablePredicate';
 export {type TableAction, type TableLayout, type TableLayoutProps, type TableProps} from './Table.types';
 export {useTableContext} from './TableContext';
 export {useTable, type TableState, type TableStore, type UseTableOptions} from './use-table';
