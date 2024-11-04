cbe221f214a8764cdbe3f47a0bb456539c69c478
Antoine Boissinot <aboissinot@coveo.com>
2024-11-04 09:17:03 -0500

    feat(mantine): adjust demos

---
+++ packages/website/src/examples/layout/EllipsisText/EllipsisTextDefaultShort.demo.tsx
@@ -0,0 +1,4 @@
+import {EllipsisText} from '@coveord/plasma-mantine';
+
+const Demo = () => <EllipsisText maw={250}>This short text is not truncated.</EllipsisText>;
+export default Demo;
--- packages/website/src/pages/layout/EllipsisText.tsx
+++ packages/website/src/pages/layout/EllipsisText.tsx
@@ -1,6 +1,11 @@
 import {EllipsisTextMetadata} from '@coveord/plasma-components-props-analyzer';
-import EllipsisTextDemo from '@examples/layout/EllipsisText/EllipsisText.demo?demo';
+import EllipsisTextDefaultLongDemo from '@examples/layout/EllipsisText/EllipsisTextDefaultLong.demo?demo';
+import EllipsisTextDefaultShortDemo from '@examples/layout/EllipsisText/EllipsisTextDefaultShort.demo?demo';
+import EllipsisTextLineClampLongDemo from '@examples/layout/EllipsisText/EllipsisTextLineClampLong.demo?demo';
+import EllipsisTextLineClampShortDemo from '@examples/layout/EllipsisText/EllipsisTextLineClampShort.demo?demo';
+import EllipsisTextNoWrapContainerDemo from '@examples/layout/EllipsisText/EllipsisTextNoWrapContainer.demo?demo';
 
+import {Stack} from '@mantine/core';
 import {PageLayout} from '../../building-blocs/PageLayout';
 
 const Page = () => (
@@ -11,8 +16,18 @@ const Page = () => (
         description="Allows to display text that will automatically truncate and shows a tooltip on hover if it overflows the max width."
         id="EllipsisText"
         propsMetadata={EllipsisTextMetadata}
-        demo={<EllipsisTextDemo />}
-    />
+        demo={<EllipsisTextDefaultLongDemo />}
+    >
+        <div className="plasma-page-layout__section">
+            <Stack>
+                <EllipsisTextDefaultLongDemo title="Default ellipsis" />
+                <EllipsisTextDefaultShortDemo title="Default no ellipsis" />
+                <EllipsisTextNoWrapContainerDemo title="Default in no wrap container" />
+                <EllipsisTextLineClampLongDemo title="Line clamp ellipsis" />
+                <EllipsisTextLineClampShortDemo title="Line clamp no ellipsis" />
+            </Stack>
+        </div>
+    </PageLayout>
 );
 
 export default Page;
