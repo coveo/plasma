7325bf8adbeb6d9aa29ae0443e688d6471fb7b0a
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2024-11-13 12:33:42 -0500

    feat(prompt): +disabledwithtooltip on confirm button

--- packages/mantine/src/components/prompt/PromptConfirmButton.tsx
+++ packages/mantine/src/components/prompt/PromptConfirmButton.tsx
@@ -1,5 +1,6 @@
 import {Button, CompoundStylesApiProps, factory, Factory, useProps} from '@mantine/core';
 import {ButtonProps} from '../button/Button';
+import {ButtonWithDisabledTooltip} from '../button/ButtonWithDisabledTooltip';
 import {PromptVariant} from './Prompt';
 import {usePromptContext} from './Prompt.context';
 
@@ -28,15 +29,32 @@ const defaultProps: Partial<PromptConfirmButtonProps> = {};
 export const PromptConfirmButton = factory<PromptConfirmButtonFactory>((_props, ref) => {
     const {variant, getStyles} = usePromptContext();
     const props = useProps('PromptConfirmButton', defaultProps, _props);
-    const {className, classNames, style, styles, unstyled, vars, ...others} = props;
+    const {
+        className,
+        classNames,
+        style,
+        styles,
+        unstyled,
+        vars,
+        disabled,
+        disabledTooltip,
+        disabledTooltipProps,
+        ...others
+    } = props;
 
     return (
-        <Button
-            ref={ref}
-            variant="filled"
-            color={COLOR_BY_VARIANT[variant]}
-            {...others}
-            {...getStyles('cancel', {style, styles, className, classNames})}
-        />
+        <ButtonWithDisabledTooltip
+            disabled={disabled}
+            disabledTooltip={disabledTooltip}
+            disabledTooltipProps={disabledTooltipProps}
+        >
+            <Button
+                ref={ref}
+                variant="filled"
+                color={COLOR_BY_VARIANT[variant]}
+                {...others}
+                {...getStyles('confirm', {style, styles, className, classNames})}
+            />
+        </ButtonWithDisabledTooltip>
     );
 });
