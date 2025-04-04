54b2564c2fd4c39947541cf4c45bf0be172935e5
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2025-04-04 16:53:22 -0400

    fix(mantine): adjust border colors and table grays

--- packages/mantine/src/theme/plasmaCSSVariablesResolver.ts
+++ packages/mantine/src/theme/plasmaCSSVariablesResolver.ts
@@ -9,6 +9,8 @@ export const plasmaCSSVariablesResolver: CSSVariablesResolver = (theme) => {
             '--mantine-color-input-border': theme.colors.gray[3],
             '--mantine-color-error': theme.colors.red[5],
             '--mantine-color-dimmed': theme.colors.gray[5],
+            '--mantine-color-gray-light': theme.colors.gray[0],
+            '--mantine-color-gray-light-hover': theme.colors.gray[1],
         },
     };
     return result;
