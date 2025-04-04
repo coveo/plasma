3b1d3b3a241f7dbe0cfc7a74367b446f2f379c0b
Danny Gauthier <dgauthier@coveo.com>
2025-04-04 10:47:03 -0400

    feat(header): header body compound component

--- packages/website/src/examples/layout/Header/Header.demo.tsx
+++ packages/website/src/examples/layout/Header/Header.demo.tsx
@@ -1,4 +1,4 @@
-import {Anchor, Button, Header} from '@coveord/plasma-mantine';
+import {Anchor, Button, Group, Header, Stack, Text} from '@coveord/plasma-mantine';
 
 const Demo = () => (
     <Header description="The header description" borderBottom>
@@ -9,10 +9,16 @@ const Demo = () => (
         </Header.Breadcrumbs>
         Title
         <Header.DocAnchor href="https://about:blank" label="Tooltip text" />
-        <Header.Actions>
-            <Button>Primary</Button>
-            <Button variant="outline">Secondary</Button>
-        </Header.Actions>
+        <Header.Right wrap="nowrap">
+            <Stack gap="sm" bg="gray.3" p="sm">
+                <Text>Some content</Text>
+                <Text>Some more content</Text>
+            </Stack>
+            <Group gap="sm">
+                <Button>Primary</Button>
+                <Button variant="outline">Secondary</Button>
+            </Group>
+        </Header.Right>
     </Header>
 );
 export default Demo;
