08987f384cabfb1de167127320e3368118d371e7
WilliamBlanchetLafreniere <wblanchet@coveo.com>
2025-05-23 13:41:37 -0400

    fix(website): top-level await not available in env

--- packages/website/scripts/i18n.ts
+++ packages/website/scripts/i18n.ts
@@ -2,8 +2,7 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import HttpBackend from 'i18next-http-backend';
 
-await i18n
-    .use(HttpBackend)
+i18n.use(HttpBackend)
     .use(initReactI18next)
     .init({
         lng: 'en',
