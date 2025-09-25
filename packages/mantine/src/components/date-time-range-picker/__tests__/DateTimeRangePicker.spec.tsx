7ec0f6aceceaa0ed35491d940b944a782e6f1945
Paul Gerold <geroldpaul@gmail.com>
2025-09-25 15:23:10 -0400

    fix: add test for the selection of the date time of datetiemrangepicker

--- packages/mantine/src/components/date-time-range-picker/__tests__/DateTimeRangePicker.spec.tsx
+++ packages/mantine/src/components/date-time-range-picker/__tests__/DateTimeRangePicker.spec.tsx
@@ -1,5 +1,5 @@
 import {useForm} from '@mantine/form';
-import {render, screen, userEvent} from '@test-utils';
+import {render, screen, userEvent, waitFor} from '@test-utils';
 
 import {DatesRangeValue, DateStringValue} from '@mantine/dates';
 import {DateTimeRangePicker} from '../DateTimeRangePicker.js';
@@ -41,5 +41,43 @@ describe('DateTimeRangePicker', () => {
         await user.click(screen.getByRole('option', {name: 'Year 2K'}));
 
         expect(screen.getByTestId('json')).toHaveTextContent('["2000-01-01T00:00:00.000Z","2000-12-31T23:59:59.000Z"]');
+    });
+
+    it('can change the date and time', async () => {
+        const user = userEvent.setup();
+        const Fixture = () => {
+            const form = useForm<{dates: DatesRangeValue<DateStringValue | null>}>({
+                initialValues: {dates: [null, null]},
+            });
+            return (
+                <>
+                    <DateTimeRangePicker
+                        presets={{
+                            year2k: {
+                                label: 'Year 2K',
+                                range: [new Date(2000, 0, 1, 0, 0, 0).toISOString(), null],
+                            },
+                        }}
+                        {...form.getInputProps('dates')}
+                    />
+                    <code data-testid="json">{JSON.stringify(form.values.dates)}</code>
+                </>
+            );
+        };
+        render(<Fixture />);
+
+        await user.click(screen.getByRole('textbox', {name: 'Date range'}));
+        await user.click(screen.getByRole('option', {name: 'Year 2K'}));
+
+        await user.click(screen.getByRole('button', {name: /start/i}));
+        await waitFor(() => expect(screen.getByRole('dialog')).toBeVisible());
+
+        await user.click(screen.getAllByRole('button', {name: /15/i})[0]);
+        await user.click(screen.getAllByRole('spinbutton')[0]);
+        await user.click(screen.getAllByRole('button', {name: '01'})[0]);
+        await user.click(screen.getAllByRole('button', {name: '02'})[1]);
+        await user.click(screen.getByRole('button', {name: 'AM'}));
+
+        expect(screen.getByTestId('json')).toHaveTextContent('["2000-01-15 01:02:00",null]');
     });
 });
