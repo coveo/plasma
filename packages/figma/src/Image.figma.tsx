b85c30cd271bf6d4185e0f48ad24e61a58127667
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-11-14 13:19:41 -0500

    feat(image): connect image to figma

---
+++ packages/figma/src/Image.figma.tsx
@@ -0,0 +1,17 @@
+import {Image} from '@coveord/plasma-mantine';
+import {figma} from '@figma/code-connect';
+
+figma.connect(
+    Image,
+    'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=7-51218&m=dev',
+    {
+        props: {
+            h: figma.enum('Size', {
+                Thumb: 16,
+                Medium: 48,
+                Large: 134,
+            }),
+        },
+        example: ({h}) => <Image h={h} w={h} src={'ImageSVGHere'} />,
+    },
+);
