95118696d6be44223e5a9a45e216917fa79c56f8
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-11-14 15:50:17 -0500

    feat(badge): group the props together

--- packages/figma/src/Badge.figma.tsx
+++ packages/figma/src/Badge.figma.tsx
@@ -1,24 +1,26 @@
 import {Badge} from '@coveord/plasma-mantine';
 import {figma} from '@figma/code-connect';
 
+const badgeProps = {
+    size: figma.enum('Size', {
+        Small: 'small',
+        Large: 'large',
+    }),
+    iconLeft: figma.boolean('Icon Left', {
+        true: figma.instance('Swap Left'),
+    }),
+    iconRight: figma.boolean('Icon Right', {
+        true: figma.instance('Swap Right'),
+    }),
+    placeholder: figma.string('Placeholder'),
+};
+
 figma.connect(
     Badge.Primary,
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma---Components-Library?node-id=7%3A50600',
     {
         variant: {Type: 'Primary'},
-        props: {
-            size: figma.enum('Size', {
-                Small: 'small',
-                Large: 'large',
-            }),
-            iconLeft: figma.boolean('Icon Left', {
-                true: figma.instance('Swap Left'),
-            }),
-            iconRight: figma.boolean('Icon Right', {
-                true: figma.instance('Swap Right'),
-            }),
-            placeholder: figma.string('Placeholder'),
-        },
+        props: badgeProps,
         example: ({size, iconLeft, iconRight, placeholder}) => (
             <Badge.Primary size={size} rightSection={iconRight} leftSection={iconLeft}>
                 {placeholder}
@@ -32,19 +34,7 @@ figma.connect(
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma---Components-Library?node-id=7%3A50600',
     {
         variant: {Type: 'Secondary'},
-        props: {
-            size: figma.enum('Size', {
-                Small: 'small',
-                Large: 'large',
-            }),
-            iconLeft: figma.boolean('Icon Left', {
-                true: figma.instance('Swap Left'),
-            }),
-            iconRight: figma.boolean('Icon Right', {
-                true: figma.instance('Swap Right'),
-            }),
-            placeholder: figma.string('Placeholder'),
-        },
+        props: badgeProps,
         example: ({size, iconLeft, iconRight, placeholder}) => (
             <Badge.Secondary size={size} rightSection={iconRight} leftSection={iconLeft}>
                 {placeholder}
@@ -58,19 +48,7 @@ figma.connect(
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma---Components-Library?node-id=7%3A50600',
     {
         variant: {Type: 'Warning'},
-        props: {
-            size: figma.enum('Size', {
-                Small: 'small',
-                Large: 'large',
-            }),
-            iconLeft: figma.boolean('Icon Left', {
-                true: figma.instance('Swap Left'),
-            }),
-            iconRight: figma.boolean('Icon Right', {
-                true: figma.instance('Swap Right'),
-            }),
-            placeholder: figma.string('Placeholder'),
-        },
+        props: badgeProps,
         example: ({size, iconLeft, iconRight, placeholder}) => (
             <Badge.Warning size={size} rightSection={iconRight} leftSection={iconLeft}>
                 {placeholder}
@@ -84,19 +62,7 @@ figma.connect(
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma---Components-Library?node-id=7%3A50600',
     {
         variant: {Type: 'Critical'},
-        props: {
-            size: figma.enum('Size', {
-                Small: 'small',
-                Large: 'large',
-            }),
-            iconLeft: figma.boolean('Icon Left', {
-                true: figma.instance('Swap Left'),
-            }),
-            iconRight: figma.boolean('Icon Right', {
-                true: figma.instance('Swap Right'),
-            }),
-            placeholder: figma.string('Placeholder'),
-        },
+        props: badgeProps,
         example: ({size, iconLeft, iconRight, placeholder}) => (
             <Badge.Critical size={size} rightSection={iconRight} leftSection={iconLeft}>
                 {placeholder}
@@ -110,19 +76,7 @@ figma.connect(
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma---Components-Library?node-id=7%3A50600',
     {
         variant: {Type: 'Disabled'},
-        props: {
-            size: figma.enum('Size', {
-                Small: 'small',
-                Large: 'large',
-            }),
-            iconLeft: figma.boolean('Icon Left', {
-                true: figma.instance('Swap Left'),
-            }),
-            iconRight: figma.boolean('Icon Right', {
-                true: figma.instance('Swap Right'),
-            }),
-            placeholder: figma.string('Placeholder'),
-        },
+        props: badgeProps,
         example: ({size, iconLeft, iconRight, placeholder}) => (
             <Badge.Disabled size={size} rightSection={iconRight} leftSection={iconLeft}>
                 {placeholder}
