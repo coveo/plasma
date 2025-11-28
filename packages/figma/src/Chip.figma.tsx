04b224b53d2846d717ee596fb3a26990a9ca4bdf
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-11-28 14:02:59 -0500

    feat(chip): fix figma node link

--- packages/figma/src/Chip.figma.tsx
+++ packages/figma/src/Chip.figma.tsx
@@ -1,23 +1,19 @@
 import {Chip} from '@coveord/plasma-mantine';
 import {figma} from '@figma/code-connect';
 
-figma.connect(
-    Chip,
-    'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=7-51718&m=dev',
-    {
-        props: {
-            checked: figma.enum('State', {
-                Checked: true,
-            }),
-            disabled: figma.enum('State', {
-                Disabled: true,
-            }),
-            children: figma.textContent('Awesome chip'),
-        },
-        example: ({checked, disabled, children}) => (
-            <Chip checked={checked} disabled={disabled}>
-                {children}
-            </Chip>
-        ),
+figma.connect(Chip, 'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=7-51718', {
+    props: {
+        checked: figma.enum('State', {
+            Checked: true,
+        }),
+        disabled: figma.enum('State', {
+            Disabled: true,
+        }),
+        children: figma.textContent('Awesome chip'),
     },
-);
+    example: ({checked, disabled, children}) => (
+        <Chip checked={checked} disabled={disabled}>
+            {children}
+        </Chip>
+    ),
+});
