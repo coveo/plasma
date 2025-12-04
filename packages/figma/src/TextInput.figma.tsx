17747a74b49e0b72597f71f1dcd2d5955cb80f3b
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2025-12-04 10:57:16 -0500

    feat(figma): adjust inputs to new figma component structure (#4223)

---
+++ packages/figma/src/TextInput.figma.tsx
@@ -0,0 +1,53 @@
+import {TextInput} from '@coveord/plasma-mantine';
+import {figma} from '@figma/code-connect';
+import {type ReactNode} from 'react';
+
+figma.connect(
+    TextInput,
+    'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=7-50528',
+    {
+        props: {
+            wrapperProps: figma.nestedProps('Input.Wrapper', {
+                labelProps: figma.boolean('Show Label', {
+                    true: figma.nestedProps('Input.Label', {
+                        required: figma.boolean('Show Asterisk'),
+                        label: figma.string('Label'),
+                    }),
+                    false: {label: undefined, required: false},
+                }),
+                descriptionProps: figma.boolean('Show Description', {
+                    true: figma.nestedProps('Input.Description', {
+                        description: figma.string('Description'),
+                    }),
+                    false: {description: undefined},
+                }),
+                errorProps: figma.boolean('Show Error', {
+                    true: figma.nestedProps('Input.Error', {
+                        error: figma.string('Error'),
+                    }),
+                    false: {error: undefined},
+                }),
+            }),
+            inputProps: figma.nestedProps('Input.Input', {
+                placeholder: figma.boolean('Show Placeholder', {true: figma.string('Placeholder'), false: undefined}),
+                leftSection: figma.boolean<ReactNode, never>('Left Section', {true: figma.instance('Swap Left')}),
+                rightSection: figma.boolean<ReactNode, never>('Right Section', {true: figma.instance('Swap Right')}),
+                disabled: figma.enum('State', {Disabled: true}),
+                readOnly: figma.enum('State', {'Read-only': true}),
+            }),
+        },
+        example: (props) => (
+            <TextInput
+                label={props.wrapperProps.labelProps.label}
+                description={props.wrapperProps.descriptionProps.description}
+                placeholder={props.inputProps.placeholder}
+                leftSection={props.inputProps.leftSection}
+                rightSection={props.inputProps.rightSection}
+                required={props.wrapperProps.labelProps.required}
+                disabled={props.inputProps.disabled}
+                readOnly={props.inputProps.readOnly}
+                error={props.wrapperProps.errorProps.error}
+            />
+        ),
+    },
+);
