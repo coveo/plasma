47a102ce3f0e71ba1171889545aad6ffef2de88f
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-12-12 14:25:53 -0500

    feat(codeconect): preparing switch, kbd, loader and slider

--- packages/figma/src/Loader.figma.tsx
+++ packages/figma/src/Loader.figma.tsx
@@ -6,9 +6,9 @@ figma.connect(
     'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=2266-12188',
     {
         props: {
-            size: figma.enum('size', {
-                Small: 'sm',
-                Large: 'lg',
+            size: figma.enum('Size', {
+                sm: 'sm',
+                lg: 'lg',
             }),
         },
         example: (props) => <Loader size={props.size} />,
--- packages/figma/src/Switch.figma.tsx
+++ packages/figma/src/Switch.figma.tsx
@@ -40,3 +40,46 @@ figma.connect(Switch, 'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasm
         />
     ),
 });
+
+figma.connect(
+    Switch.Group,
+    'https://www.figma.com/design/FIkUthFdwxiJKSBE06qjY0/Plasma-3.0---Components?node-id=2250-5136',
+    {
+        props: {
+            checked: figma.boolean('Checked'),
+            disabled: figma.enum('State', {
+                Disabled: true,
+            }),
+            readOnly: figma.enum('State', {
+                'Read-Only': true,
+            }),
+            labelProps: figma.nestedProps('Input.Label', {
+                required: figma.boolean('Asterisk'),
+                label: figma.string('Label'),
+            }),
+
+            descriptionProps: figma.boolean('Description', {
+                true: figma.nestedProps('Input.Description', {
+                    description: figma.string('Description'),
+                }),
+                false: {description: undefined},
+            }),
+            errorProps: figma.boolean('Error', {
+                true: figma.nestedProps('Input.Error', {
+                    error: figma.string('Error'),
+                }),
+                false: {error: undefined},
+            }),
+        },
+        example: (props) => (
+            <Switch.Group
+                label={props.labelProps.label}
+                description={props.descriptionProps.description}
+                disabled={props.disabled}
+                readOnly={props.readOnly}
+                required={props.labelProps.required}
+                error={props.errorProps.error}
+            />
+        ),
+    },
+);
