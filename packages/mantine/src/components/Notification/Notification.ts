7716462396947b980f462c95d298fb1558563271
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-12-25 12:52:19 -0500

    Ad UI 11088 notification codeconnect (#4232)
    * feat(notifications): +notifications in codeconnect
    
    * feat(notification): apply ux review
    
    * feat(notification): +stories for notif and notif system

--- packages/mantine/src/components/Notification/Notification.ts
+++ packages/mantine/src/components/Notification/Notification.ts
@@ -1,1 +1,5 @@
+import {Notification} from '@mantine/core';
+
+Notification.displayName = 'Notification';
+
 export {Notification, type NotificationFactory, type NotificationProps} from '@mantine/core';
