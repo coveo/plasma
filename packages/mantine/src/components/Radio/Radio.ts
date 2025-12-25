addbdb7e8d3d75f82cb5077c4eb4addc9f5d64c2
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2025-12-25 11:04:40 -0500

    Ad UI 11066 checkbox pill radio codeconnect (#4261)
    * feat(checkbox): add readonly, label and disabled
    
    * feat(checkbox): fix config file
    
    * feat(codeconnect): +connect radio, switch and cehckbox, fix type in inputs
    
    * feat(codeconnect): +pill, fix checkbox
    
    * feat(code-connect): +storybook checkbox, pill, radio
    
    * feat(radiocard): fix checked state
    
    * feat(codeconnect): +radio, checkbox stories, fix in figma too

--- packages/mantine/src/components/Radio/Radio.ts
+++ packages/mantine/src/components/Radio/Radio.ts
@@ -1,1 +1,6 @@
+import {Radio} from '@mantine/core';
+
+Radio.displayName = 'Radio';
+Radio.Group.displayName = 'Radio.Group';
+
 export {Radio, type RadioFactory, type RadioProps} from '@mantine/core';
