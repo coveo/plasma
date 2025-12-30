6ff9632094819942714c31e0b3c4333c2850bbe1
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2025-12-30 10:32:04 -0500

    feat: component Select storybook

---
+++ packages/storybook/src/form/Select.stories.tsx
@@ -0,0 +1,54 @@
+import {Select} from '@coveord/plasma-mantine/components/Select';
+import type {Meta, StoryObj} from '@storybook/react-vite';
+import {InputArgs, InputArgTypes, InputWrapperArgs, InputWrapperArgTypes} from './InputArgs.js';
+
+const meta: Meta<typeof Select> = {
+    title: '@components/form/Select',
+    component: Select,
+    parameters: {
+        layout: 'centered',
+    },
+    args: {
+        ...InputWrapperArgs,
+        ...InputArgs,
+        placeholder: 'Pick a value',
+        data: ['Apple', 'Orange', 'Banana', 'Grape', 'Pineapple', 'Mango', 'Strawberry', 'Blueberry', 'Watermelon'],
+        searchable: false,
+        clearable: false,
+        allowDeselect: true,
+    },
+    argTypes: {
+        ...InputWrapperArgTypes,
+        ...InputArgTypes,
+        searchable: {
+            control: 'boolean',
+            description: 'Allow searching through options',
+            table: {
+                defaultValue: {summary: 'false'},
+                type: {summary: 'boolean'},
+            },
+        },
+        clearable: {
+            control: 'boolean',
+            description: 'Allow clearing the value',
+            table: {
+                defaultValue: {summary: 'false'},
+                type: {summary: 'boolean'},
+            },
+        },
+        allowDeselect: {
+            control: 'boolean',
+            description: 'Allow deselecting the value by clicking on the selected option',
+            table: {
+                defaultValue: {summary: 'true'},
+                type: {summary: 'boolean'},
+            },
+        },
+    },
+};
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+export const Demo: Story = {
+    render: (props: any) => <Select {...props} />,
+};
