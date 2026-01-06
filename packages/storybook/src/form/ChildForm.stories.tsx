c27ebe8f5ad1f827043d227b4054f54b091a1c8b
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2026-01-06 11:13:51 -0500

    feat: component ChildForm storybook (#4273)

--- packages/storybook/src/form/ChildForm.stories.tsx
+++ packages/storybook/src/form/ChildForm.stories.tsx
@@ -1,6 +1,6 @@
-import type {StoryObj, Meta} from '@storybook/react-vite';
-import {ChildForm} from '@coveord/plasma-mantine/components/ChildForm';
-import {TextInput, Stack, Switch, useForm} from '@coveord/plasma-mantine';
+import {ChildForm, type ChildFormProps} from '@coveord/plasma-mantine/components/ChildForm';
+import type {Meta, StoryObj} from '@storybook/react-vite';
+import {FunctionComponent, ReactNode} from 'react';
 
 const meta: Meta<typeof ChildForm> = {
     title: '@components/form/ChildForm',
@@ -8,46 +8,44 @@ const meta: Meta<typeof ChildForm> = {
     parameters: {
         layout: 'centered',
     },
-    tags: ['autodocs'],
+    args: {
+        in: true,
+    },
+    argTypes: {
+        title: {control: 'text'},
+        description: {control: 'text'},
+    },
 };
 export default meta;
 type Story = StoryObj<typeof ChildForm>;
 
-export const Default: Story = {
-    render: () => (
-        <ChildForm in={true}>
-            <TextInput label="First name" required />
-            <TextInput label="Last name" required />
-        </ChildForm>
-    ),
-};
+const Inside = () => (
+    <div
+        style={{
+            height: 72,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'var(--mantine-color-blue-2)',
+        }}
+    >
+        Inside
+    </div>
+);
+Inside.displayName = 'Inside';
 
-export const ChildFormWithForm: Story = {
-    render: () => {
-        const form = useForm({
-            initialValues: {
-                provideInfo: false,
-                firstName: '',
-                lastName: '',
-            },
-        });
-        return (
-            <Stack gap="md">
-                <Switch {...form.getInputProps('provideInfo')} label="Provide information" />
-                <ChildForm in={!!form.values.provideInfo}>
-                    <TextInput label="First name" required {...form.getInputProps('firstName')} />
-                    <TextInput label="Last name" required {...form.getInputProps('lastName')} />
-                </ChildForm>
-            </Stack>
-        );
-    },
-};
+const Text: FunctionComponent<{children: ReactNode}> = ({children}) => <div style={{width: 300}}>{children}</div>;
+Text.displayName = 'Text';
 
-export const ChildFormWithTitleAndDescription: Story = {
-    render: () => (
-        <ChildForm in={true} title="Personal information" description="Provide the user's personal information">
-            <TextInput label="First name" required />
-            <TextInput label="Last name" required />
-        </ChildForm>
+export const Demo: Story = {
+    render: (props: ChildFormProps) => (
+        <>
+            <Text>Above</Text>
+            <ChildForm {...props}>
+                <Inside />
+            </ChildForm>
+            <Text>Below</Text>
+        </>
     ),
 };
