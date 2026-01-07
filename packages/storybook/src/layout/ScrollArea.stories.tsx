a84ba040474834cf1d25f3da72216cfaa9c22187
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2026-01-07 11:42:56 -0500

    feat(scrollarea): +,story, +figma

---
+++ packages/storybook/src/layout/ScrollArea.stories.tsx
@@ -0,0 +1,37 @@
+import {ScrollArea} from '@coveord/plasma-mantine/components/ScrollArea';
+import {faker} from '@faker-js/faker';
+import type {Meta, StoryObj} from '@storybook/react-vite';
+
+const meta: Meta<typeof ScrollArea> = {
+    title: '@components/layout/ScrollArea',
+    component: ScrollArea,
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        h: {
+            control: {type: 'number', max: 550},
+            description: 'Height of the scroll area',
+            table: {
+                type: {summary: 'string | number'},
+                defaultValue: {summary: '100%'},
+            },
+        },
+    },
+    args: {
+        h: 250,
+    },
+    decorators: [
+        (Story: any) => (
+            <div style={{width: 300, border: '2px dashed #ccc'}}>
+                <Story />
+            </div>
+        ),
+    ],
+};
+export default meta;
+type Story = StoryObj<typeof ScrollArea>;
+
+export const Demo: Story = {
+    render: (props: any) => <ScrollArea {...props}>{faker.lorem.paragraph(30)}</ScrollArea>,
+};
