9c5f1452f9c8cbdfdc4fd3f4d62565f5a58f7842
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2026-01-07 16:43:37 -0500

    feat(stickyfooter): +figma, *story adjustment (#4287)

--- packages/storybook/src/layout/StickyFooter.stories.tsx
+++ packages/storybook/src/layout/StickyFooter.stories.tsx
@@ -1,6 +1,6 @@
-import type {StoryObj, Meta} from '@storybook/react-vite';
-import {StickyFooter} from '@coveord/plasma-mantine/components/StickyFooter';
 import {Button} from '@coveord/plasma-mantine';
+import {StickyFooter} from '@coveord/plasma-mantine/components/StickyFooter';
+import type {Meta, StoryObj} from '@storybook/react-vite';
 
 const meta: Meta<typeof StickyFooter> = {
     title: '@components/layout/StickyFooter',
@@ -8,15 +8,39 @@ const meta: Meta<typeof StickyFooter> = {
     parameters: {
         layout: 'centered',
     },
-    tags: ['autodocs'],
+    argTypes: {
+        borderTop: {
+            control: 'boolean',
+            description: 'Whether to show a border on the top of the footer',
+            table: {
+                type: {summary: 'boolean'},
+                defaultValue: {summary: 'false'},
+            },
+        },
+    },
+    args: {
+        borderTop: false,
+    },
+    decorators: [
+        (Story: any) => (
+            <div
+                style={{
+                    width: 1000,
+                }}
+            >
+                <Story />
+            </div>
+        ),
+    ],
 };
 export default meta;
 type Story = StoryObj<typeof StickyFooter>;
 
 export const Default: Story = {
-    render: () => (
-        <StickyFooter borderTop>
+    render: (props) => (
+        <StickyFooter {...props}>
             <Button.Secondary>Cancel</Button.Secondary>
+            <Button.Secondary>Back</Button.Secondary>
             <Button.Primary>Save</Button.Primary>
         </StickyFooter>
     ),
