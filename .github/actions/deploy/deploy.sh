34e4a01b969025988d8f60e88ca28cddef87335a
Gaël Dostie <35579930+gdostie@users.noreply.github.com>
2026-01-20 15:52:12 -0500

    feat(deploy): update deployment url for Storybook and Plasma website
    Storybook is now at /
    Website is now at /old/

--- .github/actions/deploy/deploy.sh
+++ .github/actions/deploy/deploy.sh
@@ -5,16 +5,26 @@ set -e
 BRANCH="$1"
 BUCKET="$2"
 DESTINATION=react-vapor
+ORIGIN="https://plasma.coveo.com"
+STORYBOOK_PATHNAME="/"
+OLD_WEBSITE_PATHNAME="/old/"
+DEMO_PATHNAME="/feature/"
+
 
 if [ "$BRANCH" = "master" ]
 then
-    echo "Deploying Plasma website"
-    aws s3 sync ./packages/website/dist s3://${BUCKET}/${DESTINATION}
-    echo "master branch successfully deployed to https://plasma.coveo.com/"
+    echo "Deploying Storybook"
+    aws s3 cp ./packages/storybook/storybook-static s3://${BUCKET}/${DESTINATION}${STORYBOOK_PATHNAME} --recursive
+    echo "Storybook successfully deployed to ${ORIGIN}${STORYBOOK_PATHNAME}"
+    echo "Deploying old Plasma website"
+    aws s3 cp ./packages/website/dist s3://${BUCKET}/${DESTINATION}${OLD_WEBSITE_PATHNAME} --recursive
+    echo "Old Plasma website successfully deployed to ${ORIGIN}${OLD_WEBSITE_PATHNAME}"
 else
     CLEAN_BRANCH_NAME=${BRANCH//\//-} # replaces all / by -
-    echo "Deploying new demo of branch ${BRANCH}."
-    aws s3 sync ./packages/website/dist s3://${BUCKET}/${DESTINATION}/feature/${CLEAN_BRANCH_NAME}
-    echo "demo-output-path=$CLEAN_BRANCH_NAME" >> $GITHUB_OUTPUT
-    echo "Branch ${BRANCH} successfully deployed to https://plasma.coveo.com/feature/${CLEAN_BRANCH_NAME}/"
+    echo "demo-output-path=${ORIGIN}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}" >> $GITHUB_OUTPUT
+    echo "Uploading storybook files of ${BRANCH} to ${BUCKET}/${DESTINATION}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${STORYBOOK_PATHNAME}."
+    aws s3 cp ./packages/storybook/storybook-static s3://${BUCKET}/${DESTINATION}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${STORYBOOK_PATHNAME} --recursive
+    echo "Uploading plasma-website files of ${BRANCH} to ${BUCKET}/${DESTINATION}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${OLD_WEBSITE_PATHNAME}."
+    aws s3 cp ./packages/website/dist s3://${BUCKET}/${DESTINATION}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${OLD_WEBSITE_PATHNAME} --recursive
+    echo "Branch ${BRANCH} successfully deployed to ${ORIGIN}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${STORYBOOK_PATHNAME} and ${ORIGIN}${DEMO_PATHNAME}${CLEAN_BRANCH_NAME}${OLD_WEBSITE_PATHNAME}"
 fi
--- packages/website/build/getBasePath.ts
+++ packages/website/build/getBasePath.ts
@@ -2,8 +2,8 @@ const isCI = !!process.env.JENKINS_HOME || process.env.CI === 'true';
 const branchName = process.env.BRANCH_NAME || process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF_NAME;
 
 let basePath = '/';
-if (isCI && branchName !== 'master') {
-    basePath = `/feature/${branchName.replaceAll('/', '-')}/`;
+if (isCI) {
+    basePath = branchName === 'master' ? '/old/' : `/feature/${branchName.replaceAll('/', '-')}/old/`;
 }
 
 export default basePath;
