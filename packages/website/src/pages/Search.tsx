747492ed90f1baa7f206b7eb49ca587b1eaed11b
Germain Bergeron <GermainBergeron@users.noreply.github.com>
2026-03-19 11:23:05 -0400

    chore(website): remove search functionnalities

--- packages/website/src/pages/Search.tsx
+++ packages/website/src/pages/Search.tsx
@@ -1,50 +1,5 @@
-import {
-    buildResultList,
-    loadPaginationActions,
-    loadQueryActions,
-    loadSearchActions,
-    loadSearchAnalyticsActions,
-} from '@coveo/headless';
-import {AppShell} from '@coveord/plasma-mantine';
-import {useContext} from 'react';
-import {ResultList} from '../building-blocs/ResultList.js';
-import {EngineContext} from '../search/engine/EngineContext.js';
+import {Navigate} from 'react-router-dom';
 
-const Search = () => {
-    const engine = useContext(EngineContext);
-
-    if (!engine) {
-        return null;
-    }
-
-    const query = (engine?.state?.query?.q as string) ?? '';
-    const data = localStorage.getItem('coveo-standalone-search-box-data');
-    const {registerNumberOfResults} = loadPaginationActions(engine);
-
-    if (data) {
-        localStorage.removeItem('coveo-standalone-search-box-data');
-        const {value, analytics} = JSON.parse(data);
-        const {cause, metadata} = analytics;
-
-        const {updateQuery} = loadQueryActions(engine);
-        const {logOmniboxFromLink, logSearchFromLink} = loadSearchAnalyticsActions(engine);
-        const {executeSearch} = loadSearchActions(engine);
-        const event = cause === 'searchFromLink' ? logSearchFromLink() : logOmniboxFromLink(metadata);
-
-        engine.dispatch(registerNumberOfResults(1000));
-        engine.dispatch(updateQuery({q: value}));
-        engine.dispatch(executeSearch(event));
-    } else {
-        engine.dispatch(registerNumberOfResults(1000));
-        engine.executeFirstSearch();
-    }
-
-    const controller = buildResultList(engine);
-    return (
-        <AppShell.Main>
-            <ResultList controller={controller} engine={engine} query={query} />
-        </AppShell.Main>
-    );
-};
+const Search = () => <Navigate to="/" replace />;
 
 export default Search;
