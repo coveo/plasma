dee08fdec543708cf1c4212320cf6f7dfa9d82c3
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2026-04-09 10:41:30 -0400

    feat(alert): +Success variant, +border

--- packages/mantine/src/components/Alert/Alert.tsx
+++ packages/mantine/src/components/Alert/Alert.tsx
@@ -22,6 +22,7 @@ type AlertOverloadFactory = Factory<{
         Advice: typeof AlertAdvice;
         Warning: typeof AlertWarning;
         Critical: typeof AlertCritical;
+        Success: typeof AlertSuccess;
     };
 }>;
 
@@ -30,45 +31,61 @@ Alert.displayName = 'Alert';
 
 const AlertInformation = Alert.withProps({
     color: 'gray',
-    icon: <InfoToken.Information />,
+    icon: <InfoToken.Information size="lg" />,
     vars: () => ({
         root: {
             '--alert-color': 'var(--mantine-color-text)',
             '--alert-bg': 'var(--mantine-color-gray-light)',
+            '--alert-bd': '2px solid var(--mantine-color-gray-light)',
         },
     }),
 });
 (AlertInformation as ComponentType).displayName = 'Alert.Information';
 
 const AlertAdvice = Alert.withProps({
-    icon: <InfoToken.Advice />,
+    icon: <InfoToken.Advice size="lg" />,
     vars: () => ({
         root: {
             '--alert-color': 'var(--mantine-primary-color-filled)',
             '--alert-bg': 'var(--mantine-primary-color-light)',
+            '--alert-bd': '2px solid var(--mantine-primary-color-light)',
         },
     }),
 });
 (AlertAdvice as ComponentType).displayName = 'Alert.Advice';
 
+const AlertSuccess = Alert.withProps({
+    icon: <InfoToken.Success size="lg" />,
+    vars: () => ({
+        root: {
+            '--alert-color': 'var(--mantine-color-green-filled)',
+            '--alert-bg': 'var(--mantine-color-green-light)',
+            '--alert-bd': '2px solid var(--mantine-color-green-light)',
+        },
+    }),
+});
+(AlertSuccess as ComponentType).displayName = 'Alert.Success';
+
 const AlertWarning = Alert.withProps({
     color: 'warning',
-    icon: <InfoToken.Warning />,
+    icon: <InfoToken.Warning size="lg" />,
     vars: () => ({
         root: {
             '--alert-color': 'var(--mantine-color-warning-filled)',
             '--alert-bg': 'var(--mantine-color-warning-light)',
+            '--alert-bd': '2px solid var(--mantine-color-warning-light)',
         },
     }),
 });
 (AlertWarning as ComponentType).displayName = 'Alert.Warning';
 
 const AlertCritical = Alert.withProps({
-    icon: <InfoToken.Error />,
+    icon: <InfoToken.Error size="lg" />,
     vars: () => ({
         root: {
             '--alert-color': 'var(--mantine-color-error)',
             '--alert-bg': 'var(--mantine-color-red-light)',
+            '--alert-bd': '2px solid var(--mantine-color-red-light)',
         },
     }),
 });
@@ -78,3 +95,4 @@ Alert.Information = AlertInformation;
 Alert.Advice = AlertAdvice;
 Alert.Warning = AlertWarning;
 Alert.Critical = AlertCritical;
+Alert.Success = AlertSuccess;
